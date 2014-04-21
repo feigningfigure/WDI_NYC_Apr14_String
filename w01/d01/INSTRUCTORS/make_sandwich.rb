5dda5121c553f446c25aeeba64201c7a52b04525
omardelarosa <thedelarosa@gmail.com>
2014-04-21 14:56:02 -0400

    added pseudocode

---
+++ w01/d01/INSTRUCTORS/make_sandwich.rb
@@ -0,0 +1,9 @@
+load './student.rb'
+
+john = Student.new
+
+john.find("jelly")
+john.find("peanut butter")
+
+puts "John's bag appears below:"
+puts john.bag
---
+++ w01/d01/INSTRUCTORS/student.rb
@@ -0,0 +1,31 @@
+class Student
+
+  # sets the starting state of the object
+  def initialize
+    @bag = ["bread"]
+  end
+
+  # this is a method
+  def find(ingredient)
+    # go through process of locating ingredient
+    puts "Running around"
+    add_to_bag(ingredient)
+    return "#{ingredient} Found!"
+  end
+
+  # this is a data field
+  def bag
+    @bag
+  end
+
+  # this is a method
+  def add_to_bag(ingredient)
+    @bag.push(ingredient)
+  end
+
+  # this is a method
+  def learn
+
+  end
+
+end
