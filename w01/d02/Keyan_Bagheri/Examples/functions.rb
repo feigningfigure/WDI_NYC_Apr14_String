3d574f5d238ee0bc74293c90922b99a2a95aef03
keyanbagheri <keyanbagheri@gmail.com>
2014-04-22 16:46:19 -0400

    test

--- w01/d02/Keyan_Bagheri/Examples/functions.rb
+++ w01/d02/Keyan_Bagheri/Examples/functions.rb
@@ -11,3 +11,16 @@ end
 def double_of_string(string)
 	return string+string if string.class == String
 end
+
+def ask_question
+	"Do you want to quit? (y)es?"
+	answer = gets.chomp
+	if answer = "y"
+		puts "Goodbye!"
+		return true
+	else
+		ask_question
+	end
+end
+
+ask_question
