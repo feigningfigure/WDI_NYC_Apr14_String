ecbaeec85396ddb3d4aca353973911b4c23f3991
jpark1120 <parkj1120@gmail.com>
2014-04-22 16:45:18 -0400

    updated at the end of class

---
+++ w01/d02/Joe_Park/drinking_age.rb
@@ -0,0 +1,26 @@
+print "what is your age? "
+user_age = gets.chomp.to_i
+
+if user_age > 100 && user_age < 300
+	print "Your age is: "
+	print user_age
+	print "\n" #puts new line... puts "" also works
+	puts "You are way old! "
+elsif user_age >= 21 && user_age <= 100
+	print "Your age is: "
+	print user_age
+	puts ""
+	puts "Enjoy your drinks"
+elsif user_age < 21 && user_age > 10
+	print "Your age is: "
+	print user_age
+	print "\n"
+	puts "Don't even think about it!"
+elsif user_age >= 300
+	print "Your age is: "
+	print user_age
+	puts ""
+	puts "You are not a mortal human being!"
+else
+	puts "No...just No"
+end
