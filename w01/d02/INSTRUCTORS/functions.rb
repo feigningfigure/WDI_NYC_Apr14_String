cee819f5963c65d85df444b9f24881ce606c2564
omardelarosa <thedelarosa@gmail.com>
2014-04-22 16:55:33 -0400

    updated some class code in functions.rb

--- w01/d02/INSTRUCTORS/functions.rb
+++ w01/d02/INSTRUCTORS/functions.rb
@@ -12,14 +12,3 @@ def double_of_string(word)
   return word+word if word.class == String
   "Not a string"
 end
-
-def ask_question
-  "Do you want to quit? (y)es?"
-  answer = gets.chomp
-  if answer == "y"
-    puts "Goodbye!"
-    return true
-  else
-    ask_question
-  end
-end
