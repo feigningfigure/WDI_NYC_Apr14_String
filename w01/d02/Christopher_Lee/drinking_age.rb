ea1f158461d32674eec0c94f0b336ce1fe6fee57
ChrisMLee <cristovao.m.lee@gmail.com>
2014-04-22 16:30:37 -0400

    uploading homework from day 1 and the drinking_age exercise

---
+++ w01/d02/Christopher_Lee/drinking_age.rb
@@ -0,0 +1,12 @@
+puts "What's your age?"
+user_age = gets.chomp.to_i
+
+puts "Your age is #{user_age}"
+
+if user_age >= 21
+  puts "Congrats! Here're some good bars"
+elsif user_age < 21 && user_age >= 18
+  puts "Hang in there, just a few more years"
+else
+  puts "Stay in School"
+end
