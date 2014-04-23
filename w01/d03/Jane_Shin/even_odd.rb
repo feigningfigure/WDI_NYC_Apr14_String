16b5654a8a678873c25b860074de6a6d87d26829
janespural <mlle.jihshin@gmail.com>
2014-04-23 13:34:15 -0400

    added array loop to even_odd.rb

--- w01/d03/Jane_Shin/even_odd.rb
+++ w01/d03/Jane_Shin/even_odd.rb
@@ -1,7 +1,13 @@
-for x in (1..100)
-	if x % 2 == 0
-		puts x
-	else
-		puts "odd"
-	end
+# for x in (1..100)
+# 	if x % 2 == 0
+# 		puts x
+# 	else
+# 		puts "odd"
+# 	end
+# end
+
+array = ["wednesday", "macbook", "coffee", "lunch"]
+
+for x in (0..array.length)
+	puts array[x]
 end
