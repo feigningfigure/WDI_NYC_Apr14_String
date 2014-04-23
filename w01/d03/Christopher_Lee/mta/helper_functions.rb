abe89a2d29455d7c334feb0e8c2f5693298d0af1
ChrisMLee <cristovao.m.lee@gmail.com>
2014-04-23 19:10:06 -0400

    Working helper functions. Changed the spec which was mislabled. count_stops function should be titled 'how_many_stops'

--- w01/d03/Christopher_Lee/mta/helper_functions.rb
+++ w01/d03/Christopher_Lee/mta/helper_functions.rb
@@ -1,10 +1,24 @@
+require_relative 'mta.rb'
+
 def which_line(start_name,ending_name)
   if $trains[:n_line].include?(start_name) && $trains[:n_line].include?(ending_name)
     return :n_line
   end
 end
 
-  # counts the stop between two on the same line
+def how_many_stops(start_name, ending_name)
+  start_name = start_name.to_s
+  ending_name = ending_name.to_s
+  if need_to_transfer?(start_name, ending_name)
+    return nil
+  else
+    train = which_line(start_name,ending_name)
+    trip_length = ($trains[train].index(start_name)-$trains[train].index(ending_name)).abs
+    return trip_length
+  end
+end
+
+# counts the stop between two on the same line
   #
   # takes two params:
   # 1. a starting station name
@@ -12,18 +26,22 @@ end
   #
   # returns an "integer equal to how many stops between two stops on the same line"
   # if two stops are not on the same line, return nil
-def count_stops(start_name, ending_name)
-  start_name = start_name.to_s
-  ending_name = ending_name.to_s
-  train = which_line(start_name,ending_name)
-  trip_length = ($trains[train].index(start_name)-$trains[train].index(ending_name)).abs
-  return trip_length
-end
 
-count_stops("Times Square","28th")
-which_line("Times Square", "28th")
 
 def need_to_transfer?(start_name, ending_name)
+  start_name = start_name.to_s
+  ending_name = ending_name.to_s
+  route = [start_name, ending_name]
+  $trains.each do |k,v|
+    match = v & route
+    if match.length == 2
+      return false
+    else
+      return true
+    end
+  end
+end
+
   # deteremines if a transfer is needed
   #
   # takes two params:
@@ -31,5 +49,7 @@ def need_to_transfer?(start_name, ending_name)
   # 2. another station name
   #
   # returns true if transfer is need, or false for all other cases
-end
+
+
+
 
