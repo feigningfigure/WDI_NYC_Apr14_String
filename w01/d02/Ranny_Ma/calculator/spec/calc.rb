132510143e137d78a438dc836faf773ae748f0d3
ranma08 <ranma08@gmail.com>
2014-04-22 20:33:47 -0400

    added partially working calculator, but rspec not working

--- w01/d02/Ranny_Ma/calculator/spec/calc.rb
+++ w01/d02/Ranny_Ma/calculator/spec/calc.rb
@@ -3,30 +3,30 @@ def get_user_choice(num1, operation, num2)
   num2 == num2
 
   if operation == "a"
-        add(num1, num2)
-      elsif operation == "s"
-        subtract(num1, num2)
-      elsif operation == "m"
-        multiply(num1, num2)
-      else operation == "d"
-        divide(num1, num2)
+    add(num1, num2)
+  elsif operation == "s"
+    subtract(num1, num2)
+  elsif operation == "m"
+    multiply(num1, num2)
+  else operation == "d"
+    divide(num1, num2)
   end
 end
 
 def add(num1,num2)
-  num1 + num2
+  puts "#{num1 + num2}"
 end
 
 def subtract(num1,num2)
-  num1 - num2
+  puts "#{num1 - num2}"
 end
 
 def divide(num1,num2)
-  num1 / num2
+  puts "#{num1 / num2}"
 end
 
 def multiply(num1,num2)
-  num1 * num2
+  puts "#{num1 * num2}"
 end
 
 puts "Enter number 1"
