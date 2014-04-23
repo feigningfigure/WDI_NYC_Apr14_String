43698aa3525c7b3e4e35cd03d76f3235543de023
feigningfigure <andrew.g.wallace@gmail.com>
2014-04-22 21:08:20 -0400

    Create working Phase 2 of calculator app

--- w01/d02/Andrew_Wallace/calculator/calc.rb
+++ w01/d02/Andrew_Wallace/calculator/calc.rb
@@ -1,28 +1,63 @@
 def get_user_choice
-  print 'enter your choice: (a)dd, (s)ubtract, (m)ultiply, (d)ivide'
+  print 'enter your choice: (a)dd, (s)ubtract, (m)ultiply, (d)ivide, (e)xpontent, square(r)oot'
   operation = gets.chomp.downcase
-end
 
-  case
-  when operation == "A"
-    print "Okay, let's add some numbers!"
-    add()
-  when operation == "S"
-    print "Okay, let's do some subtraction!"
-    subtract()
-  when operation == "M"
-    print "Okay, let's multiply some numbers!"
-    multiply()
-  when operation == "D"
-    print "Okay, let's divide some numbers!"
-    divide()
+  case operation
+  when "a"
+  puts "What is your first number?"
+  num1 = gets.chomp.to_i
+  puts "What is your second number?"
+  num2 = gets.chomp.to_i
+    add(num1, num2)
+   puts "#{add(num1,num2)}"
+  when "s"
+  puts "What is your first number?"
+  num1 = gets.chomp.to_i
+  puts "What is your second number?"
+  num2 = gets.chomp.to_i
+    subtract(num1, num2)
+    puts "#{subtract(num1,num2)}"
+  when "m"
+  puts "What is your first number?"
+  num1 = gets.chomp.to_i
+  puts "What is your second number?"
+  num2 = gets.chomp.to_i
+    multiply(num1, num2)
+    puts "#{multiply(num1,num2)}"
+  when "d"
+  puts "What is your first number?"
+  num1 = gets.chomp.to_i
+  puts "What is your second number?"
+  num2 = gets.chomp.to_i
+    divide(num1, num2)
+    puts "#{divide(num1,num2)}"
+  when "e"
+    puts "What is your first number?"
+  num1 = gets.chomp.to_i
+  puts "What is your second number?"
+  num2 = gets.chomp.to_i
+  puts "#{exponent(num1,num2)}"
+when "r"
+  puts "What number would you like the square root of?"
+  num = gets.chomp.to_i
+  puts "#{squareroot(num)}"
   else
-    puts "Please enter 'A', 'S', 'M', or 'D'"
+    puts "Please enter 'a', 's', 'm', or 'd', 'e', 'r'"
   end
 end
 
+# Get numbers method to clean up above code
+=begin
+def get_numbers(num1, num2)
+  puts "What is your first number?"
+  num1 = gets.chomp.to_i
+  puts "What is your second number?"
+  num2 = gets.chomp.to_i
+end
+=end
+
 def add(num1,num2)
-  num1.to_i+num2.to_i
+  num1+num2
 end
 
 def subtract(num1,num2)
@@ -41,3 +76,12 @@ def multiply(num1,num2)
   num1.to_i * num2.to_i
 end
 
+def squareroot(num)
+  Math.sqrt(num)
+end
+
+def exponent(num1, num2)
+  num1 ** num2
+end
+
+get_user_choice
