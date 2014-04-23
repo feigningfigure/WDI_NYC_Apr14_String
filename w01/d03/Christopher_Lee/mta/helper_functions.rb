2fc62e1a060c5728d40c4aad55253ff89f72189b
ChrisMLee <cristovao.m.lee@gmail.com>
2014-04-23 18:27:35 -0400

    count_stops is working, assisted by the which_line method

--- w01/d03/Christopher_Lee/mta/helper_functions.rb
+++ w01/d03/Christopher_Lee/mta/helper_functions.rb
@@ -1,13 +1,27 @@
-def count_stops(start_name, ending_name)
+def which_line(start_name,ending_name)
+  if $trains[:n_line].include?(start_name) && $trains[:n_line].include?(ending_name)
+    return :n_line
+  end
+end
+
   # counts the stop between two on the same line
   #
   # takes two params:
   # 1. a starting station name
   # 2. an ending station name
   #
-  # returns an integer equal to how many stops between two stops on the same line
+  # returns an "integer equal to how many stops between two stops on the same line"
   # if two stops are not on the same line, return nil
+def count_stops(start_name, ending_name)
+  start_name = start_name.to_s
+  ending_name = ending_name.to_s
+  train = which_line(start_name,ending_name)
+  trip_length = ($trains[train].index(start_name)-$trains[train].index(ending_name)).abs
+  return trip_length
 end
+
+count_stops("Times Square","28th")
+which_line("Times Square", "28th")
 
 def need_to_transfer?(start_name, ending_name)
   # deteremines if a transfer is needed
@@ -17,6 +31,5 @@ def need_to_transfer?(start_name, ending_name)
   # 2. another station name
   #
   # returns true if transfer is need, or false for all other cases
-
-
 end
+
--- w01/d03/Christopher_Lee/mta/mta.rb
+++ w01/d03/Christopher_Lee/mta/mta.rb
@@ -2,7 +2,53 @@ require_relative 'helper_functions'
 
 # global hash of arrays of strings of station names
 $trains = {
-  :n_line => []
-  :l_line => []
-  :six_line => []
+  :n_line => ["Times Square", "34th", "28th", "23rd", "Union Square", "8th Street"],
+  :l_line => ["8th Ave","6th Ave","Union Square","3rd Ave", "1st Ave"],
+  :six_line => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
 }
+
+test_n_line = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th Street"]
+
+test_six_line = ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
+
+# def total_stops(entrance_line, entrance_stop, exit_line, exit_stop)
+
+# end
+
+# # counting distance between areas of an arrays
+# # e.g. array.length
+# #
+
+
+# test_array = ["8th Ave","6th Ave","Union Square","3rd Ave", "1st Ave"]
+
+# test_array.each_with_index do |item, index|
+#   puts item
+#   puts index
+# end
+
+# puts test_array.index("8th Ave")
+
+# hots = test_array.index("8th Ave")
+
+
+# test_array.index("8th Ave") # 0
+# test_array.index("Union Square") # 2
+
+
+# test_array.index("Union Square") - test_array.index("8th Ave") #works
+
+# # you reverse it: format is final stop - beginning stop
+# test_array.index("1st Ave")-test_array.index("6th Ave") #works
+
+# # works: this is how you want to do it
+# (test_array.index("8th Ave")-test_array.index("3rd Ave")).abs
+
+
+def count_stops(start_name, ending_name)
+  start_name = start_name.to_s
+  ending_name = ending_name.to_s
+  l_line = ["8th Ave","6th Ave","Union Square","3rd Ave", "1st Ave"]
+  trip_length = (l_line.index(start_name)-l_line.index(ending_name)).abs
+  puts trip_length
+end
