fa54cfc0872b31cbf66d91c5429cde31c4f0690c
janespural <mlle.jihshin@gmail.com>
2014-04-23 14:05:55 -0400

    added more to even_odd.rb

--- w01/d03/Jane_Shin/even_odd.rb
+++ w01/d03/Jane_Shin/even_odd.rb
@@ -8,6 +8,16 @@
 
 array = ["wednesday", "macbook", "coffee", "lunch"]
 
-for x in (0..array.length)
-	puts array[x]
+# for x in (0..array.length)
+# 	puts array[x]
+# end
+
+array.each_with_index do |item, index|
+	puts "#{item} is at position #{index} in this array"
 end
+
+# hash = {monday: "52 degrees", tuesday: "60 degrees", wednesday: "56 degrees", thursday: "49 degrees"}
+# #to pull something out of a hash you need to know the key name
+# hash.each do |k, v|
+# 	puts "On #{k.capitalize} it was #{v} outside."
+# end
