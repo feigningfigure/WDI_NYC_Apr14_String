78d73da3c9a2862bf23a62f1daee6e559c58bbbd
cdbajorin <christopher.bajorin@gmail.com>
2014-04-22 20:18:44 -0400

    finished basic calculator

--- w01/d02/Christopher_Bajorin/homework/part3/calculator.rb
+++ w01/d02/Christopher_Bajorin/homework/part3/calculator.rb
@@ -12,17 +12,39 @@ Step 5: output and loop
 =end
 
 
-def calculator # starting method. Starts the menu.
+def turn_calculator_on # starting method. Starts the menu.
   puts "Here is a list of operations. input the symbol:\n
   Addition(+) \n Subtraction(-) \n Multiplication(*) \n Division(%) \n
   If you would like to exit, type 'quit'"
-  math_operation = gets.chomp.to_s
-  operation(math_operation)
-  # puts math_operation
+  operator_user_input = gets.chomp.to_s
+
+  if operator_user_input == "quit" || operator_user_input == "q"
+    puts "Thanks for using this!"
+  elsif operator_user_input == "+" || operator_user_input == "-" || operator_user_input == "*" || operator_user_input == "%"
+  puts "What is your first number?" #later, multiple numbers, create empty array and use .push
+  first_number_user_input = gets.chomp.to_i
+
+  puts "What is your second number?"
+  second_number_user_input = gets.chomp.to_i
+
+  number_array = [first_number_user_input, second_number_user_input] # array of the numbers being used
+
+  calculate( operator_user_input, number_array )
+  end
 end
 
-def operation(operator_input)
-  puts operator_input
+def calculate( operator, number_array )
+  if operator == "+"
+    result =  number_array[0] + number_array[1]
+  elsif operator == "-"
+    result = number_array[0] - number_array[1]
+  elsif operator == "*"
+    result = number_array[0] * number_array[1]
+  elsif operator == "%"
+    result = number_array[0] + number_array[1]
+  end
+  puts "the result of #{number_array[0]} #{operator} #{number_array[1]} is: #{result}\n \n Let's go again! \n \n"
+  turn_calculator_on
 end
 
-calculator
+turn_calculator_on
