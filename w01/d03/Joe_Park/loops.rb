8959bdd966cda4a5712af51ccf8b48607e5178d9
jpark1120 <parkj1120@gmail.com>
2014-04-23 14:02:44 -0400

    finished exercise on arrays/hashes and .each

--- w01/d03/Joe_Park/loops.rb
+++ w01/d03/Joe_Park/loops.rb
@@ -18,14 +18,42 @@
 # end
 
 
-array = ["wednesday", "macbook", "coffee", "lunch", "apple core"]
+# array = ["wednesday", "macbook", "coffee", "lunch", "apple core"]
 
-for index in (0..array.length)
-	puts array[index]
-end
+# for index in (0..array.length)
+# 	puts array[index]
+# end
 
 #or
 
-for item in array
-	puts item
+# for item in array
+# 	puts item
+# end
+
+
+
+
+#using .each with an array = .each means call method each
+array = ["wednesday", "macbook", "coffee", "lunch", "apple core"]
+
+array.each do |item|
+	puts "right now I am thinking of #{item}"
 end
+
+array.each_with_index do |item, index|
+	puts "#{item} is at position #{index} in this array"
+end
+#position matters..what you call it doesn't. if |item, index| was swapped to |index, item|, then ex "0 is at position wednesday in this array" would happen.
+
+hash = {monday: "52 degrees", tuesday: "60 degrees", wednesday: "56 degrees", thursday: "66 degrees"}
+
+hash.each do |key, value|
+	puts key
+	puts value
+end
+
+hash.each do |k, v|
+	puts "On #{k.capitalize} it was #{v} outside"
+end
+
+#you can call methods and operations in string concatenation
