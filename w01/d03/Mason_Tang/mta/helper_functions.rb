cdf352c8b3f9faeaaf3700bc89eda27fbec4516d
masewillis <mwillistang@gmail.com>
2014-04-23 19:30:53 -0400

    first draft of mta code with single line

--- w01/d03/Mason_Tang/mta/helper_functions.rb
+++ w01/d03/Mason_Tang/mta/helper_functions.rb
@@ -1,22 +1,48 @@
-def count_stops(start_name, ending_name)
-  # counts the stop between two on the same line
-  #
-  # takes two params:
-  # 1. a starting station name
-  # 2. an ending station name
-  #
-  # returns an integer equal to how many stops between two stops on the same line
-  # if two stops are not on the same line, return nil
+ def count_stops(dep_station, arv_station)
+#   # counts the stop between two on the same line
+#   #
+#   # takes two params:
+#   # 1. a starting station name
+#   # 2. an ending station name
+#   #
+#   # returns an integer equal to how many stops between two stops on the same line
+#   # if two stops are not on the same line, return nil
+  def return_index (x, y)
+      x.each do |key, value|
+        value.each do |array_value|
+          if array_value == y
+              return value.index(array_value)
+          end
+        end
+      end
+  end
+  count = return_index($trains, dep_station) - return_index($trains, arv_station)
+  return count.abs
+
 end
 
-def need_to_transfer?(start_name, ending_name)
-  # deteremines if a transfer is needed
-  #
-  # takes two params:
-  # 1. a station name
-  # 2. another station name
-  #
-  # returns true if transfer is need, or false for all other cases
+# def need_to_transfer?(start_name, ending_name)
+#   # deteremines if a transfer is needed
+#   #
+#   # takes two params:
+#   # 1. a station name
+#   # 2. another station name
+#   #
+#   # returns true if transfer is need, or false for all other cases
 
 
-end
+# end
+
+# def find_stops(dep_station, arv_station)
+#    $trains.index[:n_line] do |item, index|
+#     puts "#{index}"
+#   end
+# end
+
+
+# def inspect_stops(stop)
+#   if stop.class == Array
+#     amount_stops(stop)
+#   end
+# end
+
--- w01/d03/Mason_Tang/mta/mta.rb
+++ w01/d03/Mason_Tang/mta/mta.rb
@@ -1,8 +1,33 @@
 require_relative 'helper_functions'
 
+
+
+
 # global hash of arrays of strings of station names
 $trains = {
-  :n_line => []
-  :l_line => []
-  :six_line => []
+  :n_line => ["Time Square", "34th", "28th", "23rd", "Union Square", "8th"],
+  :l_line => ["8th", "6th", "Union Square", "3rd", "1st"],
+  :six_line => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
 }
+
+puts "Departure station?"
+dep_station = gets.chomp
+
+puts "Arriving Station?"
+arv_station = gets.chomp
+
+puts count_stops(dep_station, arv_station)
+
+# def test_function2 ($trains, arv_station)
+#   $trains.each do |key, value|
+#         value.each do |array_value|
+#           if array_value == end_name
+#               return value.index(array_value)
+#               end
+#             end
+#           end
+#         end
+
+#         puts test_function($trains, "23rd")
+
+
