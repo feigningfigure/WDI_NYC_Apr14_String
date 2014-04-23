beed3c7172b05463d0060de1b59b6c76afa5cb6c
slaffont <slaffont@gmail.com>
2014-04-23 10:07:31 -0400

    adding homework and files

--- w01/d02/Sophie_Laffont/calculator/calc.rb
+++ w01/d02/Sophie_Laffont/calculator/calc.rb
@@ -1,48 +1,53 @@
 def get_user_choice
-    
+
     print "Hello. I am a calculator. You can use me to add, substract, multiply,
-    or divide numbers. So what would you like to do?"
-    
+    or divide numbers. So what would you like to do? Enter Q to quit."
+
     userRequest = gets.chomp.to_s
-    
-    
+
     print "enter a number"
     x = gets.chomp.to_i
     print "enter another"
     y = gets.chomp.to_i
-    
+
+# OTHER OPTION TO TEST
+# while userRequest != "Q"
+# userRequest == "G"
+ #puts "Bye!"
+#end
+
     if userRequest == "add"
-        
+
         def add(num1, num2)
         return num1 + num2
     end
     puts add(x, y)
-    
+
     elsif userRequest == "substract"
-    
+
     def substract(num1, num2)
         return num1 - num2
     end
     puts substract(x, y)
-    
+
     elsif userRequest == "multiply"
     def multiply(num1, num2)
         return num1 * num2
     end
     puts multiply(x, y)
-    
+
     elsif userRequest == "divide"
     def divide(num1, num2)
         return num1 / num2
     end
-else
+else userRequest == "Q"
     def quit()
      puts "Goodbye!"
 end
-   
+
 end
 
     get_user_choice
-    
+
 
 
