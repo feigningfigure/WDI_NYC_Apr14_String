f719e0cd747f9d99c50fe0fc167d2281a0dfb73d
daramao <dara.mao@gmail.com>
2014-04-24 06:22:35 -0400

    Set up heler functions and userinputs

--- w01/d03/Dara_Mao/mta/helper_functions.rb
+++ w01/d03/Dara_Mao/mta/helper_functions.rb
@@ -1,22 +1,32 @@
-def count_stops(start_name, ending_name)
-  # counts the stop between two on the same line
-  #
-  # takes two params:
-  # 1. a starting station name
-  # 2. an ending station name
-  #
-  # returns an integer equal to how many stops between two stops on the same line
-  # if two stops are not on the same line, return nil
+def how_many_stops(start_name, ending_name,line)
+#If two stations are from the same line, then return nil, else count how many stops.
+    this_line = nil
+
+  $trains.each do |lines,stations|
+   puts lines
+ end
+
+  if  $trains[lines].include? (start_name) && $trains[lines].include? (ending_name)
+      this_line = lines
+  end
+  return this_line
 end
 
-def need_to_transfer?(start_name, ending_name)
-  # deteremines if a transfer is needed
-  #
-  # takes two params:
-  # 1. a station name
-  # 2. another station name
-  #
-  # returns true if transfer is need, or false for all other cases
+#How many stops between 2 stations.
+    diff_btw = $trains[line].index(start_name) - $trains[line].index(ending_name)
+    puts "#{diff_btw.abs} stops"
+  else
+    return nil
+  end
 
+end
 
+def need_to_transfer(start_name,ending_name,line_on, line_off)
+  #return true if stops are not on the same line
+  if $trains[:n_line](start_name) != $trains[:n_line](start_name)
+    return true
+    #return false for stops on the same line 'Times Square' and '28th'
+  else
+    return false
+  end
 end
--- w01/d03/Dara_Mao/mta/mta.rb
+++ w01/d03/Dara_Mao/mta/mta.rb
@@ -1,8 +1,19 @@
 require_relative 'helper_functions'
 
-# global hash of arrays of strings of station names
+# # global hash of arrays of strings of station names
 $trains = {
-  :n_line => []
-  :l_line => []
-  :six_line => []
+  :n => ["Time Square", "34th", "28th", "23rd", "Union Square","8th"],
+  :l => ["8th", "6th", "Union Square", "3rd", "1st"],
+  :six => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
 }
+
+  puts "What station are you getting on?"
+  start_name=gets.chomp
+  puts "Which line is that? (n,l,or 6)"
+  line_on=gets.chomp
+  puts "What station are you getting off?"
+  ending_name=gets.chomp
+  puts "Which line is that? (n,l,or 6)"
+  line_off=gets.chomp
+
+
