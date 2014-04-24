d7f2681d9c54f20d8bc365153c879d4326139545
ChrisMLee <cristovao.m.lee@gmail.com>
2014-04-23 21:10:29 -0400

    Final version with improved user interface. User interface can still be improved. It needs be more forgiving for user input

--- w01/d03/Christopher_Lee/mta/helper_functions.rb
+++ w01/d03/Christopher_Lee/mta/helper_functions.rb
@@ -1,38 +1,3 @@
-require_relative 'mta.rb'
-
-def which_line(start_name,ending_name)
-  if $trains[:n_line].include?(start_name) && $trains[:n_line].include?(ending_name)
-    return :n_line
-  end
-end
-
-# they specify their line!!!!!!
-# def how_many_stops(start_name, ending_name)
-#   start_name = start_name.to_s
-#   ending_name = ending_name.to_s
-#   if need_to_transfer?(start_name, ending_name)
-#     return nil
-#   else
-#     train = which_line(start_name,ending_name)
-#     trip_length = ($trains[train].index(start_name)-$trains[train].index(ending_name)).abs
-#     return trip_length
-#   end
-# end
-
-# without which line
-# def how_many_stops(start_name, ending_name, train_line)
-#   start_name = start_name.to_s
-#   ending_name = ending_name.to_s
-#   if need_to_transfer?(start_name, ending_name)
-#     return nil
-#   else
-#     # train = which_line(start_name,ending_name)
-#     trip_length = ($trains[train_line].index(start_name)-$trains[train_line].index(ending_name)).abs
-#     return trip_length
-#   end
-# end
-
-###################TESTING THIS: getting rid of need to transfer############
 def how_many_stops(start_name, ending_name, train_line)
   start_name = start_name.to_s
   ending_name = ending_name.to_s
@@ -40,21 +5,6 @@ def how_many_stops(start_name, ending_name, train_line)
     trip_length = ($trains[train_line].index(start_name)-$trains[train_line].index(ending_name)).abs
     return trip_length
 end
-
-############### if you are using both lines, you don't need this ####################
-# def need_to_transfer?(start_name, ending_name)
-#   start_name = start_name.to_s
-#   ending_name = ending_name.to_s
-#   route = [start_name, ending_name]
-#   $trains.each do |k,v|
-#     match = v & route
-#     if match.length == 2
-#       return false
-#     else
-#       return true
-#     end
-#   end
-# end
 
 def need_to_transfer?(start_line, end_line)
   start_line = start_line.to_s
@@ -66,21 +16,35 @@ def need_to_transfer?(start_line, end_line)
   end
 end
 
-  # deteremines if a transfer is needed
-  #
-  # takes two params:
-  # 1. a station name
-  # 2. another station name
-  #
-  # returns true if transfer is need, or false for all other cases
+def count_my_train_stops
+  puts "What line would you like to start at?"
+  start_line = gets.chomp.to_s
+  puts "What stop would you start at?"
+  start_stop = gets.chomp.to_s
+  puts "What line would you like to end on?"
+  end_line = gets.chomp.to_s
+  puts "What stop would you like to get off on?"
+  end_stop = gets.chomp.to_s
+  beginning_line = line_picker(start_line)
+  ending_line = line_picker(end_line)
+  if need_to_transfer?(beginning_line, ending_line)
+    leg_1 = how_many_stops(start_stop,"Union Square", beginning_line)
+    leg_2 = how_many_stops("Union Square",end_stop, ending_line)
+    return leg_1 + leg_2
+  else
+    how_many_stops(start_stop,end_stop,ending_line)
+  end
+end
 
-# counts the stop between two on the same line
-  #
-  # takes two params:
-  # 1. a starting station name
-  # 2. an ending station name
-  #
-  # returns an "integer equal to how many stops between two stops on the same line"
-  # if two stops are not on the same line, return nil
 
-
+def line_picker(line)
+  line = line.to_s.downcase
+  case line
+  when "l"
+    return :l_line
+  when "n"
+    return :n_line
+  when "6", "six", "six line"
+    return :six_line
+  end
+end
--- w01/d03/Christopher_Lee/mta/mta.rb
+++ w01/d03/Christopher_Lee/mta/mta.rb
@@ -7,48 +7,4 @@ $trains = {
   :six_line => ["Grand Central", "33rd", "28th street", "23rd street", "Union Square", "Astor Place"]
 }
 
-
-def count_my_train_stops(start_line, start_stop, end_line, end_stop)
-  beginning_line = line_picker(start_line)
-  ending_line = line_picker(end_line)
-  if need_to_transfer?(beginning_line, ending_line)
-    leg_1 = how_many_stops(start_stop,"Union Square", beginning_line)
-    leg_2 = how_many_stops("Union Square",end_stop, ending_line)
-    return leg_1 + leg_2
-  else
-    how_many_stops(start_stop,end_stop,ending_line)
-  end
-
-end
-
-def line_picker(line)
-  line = line.to_s.downcase
-  case line
-  when "l"
-    return :l_line
-  when "n"
-    return :n_line
-  when "6", "six", "six line"
-    return :six_line
-  end
-end
-
-
-
-# test_n_line = ["Times Square", "34th", "28th", "23rd", "Union Square", "8th Street"]
-
-# # test_six_line = ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
-
-#  ["Times Square", "34th", "28th", "23rd", "Union Square", "8th Street"].each_with_index do |v,i|
-#   if v == "Union Square"
-#     puts i
-#   end
-# end
-
-
-#  if need_to_transfer?(start_name, ending_name)
-#   # 1st line stops till Union Square
-#   # 2nd line stops till Union Square
-#   # trip length = 1st + 2nd
-#     how_many_stops("Union Square", ending_name, train_line)
-
+puts count_my_train_stops
