d9ba08dc9cf56b41c84fe73e6213da6bdf3d3193
art <art.incoming@gmail.com>
2014-04-24 08:55:29 -0400

    Need to improve count_stops, tackle intersections and make rspec totally green

--- w01/d03/Artem_Murga/lab/mta/mta.rb
+++ w01/d03/Artem_Murga/lab/mta/mta.rb
@@ -7,23 +7,40 @@ $trains = {
   :s => ["Grand Central", "33rd", "28th_s", "23rd_s", "Union Square", "Astor Place"]
 }
 
+#helper functions just for a moment to be here
+
+
+  # deteremines if a transfer is needed
+  #
+  # takes two params:
+  # 1. a station name
+  # 2. another station name
+  #
+  # returns true if transfer is need, or false for all other cases
+  
+
 def start
-	puts "Please, type your start line"
-	start_line = gets.chomp.to_sym
-	puts "Please, type your destination line"
-	destination_line = gets.chomp.to_sym
 	puts "Please, type your start station"
-	start_station = gets.chomp
+	start_name =  gets.chomp # "Times Square"
 	puts "Please, type your destination station"
-	destination_station = gets.chomp
-	if start_line == destination_line
-		distance = start_line.index(destination_station) - start_line.index(start_station)
-		puts "The distance between stations is #{distance} stations"
-	else
-		puts "I can't understand you. Please, try again"
+	ending_name = gets.chomp # "3rd" #gets.chomp
+	#need_to_transfer? doesn't work
+	if need_to_transfer?(start_name, ending_name) == false
+		count_stops(start_name, ending_name)
+	else 
+		puts "need to figure out about 2 lines"
 	end
 end
 
 start
+
+# $trains.each do |key, value|
+#     inspect_stations(value)
+
+# 
+# p need_to_transfer?("Time Square", "28th")
+# p need_to_transfer?("Time Square", "6th")
+
+
 
 
