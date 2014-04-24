c7f9a16232bb6242941653099273249ea829ea9e
janespural <mlle.jihshin@gmail.com>
2014-04-24 10:26:47 -0400

    submitting hw for w01 d03

--- w01/d03/Jane_Shin/mta/helper_functions.rb
+++ w01/d03/Jane_Shin/mta/helper_functions.rb
@@ -1,4 +1,22 @@
-def how_many_stops(start_name, ending_name)
+def get_stop_names
+  puts "What stop are you starting from? 'N', 'L', or 'Six'?" #ask for lines and not just stops
+    start_line = gets.chomp.downcase.to_sym #keys in $trains are symbols and lowercase
+  puts "Which stop: #{$trains[start_line].join(', "')}?"
+    start_name = gets.chomp #string
+
+  puts "Which line do you want to end on? 'N', 'L', or 'Six'?"
+    ending_line = gets.chomp.downcase.to_sym #keys in $trains are symbols and lowercase
+  puts "Which stop: #{$trains[ending_line].join(', "')}?"
+    ending_name = gets.chomp #string
+
+#one way to bundle variables for future use outside of this function
+start_location = [start_line, start_name]
+ending_location = [ending_line, ending_name]
+journey = [start_location, ending_location]
+   
+end
+
+def how_many_stops(journey)
   # counts the stop between two on the same line
   #
   # takes two params:
@@ -7,13 +25,19 @@ def how_many_stops(start_name, ending_name)
   #
   # returns an integer equal to how many stops between two stops on the same line
   # if two stops are not on the same line, return nil
-  n_line = ["Times Square", "34th", "28th", "23rd", "14th", "8th"]
-  if n_line.include?(start_name) && n_line.include?(ending_name)
-    total = n_line.index(ending_name) - n_line.index(start_name)
+  if $trains[:n_line].include?(start_name) && $trains[:n_line].include?(ending_name)
+    total = $trains[:n_line].index(ending_name) - $trains[:n_line].index(start_name)
     return total.abs
+  elsif $trains[:l_line].include?(start_name) && $trains[:l_line].include?(ending_name)
+    total = $trains[:l_line].index(ending_name) - $trains[:l_line].index(start_name)
+    return total.abs
+  elsif $trains[:six_line].include?(start_name) && $trains[:six_line].include?(ending_name)
+    total = $trains[:six_line].index(ending_name) - $trains[:six_line].index(start_name)
+    return total.abs 
   else
     return nil
   end
+
 end
 
 def need_to_transfer?(start_name, ending_name)
@@ -24,6 +48,18 @@ def need_to_transfer?(start_name, ending_name)
   # 2. another station name
   #
   # returns true if transfer is need, or false for all other cases
-
-
+  if how_many_stops(start_name, ending_name) == nil
+    return true
+  else how_many_stops(start_name, ending_name) > 0
+    return false
+  end
 end
+
+
+# def list_of_lines(master)
+#   # case = master
+#   when master.each do |k, v|
+#       print "pick from the following"
+#       puts "#{k}"
+#     end
+# end
--- w01/d03/Jane_Shin/mta/mta.rb
+++ w01/d03/Jane_Shin/mta/mta.rb
@@ -2,7 +2,29 @@ require_relative 'helper_functions'
 
 # global hash of arrays of strings of station names
 $trains = {
-  :n_line => []
-  :l_line => []
-  :six_line => []
+  :n => ["Times Square", "34th", "28th_n", "23rd_n", "Union Square", "8th"],
+  :l => ["8th", "6th", "Union Square", "3rd", "1st"],
+  :six => ["Grand Central", "33rd", "28th_six", "23rd_six", "Union Square", "Astor Place"]
 }
+
+journey_start_stop = [] #array needs to exist to put the info into when we call method journey_start_stop
+journey_start_stop = get_stop_names
+
+
+
+
+
+#My HW
+# puts "Calculate how far are you going?"
+# puts $trains
+#   puts "What stop are you starting at?"
+#     start_name = gets.chomp
+#   puts "What stop are you getting of at?"
+#     ending_name = gets.chomp
+
+# how_many_stops(start_name, ending_name)
+# print "#{how_many_stops(start_name, ending_name)}"
+
+# puts "Will you need to transfer?"
+# get_stop_names
+# need_to_transfer?(start_name, ending_name)
