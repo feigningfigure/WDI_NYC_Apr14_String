78adcbd6bf52b1b0515694e87111260b4200dcdd
janespural <mlle.jihshin@gmail.com>
2014-04-23 20:25:40 -0400

    got first helper function to pass locally on n_line

--- w01/d03/Jane_Shin/mta/helper_functions.rb
+++ w01/d03/Jane_Shin/mta/helper_functions.rb
@@ -1,4 +1,4 @@
-def count_stops(start_name, ending_name)
+def how_many_stops(start_name, ending_name)
   # counts the stop between two on the same line
   #
   # takes two params:
@@ -7,6 +7,13 @@ def count_stops(start_name, ending_name)
   #
   # returns an integer equal to how many stops between two stops on the same line
   # if two stops are not on the same line, return nil
+  n_line = ["Times Square", "34th", "28th", "23rd", "14th", "8th"]
+  if n_line.include?(start_name) && n_line.include?(ending_name)
+    total = n_line.index(ending_name) - n_line.index(start_name)
+    return total.abs
+  else
+    return nil
+  end
 end
 
 def need_to_transfer?(start_name, ending_name)
