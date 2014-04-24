3987c5abba3334b97e49f6d956d004f0168db5c1
masewillis <mwillistang@gmail.com>
2014-04-24 10:22:13 -0400

    re-adding final mta homework

--- w01/d03/Mason_Tang/mta/mta.rb
+++ w01/d03/Mason_Tang/mta/mta.rb
@@ -10,7 +10,7 @@ last_station = gets.chomp
 
 
 $trains = {
-  :n_line => ["Time Square", "34th ", "28th", "23rd", "Union Square", "8th"],
+  :n_line => ["Time Square", "34th", "28th", "23rd", "Union Square", "8th"],
   :l_line => ["8th", "6th", "Union Square", "3rd", "1st"],
   :six_line => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"],
   :q_line => ["Time Square", "Herald Square", "Union Square", "Canal St"]
