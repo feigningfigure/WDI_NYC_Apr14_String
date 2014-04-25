6d739e6075474550799c8e58b45ea86dae6d7d1c
jpark1120 <parkj1120@gmail.com>
2014-04-25 15:31:47 -0400

    updated with instructor notes

---
+++ w01/d05/Joe_Park/scrumr/spec/scrumr_spec.rb
@@ -0,0 +1,36 @@
+require_relative 'spec_helper'
+
+describe "Scrumr" do
+
+  before :all do
+    $number_of_groups = rand(1..20)
+    puts "Random number #{$number_of_groups}"
+    $app.number_of_groups = $number_of_groups
+    # we know this is 6
+    $correct_group_size = $students.length/$number_of_groups
+  end
+
+  describe "#build_groups" do
+
+    before :all do
+      $app.build_groups
+    end
+
+    it "should create an array of Group objects" do
+      $app.groups.first.class.should be(Group)
+    end
+
+    it "should create the right number of groups" do
+      $app.groups.length.should eq($number_of_groups)
+    end
+  end
+
+  describe "#get_group_size" do
+
+    it "should return the number of students in each group based on a given number of groups" do
+      $app.get_group_size.should eq($correct_group_size)
+    end
+
+  end
+
+end
