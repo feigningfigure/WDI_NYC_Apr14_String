2b5f95daf85c4203448e4166a234a1e6c08767a0
zackn9ne <zackn9ne@gmail.com>
2014-04-25 17:09:32 -0400

    cehcking for jane

--- w01/d05/Zack_Levine/animal_shelter/classes/animal.rb
+++ w01/d05/Zack_Levine/animal_shelter/classes/animal.rb
@@ -1,3 +1,12 @@
-class Animal
+class Animal #start the class
+  attr_accessor :animal_name #setter/getter :animal_name is the symobl
 
+  def initialize() #initialize things
+    @animal_name = animal_name[] #@animal_name instance variable 
+  end
+
+  def
 end
+
+
+#symbol: creats the methos to assign and read the instance variable  
