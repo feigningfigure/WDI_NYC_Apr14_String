6eec9804ad55c27ad59449ff9fbbb4ebf0f5aa1b
omardelarosa <thedelarosa@gmail.com>
2014-04-25 11:34:45 -0400

    added students and stuff

--- w01/d05/INSTRUCTORS/scrumr/spec/student_spec.rb
+++ w01/d05/INSTRUCTORS/scrumr/spec/student_spec.rb
@@ -6,14 +6,20 @@ describe "Student" do
   # run this before all the tests
   before :all do
     $joe_hash = $students[0]
-    $joe = Student.new
+    $joe = Student.new($joe_hash)
   end
+
+  # expected behavior
+  it "should have a name" do
+    $joe.name.should eq($joe_hash["Name"])
+  end
+
   # method name
   describe "#to_s" do
-
     # expected behavior
-    it "should display 'My name is Joe Park" do
-      $joe.to_s.should eq("My name is Joe Park")
+    # better test...
+    it "should display the name of that student" do
+      $joe.to_s.should eq("My name is #{$joe_hash["Name"]}")
     end
 
   end
