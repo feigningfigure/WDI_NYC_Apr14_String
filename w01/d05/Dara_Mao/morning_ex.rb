d122c325583836bde162936b73b082c4cfc0829b
daramao <dara.mao@gmail.com>
2014-04-25 10:13:04 -0400

    Morning ex faker gem print multi names.

--- w01/d05/Dara_Mao/morning_ex.rb
+++ w01/d05/Dara_Mao/morning_ex.rb
@@ -3,17 +3,20 @@ require 'faker'
 class Person
   attr_reader :name
 
-  $array_name = []
-
-  def initialize(name=Faker::Name.name)
-    @name = name.to_s
-    puts "Hi My name is #{name}."
-    $array_name << Person.100times
-    puts $array_name
+  def initialize
+    @name = Faker::Name.name
   end
-  return initialize
+
+  def to_s
+    "Hi! My name is #{person.name}." #or @name; or self.name
+  end
+
 end
 
+arr = []
+100.times {arr << Person.new}
 
-
-
+arr.each do |person|
+  #puts person.to_s #to_s is the new method we created
+  puts "Hi name is #{person.name}" #or puts "Hi! My name is #{person.name}"
+end
