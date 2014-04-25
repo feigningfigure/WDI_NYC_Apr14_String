81efea8e53ede4f9096502e907ebe49a2eacd379
Mannymin <Mannymin@gmail.com>
2014-04-25 09:21:46 -0400

    homework d04

--- w01/d04/Emmanuel_Tucker/rentalapp/apartment.rb
+++ w01/d04/Emmanuel_Tucker/rentalapp/apartment.rb
@@ -3,12 +3,13 @@ class Apartment
 attr_accessor :price, :is_occupied :sqft, :num_beds, :num_baths, :renters
   # sets the starting state of the object
   def initialize(price, is_occupied, sqft, num_beds,num_beds, num_baths, renters)
-    @price = ["price"]
-    @is_occupied = ["is_occupied"]
-    @sqft = ["sqft"]
-    @num_beds = ["num_beds"]
-    @num_baths = ["num_baths"]
-    @renters = ["renters"]
+    @price = 100 # decimal
+    @is_occupied = true #boolean
+    @sqft = ["sqft"] 40
+    @num_beds = 4 #integer
+    @num_baths = 2 #integer
+    @renters = ["renters"] #array of person objects
 
   end
 end
+price.d
--- w01/d04/Emmanuel_Tucker/rentalapp/building.rb
+++ w01/d04/Emmanuel_Tucker/rentalapp/building.rb
@@ -3,10 +3,10 @@ class Building
 
   attr_accessor :, :name :age, :gender, :apartment,
   # sets the starting state of the object
-  def initialize(name, age, gender, apartment,)
+  def initialize(name, age, gender, apartment,nil)
       @address = ["address"]
       @style = ["style"]
       @has_doorman = ["has_doorman"]
-      @apartments = ["apartments"]
+      @apartments = Apartments.new
    end
  end
