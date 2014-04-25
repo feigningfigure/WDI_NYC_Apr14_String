f924cad419e772517fd5caa1a7fa7a4de9af4086
omardelarosa <thedelarosa@gmail.com>
2014-04-25 15:11:54 -0400

    added some grouping logic to scrumr

--- w01/d05/INSTRUCTORS/scrumr/classes/scrumr.rb
+++ w01/d05/INSTRUCTORS/scrumr/classes/scrumr.rb
@@ -9,6 +9,19 @@ class Scrumr
 
   def build_groups
     @groups = []
+
+    # while my DB of students still has stuff in it
+    while @students.length >= get_group_size
+      @groups << Group.new.add_students(@students.pop(get_group_size))
+    end
+
+    # get_group_size.times do |n|
+    #   group = Group.new
+    #   students_array = @students.pop()
+    #   group.add_students()
+    #   @groups <<
+    # end
+
   end
 
   def start
