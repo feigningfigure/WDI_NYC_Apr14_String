f37068afa257a7fe7ce39bfcc8cdd230406b0a8f
jbmilgrom <jbmilgrom@gmail.com>
2014-04-25 17:24:41 -0400

    initial animal shelter work

--- w01/d05/Jonathan_Milgrom/animal_shelter/classes/person.rb
+++ w01/d05/Jonathan_Milgrom/animal_shelter/classes/person.rb
@@ -1,3 +1,16 @@
 class Person
 
+	attr_accessor :name, :age, :gender, :kids
+
+	def initialize (name, age, gender, kids)
+		@name = name
+		@age = age
+		@gender = gender
+		@kids = kids
+	end
+
+	def to_s
+		"#{@name} is a #{@age} year old #{@gender} with 3 kids and 0 pets"
+	end
+
 end
