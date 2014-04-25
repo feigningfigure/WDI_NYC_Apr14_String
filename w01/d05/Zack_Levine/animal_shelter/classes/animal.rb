e9da0c4520346bf4c0b6471650acd5bc0dcb2507
zackn9ne <zackn9ne@gmail.com>
2014-04-25 17:24:07 -0400

    need

--- w01/d05/Zack_Levine/animal_shelter/classes/animal.rb
+++ w01/d05/Zack_Levine/animal_shelter/classes/animal.rb
@@ -1,12 +1,16 @@
 class Animal #start the class
-  attr_accessor :animal_name #setter/getter :animal_name is the symobl
+  attr_accessor :animal_name, :animal_species #setter/getter :animal_name is the symobl
 
   def initialize() #initialize things
-    @animal_name = animal_name[] #@animal_name instance variable 
+    @animal_name = animal_name_string #@animal_name instance variable 
+    @animal_species = animal_species_string
+    @animal_toys = [] #this is an empty array because it is able to getting multi. things
   end
 
   def
+
 end
 
 
-#symbol: creats the methos to assign and read the instance variable  
+#symbol: creats the method to assign and read the instance variable  
+#instance variable: @instance_variable because its specific to the class its in
