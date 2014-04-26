50f7b8ec73d060c31d0f81b347d6a8968fdd5f3c
LizCira <liz.cira.goldstein@gmail.com>
2014-04-26 18:39:28 -0400

    created animal class for happitails, added to_s instance method

--- w01/d05/Liz_Goldstein/animal_shelter/classes/animal.rb
+++ w01/d05/Liz_Goldstein/animal_shelter/classes/animal.rb
@@ -1,3 +1,33 @@
 class Animal
+attr_accessor :name, :species, :toys
+
+  def initialize(name, species)
+    @name = name
+    @species = species
+    @toys = []
+  end
+
+  def to_s
+    if @toys.empty? == false;
+      p "Hi my name is #{@name}, I'm a #{@species} and I love #{@toys.to_s}."
+      else
+      p "Hi my name is #{@name} and I'm a #{@species}."
+    end
+  end
+  #would be really good to turn the array in to a string and print it out pretty
+
+  def check_toys
+    puts "I have #{toys}"
+  end
 
 end
+
+# puts "Create an animal"
+# animal1 = Animal.new("lizzy", "sloth")
+
+# animal1.to_s
+# animal1.name = "Sara"
+# animal1.to_s
+# animal1.toys << "Food"
+# animal1.toys << "mice"
+# animal1.to_s
