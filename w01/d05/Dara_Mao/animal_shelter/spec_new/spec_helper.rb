819ec366a4980ddf9ed26a8a1d857e4bb937d9d2
daramao <dara.mao@gmail.com>
2014-04-26 16:25:43 -0400

    HW w01d05 Created animal_shelter classes: animal.rb person.rb shelter.rb

--- w01/d05/Dara_Mao/animal_shelter/spec_new/spec_helper.rb
+++ w01/d05/Dara_Mao/animal_shelter/spec_new/spec_helper.rb
@@ -1,13 +1,13 @@
 # For our specs to run, we need to require the Ruby classes we’re testing
-require_relative '../classes/animal'
-require_relative '../classes/shelter'
-require_relative '../classes/person'
+require_relative '../animal'
+require_relative '../shelter'
+require_relative '../client'
 
 # Configure RSpec
 RSpec.configure do |config|
   # Use color in STDOUT
   config.color_enabled = true
-
+  config.order = :random
   # Use color not only in STDOUT but also in pagers and files
   config.tty = true
 
