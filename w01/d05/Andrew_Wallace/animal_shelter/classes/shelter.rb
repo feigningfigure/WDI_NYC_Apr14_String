73723a5911eb6cf96e4451f825109e9e1614ddad
feigningfigure <andrew.g.wallace@gmail.com>
2014-04-27 13:13:24 -0400

    Have happitails app pass rspec tests

--- w01/d05/Andrew_Wallace/animal_shelter/classes/shelter.rb
+++ w01/d05/Andrew_Wallace/animal_shelter/classes/shelter.rb
@@ -1,3 +1,41 @@
 class Shelter
+	attr_accessor :name, :address, :animals, :clients
 
+	def initialize(name, address)
+		@name = name
+		@address = address
+		@animals = {}
+		@clients = {}
+	end
+
+	def to_s
+		"#{@name} shelter at #{@address} has #{@animals.length} animals and #{@clients.length} people"
+	end
+
+	def display_clients
+		client_string = []
+		@clients.each do |k, v|
+		 	client_string << v.to_s 
+		end
+		"#{client_string.join(" \n")}"
+	end
+
+	def display_animals
+		animal_string = []
+		@animals.each do |k, v|
+		 	animal_string << v.to_s 
+		end
+		"#{animal_string.join(" \n")}"
+	end
+
+	def create_animal(name, age, gender, species, toys)
+		name = Animal.new(name, age, gender, species)
+		# unless toys.empty?
+		# 	toys.each do |toy|
+		# 		<< toy
+
+		@animals[@animal_name.name.to_sym] = @animal_name
+
+	end
 end
+
