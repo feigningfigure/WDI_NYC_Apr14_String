46a09803883d50acd61bc4cb04f30dfbd18d1e12
keyanbagheri <keyanbagheri@gmail.com>
2014-04-28 09:01:03 -0400

    hmwk w01d05 also run smoothie.rb for a fun time

--- w01/d04/Keyan_Bagheri/smoothie/blender.rb
+++ w01/d04/Keyan_Bagheri/smoothie/blender.rb
@@ -1,6 +1,3 @@
-require 'rainbow'
-require_relative 'shuffle'
-
 class Blender
 
 	attr_accessor :is_on, :ingredients, :chopped_ingredients, :ingredients1
@@ -16,7 +13,6 @@ class Blender
 		if self.is_on == false
 			alert_power_required = "In order to operate you must first turn on the blender\n\n"
 			puts alert_power_required
-			system "say #{alert_power_required}"
 			false
 		else
 			true
@@ -36,29 +32,70 @@ class Blender
 			alert_power_down = "The blender is powering down\n\n"
 			puts alert_power_down
 			system "say #{alert_power_down}"
+			puts "Goodbye"
+			system "say Good"
+			system "say Bye"
+			puts
 		end
+	end
+
+	def chop_up_ingredient(ingredient_name)
+		ingredient_name = ingredient_name.gsub(' ','')
+		letter_array = Array.new
+		letter_array = ingredient_name.split('')
 	end
 
 	def add(ingredient)
-		@ingredients << ingredient
-		@chopped_ingredients << chop_up_ingredient(ingredient)
+		if @ingredients1.include?(ingredient) || ingredient == ""
+			return
+		else
+			@ingredients << color_the_ingredient(color_match(ingredient),ingredient.upcase)
+			@ingredients1 << ingredient.upcase
+			choppy = chop_up_ingredient(ingredient.upcase)
+			@chopped_ingredients << choppy.map { |letter| color_the_ingredient(color_match(ingredient),letter)}
+		end
 	end
 
 	def analyze_ingredients
-		if @is_on == true && @ingredients.length > 0
+		if @is_on == true && @ingredients.length > 4
 			alert_analyzing =  "Analyzing ingredients\n\n"
 			puts alert_analyzing
 			system "say #{alert_analyzing}"
-			sleep(1)
-			alert_count = "1.. #{self.ingredients[0]}.. 2.. #{self.ingredients[1]}.. 3.. Is that a hint of #{self.ingredients[2]}?. 4.. #{self.ingredients[3]}.. You know what. I am just going to start now.\n\n"
-			puts alert_count
-			system "say 1.. #{self.ingredients1[19]}.. 2.. #{self.ingredients1[3]}.. 3.. Is that a hint of #{self.ingredients1[1]}?. 4.. #{self.ingredients1[0]}.. You know what. I am just going to start now.\n\n"
+			puts
+			sleep(1.0/2.0)
+			puts "Ingredients:"
+			sleep(1.0/2.0)
+			puts	
+			puts "#{self.ingredients[0]}"
+			system "say 1.. #{self.ingredients1[0]}.."
+			puts "#{self.ingredients[1]}"
+			system "say 2.. #{self.ingredients1[1]}.."
+			puts "#{self.ingredients[2]}"
+			system "say 3.. Is that a hint of #{self.ingredients1[2]}?."
+			system "say 4.. What is that?"
+			puts "#{self.ingredients[3]}"
+			system "say #{self.ingredients1[3]}?"
+			system "say fa You know what. I am just going to start now.\n\n"
+			puts @ingredients[4..-1]
+			puts
+		else
+			alert_analyzing =  "Analyzing ingredients\n\n"
+			puts alert_analyzing
+			system "say #{alert_analyzing}"
+			puts
+			sleep(1.0/2.0)
+			puts "Ingredients:"
+			sleep(1.0/2.0)
+			puts
+			puts @ingredients
+			system "say #{@ingredients1}"
+			puts
 		end
 	end
-	
+
 	def has_ingredients?
-		if self.ingredients == []
-			alert_insert = "Please insert ingredients and press start button when ready.\n\n"
+		if @ingredients.length < 1
+			alert_insert = "Please insert ingredients and press start button when ready\n\n"
 			puts alert_insert
 			system "say #{alert_insert}"
 			false
@@ -68,74 +105,78 @@ class Blender
 	end
 
 	def start_button
-		alert_activate = "One minute please\n\n"
-		puts alert_activate
-		system "say #{alert_activate}"
-		sleep(1)
+		if blender_is_on? == false
+		elsif has_ingredients? == false
+		else
+			alert_activate = "One minute please\n\n"
+			puts alert_activate
+			system "say #{alert_activate}"
+			sleep(1.0/2.0)
+			blend
+		end
 	end
 
 	def poor_into_cup(smoothie)
-			blended = smoothie.shuffle
-			blended1 = blended[0..19].join
-			blended2 = blended[20..39].join
-			blended3 = blended[40..59].join
-			blended4 = blended[60..79].join
-			blended5 = blended[80..99].join
-			blended6 = blended[100..119].join
-			blended7 = blended[120..139].join
-			blended8 = blended[140..159].join
-			blended9 = blended[160..179].join
-			blended10 = blended[180..199].join
-			blended11 = blended[200..211].join
-			puts "          #{blended[110..114].join}"
-			puts "          #{blended[212].to_s}"
-			puts "          #{blended[213].to_s}"
-			puts "          #{blended[42].to_s}"
-			puts "       #{blended[100..107].join}"
-			puts "     #{blended11}"
-			puts "   #{blended[50..65].join}"
-			puts "  #{blended[110..127].join}"
-			puts " #{blended5}"
-			puts "#{blended[170..191].join}"
-			puts "#{blended[130..151].join}"
-			puts " #{blended1}"
-			puts " #{blended2}"
-			puts " #{blended3}"
-			puts " #{blended4}"
-			puts " #{blended5}"
-			puts " #{blended6}"
-			puts " #{blended7}"
-			puts " #{blended8}"
-			puts " #{blended9}"
-			puts " #{blended10}"
-			puts
+		until smoothie.length >= 214
+			smoothie += smoothie
 		end
+		blended = smoothie.flatten.shuffle
+		blended1 = blended[0..19].join
+		blended2 = blended[20..39].join
+		blended3 = blended[40..59].join
+		blended4 = blended[60..79].join
+		blended5 = blended[80..99].join
+		blended6 = blended[100..119].join
+		blended7 = blended[120..139].join
+		blended8 = blended[140..159].join
+		blended9 = blended[160..179].join
+		blended10 = blended[180..199].join
+		blended11 = blended[200..211].join
+		puts "            #{blended[110..114].join}"
+		puts "            #{blended[212].to_s}"
+		puts "            #{blended[213].to_s}"
+		puts "            #{blended[42].to_s}"
+		puts "         #{blended[100..107].join}"
+		puts "       #{blended11}"
+		puts "     #{blended[50..65].join}"
+		puts "    #{blended[110..127].join}"
+		puts "   #{blended5}"
+		puts "  #{blended[170..191].join}"
+		puts "  #{blended[130..151].join}"
+		puts "   #{blended1}"
+		puts "   #{blended2}"
+		puts "   #{blended3}"
+		puts "   #{blended4}"
+		puts "   #{blended5}"
+		puts "   #{blended6}"
+		puts "   #{blended7}"
+		puts "   #{blended8}"
+		puts "   #{blended9}"
+		puts "   #{blended10}"
+		puts
+	end
 
 
 
 	def blend
-		if blender_is_on? == false
-		elsif has_ingredients? == false
-		else
-			start_button
-			analyze_ingredients
-			alert_begin = "Now blending\n\n"
-			puts alert_begin
-			system "say #{alert_begin}"
-			system "say wurwurwurrwurwurwurwurrwurwur"
-			system "say erum"
-			system "say wrrwurwurrwurwurwurwurrwurwurwur"
-			alert_close = "Almost finished\n\n"
-			puts alert_close
-			system "say #{alert_close}"
-			system "say wurwurwurrwurwurwurwurwurwurboop"
-			puts
-			sleep(1/2)
-			alert_done = "Heres your freaking smoothie!\n\n"
-			puts alert_done
-			system "say #{alert_done}"
-			poor_into_cup(@chopped_ingredients)
-			sleep(2)
-		end
+		analyze_ingredients
+		alert_begin = "Now blending\n\n"
+		puts alert_begin
+		system "say #{alert_begin}"
+		system "say wurwurwurrwurwurwurwurrwurwur"
+		system "say erum"
+		system "say wrrwurwurrwurwurwurwurrwurwurwur"
+		alert_close = "Almost finished\n\n"
+		puts alert_close
+		system "say #{alert_close}"
+		system "say wurwurwurrwurwurwurwurwurwurboop"
+		puts
+		sleep(1.0/2.0)
+		alert_done = "Heres your freaking smoothie!\n\n"
+		puts alert_done
+		system "say #{alert_done}"
+		poor_into_cup(@chopped_ingredients)
+		sleep(1)
 	end
 end
+
--- w01/d04/Keyan_Bagheri/smoothie/smoothies_starter.rb
+++ w01/d04/Keyan_Bagheri/smoothie/smoothies_starter.rb
@@ -1,6 +1,4 @@
-require_relative 'blender'
-require_relative 'shuffle'
-require 'rainbow'
+require_relative 'helper'
 
 # docs you may enjoy
 # http://www.ruby-doc.org/core-2.0/Hash.html
@@ -25,7 +23,7 @@ smoothie_ingredients = {
   'brussel sprouts' => '1/4 cup',
   'asparagus' => '1/4 cup',
   'kale' => '1/4 cup',
-  'brocoli rabe' => '1/4 cup',
+  'broccoli rabe' => '1/4 cup',
   'blue berries' => '1/4 cup',
   'chopped banana' => '1/2 cup',
   'straw berries' => '1/4 cup',
@@ -34,31 +32,14 @@ smoothie_ingredients = {
 }
 betsy = Blender.new
 
-smoothie_ingredients.each { |ingredient, amount| betsy.add(ingredient) }
-betsy.ingredients1 = betsy.ingredients
-
-def color_ingredients(ingredients)
-collection = []
-collection << color_the_ingredient("red",ingredients[19])
-collection << color_the_ingredient("blue",ingredients[3])
-collection << color_the_ingredient("cyan",ingredients[1])
-collection << color_the_ingredient("white",ingredients[0],ingredients[21],ingredients[2],ingredients[8],ingredients[9],ingredients[11])
-collection << color_the_ingredient("yellow",ingredients[20],ingredients[5],ingredients[7],ingredients[12],ingredients[4],ingredients[18])
-collection << color_the_ingredient("green",ingredients[16],ingredients[13],ingredients[14],ingredients[6],ingredients[10])
-collection << color_the_ingredient("magenta",ingredients[15],ingredients[17])
-collection.flatten
-end
-
-betsy.ingredients = color_ingredients(betsy.ingredients)
-betsy.chopped_ingredients = color_ingredients(betsy.chopped_ingredients)
-
-betsy.blend
+puts
+betsy.start_button
 betsy.turn_on_blender
-betsy.blend
+betsy.start_button
+
+# smoothie_ingredients.each { |ingredient, amount| betsy.add(ingredient) }
+
+betsy.add("Strawbery")
+
+betsy.start_button
 betsy.turn_off_blender
-
-# Write a function called blend.
-# It should take all the smoothie ingredients (not the measurements) and chop up and mix all the characters
-# and output a mixed string of characters
-# Be sure to remove the spaces, as we don't want any air bubbles in our smoothie!
-
