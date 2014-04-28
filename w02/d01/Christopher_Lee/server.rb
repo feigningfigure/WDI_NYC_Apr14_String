81e0ca534415f09acc71787d932fabddad7259ca
ChrisMLee <cristovao.m.lee@gmail.com>
2014-04-28 15:20:27 -0400

    adding server class exercise

--- w02/d01/Christopher_Lee/server.rb
+++ w02/d01/Christopher_Lee/server.rb
@@ -7,5 +7,28 @@ get "/" do
 end
 
 get "/:name" do
-  params[:name]
+  "Hello, my name is #{params[:name]}"
+end
+
+get "/:name/:meal" do
+  "I'm #{params[:name]} and I love #{params[:meal]}"
+end
+
+get "/:operator/:number/:number2" do
+  number1 = params[:number].to_i
+  number2 = params[:number2].to_i
+
+  case params[:operator]
+    when "add"
+      "The answer is #{number1 + number2}"
+    when "minus"
+      "The answer is #{number1 - number2}"
+    when "multiply"
+      "The answer is #{number1 * number2}"
+    when "divide"
+      "The answer is #{number1 / number2}"
+    else
+      "ERROR: Stop trying to do fancy stuff!"
+  end
+
 end
