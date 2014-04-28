56921a4d41c9d2344772d6717dfbe25521b7206d
slaffont <slaffont@gmail.com>
2014-04-28 16:33:01 -0400

    day 1 week 2

--- w02/d01/Sophie_Laffont/http_basics/server.rb
+++ w02/d01/Sophie_Laffont/http_basics/server.rb
@@ -1,37 +1,11 @@
 require 'sinatra'
-require 'sinatra/reloader'
-
-
+require 'httparty'
 
 # simple request GET http://www.google.com/users
 # document = HTTParty.get("http://www.google.com/users")
 # puts document
 
 # HTTP VERB + URI Pattern + Block of Code
-#  localhost:4567
-# get "/" do
-#   "<h1>Hello World!</h1><p>This should be smaller</p>"
-# end
-
-# params is an invisible hash that you can manipulate
-#it creates a key
-get '/:operation/:num1/:num2' do
-
-  case params[:operation]
-
-  when "add"
-  "the answer is #{params[:num1].to_i+params[:num2].to_i}"
-
-    when "subtract"
-      "the answer is #{params[:num1].to_i - params[:num2].to_i}"
-
-    when "multiply"
-      "the answer is #{params[:num1].to_i * params[:num2].to_i}"
-
-    when "divide"
-      .to_i"the answer is #{params[:num1].to_i / params[:num2].to_i}"
-    end
+get "/" do
+  "<h1>Hello World!</h1><p>This should be smaller</p>"
 end
-
-
-
