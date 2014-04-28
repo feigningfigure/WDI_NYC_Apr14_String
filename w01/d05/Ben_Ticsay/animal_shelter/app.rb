9c6c19ef5460ee65dc23a913b994b798947fd88e
benticsay <benticsay@gmail.com>
2014-04-28 09:00:52 -0400

    E and F added

--- w01/d05/Ben_Ticsay/animal_shelter/app.rb
+++ w01/d05/Ben_Ticsay/animal_shelter/app.rb
@@ -76,10 +76,44 @@ def menu
     puts "Client has been added"
    end
 
-  when "E"
-    # Adopt Animal
+when "E"
+    puts "What was your HappiTails registration name:"
+    adopter = gets.chomp
+      if @happitails.clients.include?(adopter)==true
+        puts "Here's what we've got:"
+        @happitails.display_animals
+        puts "See something you like?"
+        get_that_pet = gets.chomp
+        if @happitails.animals.include?(get_that_pet)==true
+          @happitails.clients[adopter].accept_pet(get_that_pet, adopter)
+        puts "Congrats! #{get_that_pet} is yours!"
+        @happitails.give_away_animal(get_that_pet)
+        else
+        puts "sorry I dont think we have that pet"
+        end
+      else
+        puts "Please register"
+    end
+  menu
+
   when "F"
-    # Put Animal Up For Adoption
+    puts "What was your HappiTails registration name:"
+    owner = gets.chomp
+    if @happitails.clients.include?(owner)==true
+      puts "So who are you giving up?"
+      #commented out code here is attempt input control
+      @happitails.clients[owner].display_pets
+      goner = gets.chomp
+      # if @happitails.clients[owner[pets]].include?(goner)
+      @happitails.clients[owner].give_away_pet(goner)
+      @happitails.accept_animal(goner, goner)
+        puts "We've got your pet #{goner}!"
+        # @happitails.clients[owner]
+      else
+        puts "Please register"
+      end
+
+
   when "Q"
     Kernel.exit
   end
