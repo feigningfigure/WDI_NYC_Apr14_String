7abf7a32265ea518d606149f7ee69aa54eca8106
jpark1120 <parkj1120@gmail.com>
2014-04-29 16:30:24 -0400

    html and css made by me

--- w02/d02/Joe_Park/dynamic_html_sinatra/main.rb
+++ w02/d02/Joe_Park/dynamic_html_sinatra/main.rb
@@ -1,15 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+def make_heading(user_name)
+  # this is a function that generates HTML strings
+  "<h1>Welcome #{user_name}!</h1>"
+end
+
+def make_profile(favorite_color)
+  "<p>His favorite color is #{favorite_color}</p>"
+end
+
 get '/' do
   redirect "/index.html"
 end
 
-get '/wow' do
-  "Wow!"
+get '/home' do
+  "#{make_heading(params[:user_name])}#{make_profile(params[:favorite_color])}"
 end
-
-get '/bad_link' do
-  "<a href='wow'>bad link</a>"
-end
-
