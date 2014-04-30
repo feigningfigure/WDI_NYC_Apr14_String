4b296ed4293098891c229f96b2ba146ee06f69fe
janespural <mlle.jihshin@gmail.com>
2014-04-30 18:49:43 -0400

    add erb lecture notes, hw in stock_app folder

--- w02/d03/Jane_Shin/erb_notes/app.rb
+++ w02/d03/Jane_Shin/erb_notes/app.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 get '/' do
 	erb :index
 end
 
-get '/:name' do
-	@name = params[:name]
+get '/name' do
+	@name = params[:user_name]
 	erb :name
 end
