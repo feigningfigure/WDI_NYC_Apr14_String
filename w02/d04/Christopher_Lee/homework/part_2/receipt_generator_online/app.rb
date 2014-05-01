979719b3ecf1cec8c41ebc7ad88fe6cc680f4c0e
ChrisMLee <cristovao.m.lee@gmail.com>
2014-05-01 19:50:44 -0400

    Finished Part 2, now I need to make it pretty

--- w02/d04/Christopher_Lee/homework/part_2/receipt_generator_online/app.rb
+++ w02/d04/Christopher_Lee/homework/part_2/receipt_generator_online/app.rb
@@ -1,16 +1,41 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 get '/' do
   "Hello World!"
   erb :index
 end
 
+get '/last_receipt' do
+  receipt_file = File.open("./db/receipt_db.txt", "r")
+  receipt_array = receipt_file.readlines.last(10) #testing this
+  @last_receipt = receipt_array[0..4]
+  receipt_file.close
+  erb :last_receipt
+end
 
 
-post '/students' do
-  redirect "/students"
 
+get '/print_receipt' do
+  receipt_file = File.open("./db/receipt_db.txt", "r")
+  receipt_array = receipt_file.readlines
+  @final_receipt = receipt_array.last(5)
+  receipt_file.close
+  erb :print_receipt
+end
 
+post '/print_receipt' do
+  cost_of_goods = params[:cost_of_goods]
+  customer_name = params[:customer_name]
+  receipt_file = File.open("./db/receipt_db.txt", "a")
+  receipt_file.write"
+    Thank you #{customer_name}
+    Company Name: Consolidated Industries
+    Good/Service Provided: Cheetos
+    Cost: #{cost_of_goods}
+    Thank you for your patronage"
+  receipt_file.close
+  redirect "/print_receipt"
 end
 
