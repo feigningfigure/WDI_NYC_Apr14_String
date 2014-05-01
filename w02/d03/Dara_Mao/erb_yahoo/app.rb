59951e38dd83fbeca1cfc083bba83b7db2cf9261
daramao <dara.mao@gmail.com>
2014-05-01 09:01:43 -0400

    hw w02d03 implemented bonus challenge error message

--- w02/d03/Dara_Mao/erb_yahoo/app.rb
+++ w02/d03/Dara_Mao/erb_yahoo/app.rb
@@ -8,25 +8,30 @@ get '/' do
 end
 
 get '/name' do
-  name = params[:user_input]
+  name = params[:user_input].upcase
   @full_list = stocklookup(name)
-  if @full_list.lastTrade.to_f == 0.0
-    "Please Enter A Valid Stock Symbol!"
+  #Method 1:
+  if @full_list.date.to_s == "N/A"
+    @error = true
   else
-    @full_list = stocklookup(name)
+    @error =false
   end
+
+ #Method 2:
+  # @full_list.each do |key,value|
+  #   key.include? 'nil'
+   # "Please Enter A Valid Stock Symbol!"
+  # end
+    # if @full_list.lastTrade == 0.0
+
   erb :name
 end
-
-# def stocklookup(name)
-#  YahooFinance::get_standard_quotes(name)[name]
-# end
 
 def stocklookup(name)
  YahooFinance::get_standard_quotes(name)[name]
 end
 
-
+#Error message: Testing Method #3 using array.include? for symbols lookup
   # if $Symbol.include? != name
   #   "PLEASE ENTER A VALID SYMBOL!!"
   # else
