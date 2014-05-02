9cc3d72df45c40ad8bb7c13a15be566113702de7
keyanbagheri <keyanbagheri@gmail.com>
2014-05-02 14:58:38 -0400

    myspace midway

--- w02/d05/Keyan_Bagheri/myspace/main.rb
+++ w02/d05/Keyan_Bagheri/myspace/main.rb
@@ -6,8 +6,16 @@ require 'json'
 
 get '/' do
 
+	erb :index
 end
 
 get '/friend' do
+	username = params[:username]
 
+	url = "http://graph.facebook.com/#{username}"
+	friend_json = HTTParty.get(url)
+	friend = JSON(friend_json)
+
+	binding.pry
 end
+
