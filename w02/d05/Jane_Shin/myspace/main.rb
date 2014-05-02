4eaa2527fd127346e25dc166bd67536b0d8facce
janeplural <mlle.jihshi@gmail.com>
2014-05-02 16:57:23 -0400

    myspace app

--- w02/d05/Jane_Shin/myspace/main.rb
+++ w02/d05/Jane_Shin/myspace/main.rb
@@ -7,3 +7,18 @@ require 'json'
 get '/' do
 	erb :index
 end
+
+get '/friend' do
+	username = params[:username]
+	url = "http://graph.facebook.com/#{username}"
+	friend_json = HTTParty.get(url)
+	@friend = JSON(friend_json)
+
+#strict evalution, boolean conditional is very common
+	if @friend["error"]
+		@message = "ERROR: Friend not found!"
+		erb :index
+	else
+		erb :friend
+	end
+end
