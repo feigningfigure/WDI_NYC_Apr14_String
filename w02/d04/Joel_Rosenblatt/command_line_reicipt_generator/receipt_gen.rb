7de803ff3adde57b257298fd863c0e8c737c1e68
KLIMTHAUS <jojoel10@gmail.com>
2014-05-02 09:40:32 -0400

    hw w02 d04

---
+++ w02/d04/Joel_Rosenblatt/command_line_reicipt_generator/receipt_gen.rb
@@ -0,0 +1,14 @@
+puts "What is the name of the company?"
+company = gets.chomp
+
+puts "What good or service was provided?"
+good = gets.chomp
+
+puts "What was the cost of the good/service?"
+price = gets.chomp
+
+puts ""
+puts "-Company Name: #{company}"
+puts "-Good/Service Provided: #{good}"
+puts "-Cost: $#{price}"
+puts "-Thank you for your patronage"
