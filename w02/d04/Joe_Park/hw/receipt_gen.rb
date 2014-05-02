21eb6217e0ed283f1c2999a06016aa50136a1ba7
jpark1120 <parkj1120@gmail.com>
2014-05-02 07:55:49 -0400

    made adjustments to receipt_gen.rb

--- w02/d04/Joe_Park/hw/receipt_gen.rb
+++ w02/d04/Joe_Park/hw/receipt_gen.rb
@@ -1,34 +1,60 @@
-class Receipt
+class Register
 
-	attr_accessor :name, :price
+	attr_accessor :company
 
 
-	def initialize(name, price)
-		@name = name
-		@price = price
+	def initialize(company)
+		@company = company
 		@receipt = {}
 	end
 
 	def to_s
-		puts "Company Name: Particular Assembly Inc.\nGood/Services Provided: #{@name}\nCost: $#{price}\nThank you for your patronage"
+		# puts "Company Name: Particular Assembly Inc.\nGood/Services Provided: #{@name}\nCost: $#{price}\nThank you for your patronage"
+
+		puts "Company Name: #{@company}"
+		puts "Good/Services Provided: #{@item}"
+		puts "Cost: $#{@price} Thank you for your patronage"
+		puts "Thank you for your patronage!"
+
 	end
 
 
-	def add_to_receipt(item, cost)
-		@receipt[item] = cost
+	def add_to_receipt
+		puts "Enter Commodity: "
+		@item = gets.chomp.downcase
+
+		puts "Enter Price: "
+		@price = gets.chomp.to_i
+
+		@receipt[@item] = @price
+
+		puts "Did you purchase anything else? (y)es or (n)o"
+		@answer = gets.chomp.downcase
+
+		if @answer == "y" 
+			add_to_receipt
+		elsif @answer == "n"
+			@receipt.to_s
+		else
+			puts "Error!"
+		end
+
 	end
 
 end
 
+Geico = Register.new("Geico")
+Geico.add_to_receipt
 
-puts "Enter Commodity: "
-item = gets.chomp.downcase
 
-puts "Enter Price: "
-price = gets.chomp.to_i
+# puts "Enter Commodity: "
+# item = gets.chomp.downcase
 
-particular_assembly_inc = Receipt.new(item, price)
-particular_assembly_inc.to_s
+# puts "Enter Price: "
+# price = gets.chomp.to_i
+
+# particular_assembly_inc = Receipt.new(item, price)
+# particular_assembly_inc.to_s
 
 # puts "Did you purchase anything else? (y)es or (n)o"
 # answer = gets.chomp.downcase
