fde79078370a91c00098cdfda80b2617becb7409
slaffont <slaffont@gmail.com>
2014-05-04 18:08:48 -0400

    sunday

--- w02/d05/Sophie_Laffont/myspace/main.rb
+++ w02/d05/Sophie_Laffont/myspace/main.rb
@@ -9,20 +9,21 @@ get '/' do
 	erb :index
 end
 
-
-# we set the key in index.erb. once the program knows what the key is, it knows how to assign the value
-# it will make the key and value
-# http://localhost:4567/friend?username=sophie+laffont
-# in this original before any formatting. url the key is username and the value is sophie laffont
-
 get '/friend' do
 	username = params[:username]
-	# in this case we would have to type exactly the user's name
+
 	url = "http://graph.facebook.com/#{username}"
-	# this will return a json object we need to parse
-	friend_json = HTTParty.get(url)
-	friend = JSON(friend_json) # creating a hash
+	friend_json = HTTParty.get(url) 
+	@friend = JSON(friend_json) 
 
-	binding.pry
+	profile_picture = HTTParty.get("http://graph.facebook.com/#{username}?fields=picture")
+  @profile_picture_hash = JSON(profile_picture)
+
+	if @friend["error"]
+		@message = @friend["error"]["message"]
+		erb :index
+	else
+		erb :friend 
+	end
+
 end
-
