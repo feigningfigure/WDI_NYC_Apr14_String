43430ea252275d0d4a92fc17b23c5d785c5bd782
neelp22 <neelp22@gmail.com>
2014-05-05 15:41:21 -0400

    finished all CRUD functions

--- w03/d01/INSTRUCTORS/ruby_and_pg_gem/main.rb
+++ w03/d01/INSTRUCTORS/ruby_and_pg_gem/main.rb
@@ -8,6 +8,42 @@ def add_student(first_name, last_name, birthdate)
 	@conn.exec("INSERT INTO students (first_name, last_name, birthdate) VALUES ('#{first_name}', '#{last_name}', '#{birthdate}');")
 end
 
+# SELECT * FROM students;
+
+def display_students
+	all_students = @conn.exec("SELECT * FROM students;")
+	all_students.each do |student|
+		student.each do |k, v|
+			puts "#{k}: #{v}"
+		end
+	end
+end
+
+# SELECT * FROM students WHERE first_name = '';
+
+def search(category, query_term)
+	result = @conn.exec("SELECT * FROM students WHERE #{category} = '#{query_term}';")
+	result.each do |student|
+		student.each do |k, v|
+			puts "#{k}: #{v}"
+		end		
+	end
+end
+
+# UPDATE students SET first_name = '' WHERE id = '';
+
+def update(category, query_term, id)
+	@conn.exec("UPDATE students SET #{category} = '#{query_term}' WHERE id = '#{id}';")
+	puts "Your record has been updated"
+end
+
+# DELETE FROM students WHERE id = '';
+
+def delete(id)
+	@conn.exec("DELETE FROM students WHERE id = '#{id}';")
+	puts "Your record has been deleted"
+end
+
 
 
 
