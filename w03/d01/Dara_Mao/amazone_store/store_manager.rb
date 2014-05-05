f7d34768fd7fb78fd6b17fd556b821e5c22c8f66
daramao <dara.mao@gmail.com>
2014-05-05 18:48:12 -0400

    hw w03d01 amazone_store updated and search functions completed

--- w03/d01/Dara_Mao/amazone_store/store_manager.rb
+++ w03/d01/Dara_Mao/amazone_store/store_manager.rb
@@ -1,30 +1,81 @@
 require 'pg'
+require 'pry'
 
 @conn = PG.connect(dbname: 'amazone_store')
 
-# Requirement 1: A user should be able to create new products using a command-line interface.
-#User Interface for the "create" part:
-puts "What is your product called?"
-prod_name = gets.chomp
-puts "What is its price?"
-price = gets.chomp.to_f
-puts "What is the description of your product?"
-description = gets.chomp
-puts "You created #{prod_name} #{description}, that costs $#{price}"
+# # Requirement 1: A user should be able to create new products using a command-line interface.
+# #User Interface for the "create" part:
+# puts "What is your product called?"
+# prod_name = gets.chomp
+# puts "What is its price?"
+# price = gets.chomp.to_f
+# puts "What is the description of your product?"
+# description = gets.chomp
+# puts "You created #{prod_name} #{description}, that costs $#{price}"
+# #TO-DO: need to add #id after add_products is called back to the output above
 
-# Writing part: INSERT INTO products (prod_name, price, description) VALUES ();
-def add_product(prod_name, price, description)
-  @conn.exec("INSERT INTO products (prod_name, price, description) VALUES ('#{prod_name}', '#{price}', '#{description}');")
+
+# # # Writing part: INSERT INTO products (prod_name, price, description) VALUES ();
+# # def add_product(prod_name, price, description)
+# #   @conn.exec("INSERT INTO products (prod_name, price, description) VALUES ('#{prod_name}', '#{price}', '#{description}');")
+# # end
+
+# # add_product(prod_name, price, description)
+
+#===================================================================================
+# #Requirement 2: A user should be able to search for a product by name or id
+#User Interface for the "search" part:
+# puts "Do you wish to search by 'id' or 'prod_name'?"
+# category = gets.chomp
+# puts "What is your product called?"
+# query_term = gets.chomp
+# puts "Here are all the products that matched your search:"
+
+# # #Search part: SELECT * FROM products WHERE #{category} = '#{query_term}';
+# def search(category, query_term)
+#   update = @conn.exec("SELECT * FROM products WHERE #{category} = '#{query_term}';")
+#   result = @conn.exec("SELECT * FROM pg_stat_activity") do |result|
+#     puts "| id | name      | price | description          | quantity
+# |---------------------------------------------------------|"
+#           result.each do |row|
+#             puts " %s" %
+#             row.values_at("id", "prod_name", "price", "description", "quantity")
+#   end
+# end
+# end
+# search(category, query_term)
+#TO-DO: need to display the "SELECT * FROM products;" from psql to bash
+
+# def display
+#    @display_screen = @conn.exec("SELECT * FROM products;")
+#  end
+
+#  @display_screen
+#===================================================================================
+#Requirement 3: A user should be able to "update" the name, price and description of a product without updating it's quantity at the same time.
+#User Interface for the "update" part:
+puts "What is the id of the product do you wish to edit?"
+id = gets.chomp
+puts "What is old_prod_names new name?"
+prod_name = gets.chomp
+puts "What is old_prod_names new description?"
+description = gets.chomp
+#TO-DO: need to consider blank space =nil
+puts "What is old_prod_names new price?"
+price = gets.chomp.to_f
+puts "You updated #{prod_name} #{id}, #{description}, that costs $#{price}"
+
+def update(prod_name, description, price, id)
+  @conn.exec("
+    UPDATE products
+      SET prod_name = '#{prod_name}',
+          description = '#{description}',
+          price = #{price}
+      WHERE id = '#{id}';
+      ")
 end
 
-add_product(prod_name, price, description)
+update(prod_name, description, price, id)
 
-#Requirement 2: A user should be able to search for a product by name or id
-#User Interface for the "search" part:
-puts "Do you wish to search by 'id' or 'name'?"
-choice = gets.chomp
-puts "What is your product called?"
-prod_name = gets.chomp
-puts "Here are all the products that matched your search:"
-#displays all the products
-
+#===================================================================================
+#Requirement 4: A user should be able to "order" products by incrementing the quantity of a given product by some number N
