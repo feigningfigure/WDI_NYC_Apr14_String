a4eeb8588cf1c69af3df725f2787e7522dc9d5a7
jbmilgrom <jbmilgrom@gmail.com>
2014-05-05 16:57:11 -0400

    notes

--- w03/d01/Jonathan_Milgrom/ruby_and_pg_gem/main.rb
+++ w03/d01/Jonathan_Milgrom/ruby_and_pg_gem/main.rb
@@ -1,11 +1,12 @@
 require 'pg'
+require 'faker'
 
 @conn = PG.connect(dbname: 'students_db')
 
 # INSERT INTO students (first_name, last_name, birthdate) VALUES ();
 
 def add_student(first_name, last_name, birthdate)
-	@conn.exec("INSERT INTO students (first_name, last_name, birthdate) VALUES ('#{first_name}', '#{last_name}', '#{birthdate}');")
+	@conn.exec("INSERT INTO students (first_name, last_name, brithdate) VALUES ('#{first_name}', '#{last_name}', '#{birthdate}');")
 
 end
 
@@ -20,11 +21,28 @@ def display_students
 end
 
 # SELECT * FROM students WHERE first_name = '';
-def search(first_name)
-	result = @conn.exec("SELECT * FROM students WHERE first_name = '#{first_name}';")
+def search(category, query_term)
+	result = @conn.exec("SELECT * FROM students WHERE #{category} = '#{query_term}';")
 	result.each do |student|
 		student.each do |k, v|
 			puts "#{k}: #{v}"
 		end
+	end
+end
+
+# UPDATE students SET first_name = '' WHERE id = '';
+def update(category, query_term, id)
+	@conn.exec("UPDATE students SET #{category} = '#{query_term}' WHERE id = '#{id}';")
+	puts "your record has been updated"
+end
+
+# DELETE FROM students WHERE id = '';
+def delete(id)
+	@conn.exec("DELETE FROM students WHERE id = '#{id}';")
+end
+
+def seed
+	1000.times do 
+		add_student(Faker::Name.first_name, Faker::Name.last_name, "#{rand(1..12)}/#{rand(1..28)}/#{rand(1950..1999)}")
 	end
 end
