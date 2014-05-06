574aa17d3c9897fc97839127ad129305dc89ab28
daramao <dara.mao@gmail.com>
2014-05-06 08:48:34 -0400

    hw w03d01 formatted display using justification methods

--- w03/d01/Dara_Mao/amazone_store/store_manager.rb
+++ w03/d01/Dara_Mao/amazone_store/store_manager.rb
@@ -7,22 +7,22 @@ require 'pry'
 # Requirement 1: A user should be able to create new products using a command-line interface.
 
 #User Interface for the "create" part:
-# puts "What is your product called?"
-# prod_name = gets.chomp
-# puts "What is its price?"
-# price = gets.chomp.to_f
-# puts "What is the description of your product?"
-# description = gets.chomp
-# puts "You created #{prod_name} #{description}, that costs $#{price}"
-# #TO-DO: need to add #id after add_products is called back to the output above
+puts "What is your product called?"
+prod_name = gets.chomp
+puts "What is its price?"
+price = gets.chomp.to_f
+puts "What is the description of your product?"
+description = gets.chomp
+puts "You created #{prod_name} #{description}, that costs $#{price}"
+#TO-DO: need to add #id after add_products is called back to the output above
 
 
-# # Writing part: INSERT INTO products (prod_name, price, description) VALUES ();
-# def add_product(prod_name, price, description)
-#   @conn.exec("INSERT INTO products (prod_name, price, description) VALUES ('#{prod_name}', '#{price}', '#{description}');")
-# end
+# Writing part: INSERT INTO products (prod_name, price, description) VALUES ();
+def add_product(prod_name, price, description)
+  @conn.exec("INSERT INTO products (prod_name, price, description) VALUES ('#{prod_name}', '#{price}', '#{description}');")
+end
 
-# add_product(prod_name, price, description)
+add_product(prod_name, price, description)
 
 #STATUS: WORKS !
 #===================================================================================
@@ -38,7 +38,7 @@ puts "Here are all the products that matched your search:"
 # #Search part: SELECT * FROM products WHERE #{category} = '#{query_term}';
 def search(category, query_term)
   search = @conn.exec("SELECT * FROM products WHERE #{category} = '#{query_term}';")
-    @array = []
+  @array = []
   search.each do |category|
     category.each do |k,v|
       @array << v
@@ -52,34 +52,19 @@ def display_search
   index = 0
   line_width = 10
   puts "| id | name      | price | description          | quantity
-|---------------------------------------------------------|"
-  while index <= n
-    print("#{@array[index]}".ljust(line_width/12))
+  |---------------------------------------------------------|"
+  while index < n
+    print "|"+ "#{@array[index]}".ljust(line_width/12)+"   |"
     index += 1
-    print("#{@array[index]}".center(line_width/6))
+    print "#{@array[index]}".center(line_width/6)+"    |"
     index += 1
-    print("#{@array[index]}".rjust(line_width/4))
+    print "#{@array[index]}".rjust(line_width/4)+"     |"
     index += 1
-    print("#{@array[index]}".rjust(line_width/2))
+    print "#{@array[index]}".rjust(line_width/2)+"               |\n"
     index += 1
   end
 end
 display_search
-
-
-# puts "| id | name      | price | description          | quantity
-# |---------------------------------------------------------|"
-# n = array.length
-# while index <= n
-#   index = 1
-#   puts(toc[index].ljust(line_width/2))
-#   index += 1
-#   print(toc[index].ljust(line_width/2))
-#   index +=1
-#   print(toc[index].rjust(line_width/2))
-#   index +=1
-#   print(toc[index].rjust(line_width/2))
-# end
 
 #DOESN'T WORK
 # def display_search
@@ -89,127 +74,98 @@ display_search
 #   end
 #   display_search
 
-# #TO-DO: need to display the "SELECT * FROM products;" from psql to bash
-
 # def display
 #    @display_screen = @conn.exec("SELECT * FROM products;")
 #  end
 
 #  @display_screen
 
-#STATUS: DISPLAY DOESNT WORK !
+#STATUS: DISPLAY WORKS, but need to fix alignment.
 #===================================================================================
 # Requirement 3: A user should be able to "update" the name, price and description of a product without updating it's quantity at the same time.
 
 # User Interface for the "update" part:
-# puts "What is the id of the product do you wish to edit?"
-# id = gets.chomp
-# puts "What is old_prod_names new name?"
-# prod_name = gets.chomp
-# puts "What is old_prod_names new description?"
-# description = gets.chomp
-# #TO-DO: need to consider blank space =nil
-# puts "What is old_prod_names new price?"
-# price = gets.chomp.to_f
-# puts "You updated #{prod_name} #{id}, #{description}, that costs $#{price}"
+puts "What is the id of the product do you wish to edit?"
+id = gets.chomp
+puts "What is old_prod_names new name?"
+prod_name = gets.chomp
+puts "What is old_prod_names new description?"
+description = gets.chomp
+#TO-DO: need to consider blank space =nil
+puts "What is old_prod_names new price?"
+price = gets.chomp.to_f
+puts "You updated #{prod_name} #{id}, #{description}, that costs $#{price}"
 
-# def update(prod_name, description, price, id)
-#   @conn.exec("
-#     UPDATE products
-#       SET prod_name = '#{prod_name}',
-#           description = '#{description}',
-#           price = #{price}
-#       WHERE id = '#{id}';
-#       ")
-# end
+def update(prod_name, description, price, id)
+  @conn.exec("
+    UPDATE products
+    SET prod_name = '#{prod_name}',
+    description = '#{description}',
+    price = #{price}
+    WHERE id = '#{id}';
+    ")
+end
 
-# update(prod_name, description, price, id)
+update(prod_name, description, price, id)
 
 #STATUS: WORKS !
 #===================================================================================
 # Requirement 4: A user should be able to "order" products by incrementing the quantity of a given product by some number N
 
 # User Interface for the "order" part:
-
+def order
+  inventory = @array.length
+  puts "You have #{inventory} #{query_term}. How many are you shipping?"
+  add_inventory = gets.chomp.to_f
+  order = inventory + add_inventory
+  puts "You have #{add_inventory} #{query_term}. You now have #{order} #{query_term} left."
+end
 
 
 #===================================================================================
 #Requirement 5: A user should be able to "ship" products by decrementing the quantity of a given product by some number N
+def ship
+  inventory = @array.length
+  puts "You have #{inventory} #{query_term}. How many are you shipping?"
+  deduct_inventory = gets.chomp.to_f
+  ship = inventory - deduct_inventory
+  puts "You have #{deduct_inventory} #{query_term}. You now have #{ship} #{query_term} left."
+end
 
 #===================================================================================
 #Requirement 6: A user should be able to "delete" products. The result of which should also removing their row from the database.
 
 # User Interface for the "delete" part:
-# puts "What is the id of the product do you wish to delete?"
-# id = gets.chomp
-# puts "Do you really want to delete #productname? (y)es or (n)o "
-# y_n= gets.chomp
+puts "What is the id of the product do you wish to delete?"
+id = gets.chomp
+puts "Do you really want to delete #productname? (y)es or (n)o "
+y_n= gets.chomp
 
-# # Lookup the prod_name using the id entered:
-# def search(id)
-#   result = @conn.exec("SELECT * FROM products WHERE id = '#{id}';")
+# Lookup the prod_name using the id entered:
+def search(id)
+  result = @conn.exec("SELECT * FROM products WHERE id = '#{id}';")
 
-#   array = []
-#   result.each do |key|
-#     key.each do |k, v|
-#       array << v
-#     end
+  array = []
+  result.each do |key|
+    key.each do |k, v|
+      array << v
+    end
 
-#     prod_name = array[1]
-#     puts "You have deleted #{prod_name}"
-#   end
-# end
+    prod_name = array[1]
+    puts "You have deleted #{prod_name}"
+  end
+end
 
-# search(id)
+search(id)
 
 
-# if y_n == 'y'
-#   def delete(id)
-#     @conn.exec("DELETE FROM products WHERE id = '#{id}';")
-#   end
-#   delete(id)
-# else
-#   Kernel::exit
-# end
+if y_n == 'y'
+  def delete(id)
+    @conn.exec("DELETE FROM products WHERE id = '#{id}';")
+  end
+  delete(id)
+else
+  Kernel::exit
+end
 
 #STATUS: WORKS !
-#===================================================================================
-#TESTING FUNCTIONS HERE:
-
-# result = @conn.exec("SELECT * FROM products WHERE id = '3';")
-
-# result.each do |key|
-#   key.each do |k, v|
-#     puts v
-#   end
-# end
-   #v is the value of the final array
-
-#result.values =>
-#[
-#   [0] [
-#   [0] "3", [1] "Chocolate", [2] "0", [3] "Ritz chocolate", [4] nil
-#   ]
-# ]
-
-#Display testing:
-# result = @conn.exec("SELECT * FROM pg_stat_activity") do |result|
-#     puts "| id | name      | price | description          | quantity
-# |---------------------------------------------------------|"
-# n = array.length
-# while index <= n
-#   index = 1
-#   puts(toc[index].ljust(line_width/2))
-#   index += 1
-#   print(toc[index].ljust(line_width/2))
-#   index +=1
-#   print(toc[index].rjust(line_width/2))
-#   index +=1
-#   print(toc[index].rjust(line_width/2))
-# end
-
-#           result.each do |row|
-#             puts " %s" %
-#             row.values_at("id", "prod_name", "price", "description", "quantity")
-#   end
-# end
