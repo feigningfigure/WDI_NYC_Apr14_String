899f0126ff67bce54b000ed08a30dcf77f8231ad
cdbajorin <christopher.bajorin@gmail.com>
2014-05-06 08:43:25 -0400

    homework submission

--- w03/d01/Christopher_Bajorin/homework/store_manager.rb
+++ w03/d01/Christopher_Bajorin/homework/store_manager.rb
@@ -3,25 +3,24 @@ require 'pry'
 @conn = PG.connect(dbname: 'project_manager')
 
 def menu
-  puts "What would you like to do: create, search, update, order, ship, delete, or exit?"
+  puts "What would you like to do: create, search, update, order, ship, delete, or exit?\n"
   input = gets.chomp.downcase
   case input
   when "create"
-    puts "What is your product called?"
+    puts "What is your product called?\n"
     name = gets.chomp
-    puts "What is its price?"
-    price = gets.chomp.to_f
-    puts "What is the description of your product?"
+    puts "What is its price?\n"
+    price = gets.chomp
+    puts "What is the description of your product?\n"
     description = gets.chomp
-    puts "You created 1 #{name}, #{description}, that costs $#{price}."
+    puts "You created 1 #{name}, #{description}, that costs $#{price}.\n"
     @conn.exec("INSERT INTO products (name, price, description, quantity) VALUES ('#{name}', '#{price}', '#{description}', 1);")
   when "search"
-    puts "Do you wish to search by 'id' or 'name'?"
+    puts "Do you wish to search by 'id' or 'name'?\n"
     category = gets.chomp
-    puts "What #{category}?"
+    puts "What #{category}?\n"
     query = gets.chomp
-    puts "Here are all the products that matched your search:"
-
+    puts "Here are all the products that matched your search:\n"
     result = @conn.exec("SELECT * FROM products WHERE #{category} = '#{query}';")
     puts " id | name | price | description | quantity "
     puts "-------------------------------------------- "
@@ -29,13 +28,13 @@ def menu
       puts "  #{product['id']} | #{product['name']} | #{product['price']} | #{product['description']} | #{product['quantity']} |"
     end
   when "update"
-    puts "What is the id of the product do you wish to edit?"
+    puts "What is the id of the product do you wish to edit?\n"
     id = gets.chomp
-    puts "What is the new name?"
+    puts "What is the new name?\n"
     new_name = gets.chomp
-    puts "What is the new description?"
+    puts "What is the new description?\n"
     new_desc = gets.chomp
-    puts "What is the new price?"
+    puts "What is the new price?\n"
     new_price = gets.chomp
     result = @conn.exec("SELECT * FROM products WHERE id = '#{id}';")
     if new_name == ''
@@ -52,26 +51,24 @@ def menu
     @conn.exec("UPDATE products SET name = '#{new_name}' WHERE id = '#{id}';")
     @conn.exec("UPDATE products SET description = '#{new_desc}' WHERE id = '#{id}';")
     @conn.exec("UPDATE products SET price = '#{new_price.to_f}' WHERE id = '#{id}';")
-
-    puts "You updated #{new_name}, #{new_desc}, that costs $#{new_price}"
-
+    puts "You updated #{new_name}, #{new_desc}, that costs $#{new_price}.\n"
   when "order"
-    puts "What is the id of the product do you wish to order?"
+    puts "What is the id of the product do you wish to order?\n"
     id = gets.chomp
     result = @conn.exec("SELECT * FROM products WHERE id = '#{id}';")
-    puts "You have #{result[0]['quantity'].to_s} #{result[0]['name'].to_s}s. How many do you wish to order?"
+    puts "You have #{result[0]['quantity'].to_s} #{result[0]['name'].to_s}s. How many do you wish to order?\n"
     order = gets.chomp.to_i
     total = order + result[0]['quantity'].to_i
-    puts "You have added #{order} #{result[0]['name'].to_s}s and now have #{total} #{result[0]['name'].to_i}s left."
+    puts "You have added #{order} #{result[0]['name'].to_s}s and now have #{total} #{result[0]['name'].to_i}s left.\n"
     @conn.exec("UPDATE products SET quantity = '#{total}' WHERE id = '#{id}';")
   when "ship"
-    puts "What is the id of the product do you wish to ship?"
+    puts "What is the id of the product do you wish to ship?\n"
     id = gets.chomp
     result = @conn.exec("SELECT * FROM products WHERE id = '#{id}';")
-    puts "You have #{result[0]['quantity'].to_s} #{result[0]['name'].to_s}s. How many do you wish to ship?"
+    puts "You have #{result[0]['quantity'].to_s} #{result[0]['name'].to_s}s. How many do you wish to ship?\n"
     order = gets.chomp.to_i
     total = order - result[0]['quantity'].to_i
-    puts "You have shipped #{order} #{result[0]['name'].to_s}s and now have #{total} #{result[0]['name'].to_i}s left."
+    puts "You have shipped #{order} #{result[0]['name'].to_s}s and now have #{total} #{result[0]['name'].to_i}s left.\n"
     @conn.exec("UPDATE products SET quantity = '#{total}' WHERE id = '#{id}';")
   when "delete"
     result = @conn.exec("SELECT * FROM products;")
@@ -80,10 +77,10 @@ def menu
     result.each do |product|
       puts "  #{product['id']} | #{product['name']} | #{product['price']} | #{product['description']} | #{product['quantity']} |"
     end
-    puts "which item would you like to delete?"
+    puts "which item would you like to delete?\n"
     id = gets.chomp
     @conn.exec("DELETE FROM products WHERE id = '#{id}';")
-    puts "deleted!"
+    puts "deleted!\n"
   when "exit"
     Kernel.exit
   end
