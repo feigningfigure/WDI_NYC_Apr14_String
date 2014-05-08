30a171c110441ee70ecc0cb42e40ce63b4a5ec51
LizCira <liz.cira.goldstein@gmail.com>
2014-05-08 15:58:43 -0400

    trying to add instagram api project

--- w03/d04/Liz_Goldstein/rails_blog/db/seeds.rb
+++ w03/d04/Liz_Goldstein/rails_blog/db/seeds.rb
@@ -5,8 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
+100.times do
 post = Post.create({
-  title: "First Post!",
-  body: "Yeah!"
+  title: Faker::Lorem.sentence ,
+  body:  Faker::Lorem.paragraph(sentence_count = 3)
   })
+end
+# @posts = 100.times do Post.create end
+
+#create @ posts in post controller..to use in associated view
