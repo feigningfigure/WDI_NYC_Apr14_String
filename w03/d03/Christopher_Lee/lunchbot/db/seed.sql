b88e91a3e7e1639b2ee3b3cf7d34c3375a319053
ChrisMLee <cristovao.m.lee@gmail.com>
2014-05-08 19:28:54 -0400

    db migrated and rails project ready to go

---
+++ w03/d03/Christopher_Lee/lunchbot/db/seed.sql
@@ -0,0 +1,24 @@
+INSERT INTO lunches
+(lunch_name, rating)
+VALUES
+(
+  'Mcdonalds',
+  '3'
+);
+
+INSERT INTO lunches
+(lunch_name, rating)
+VALUES
+(
+  'Chez Maison Du Bordeaux',
+  '10'
+);
+
+
+INSERT INTO lunches
+(lunch_name, rating)
+VALUES
+(
+  'Hooties Fish House',
+  '7'
+);
--- w03/d04/Christopher_Lee/intro_to_rails/first_rails_app/db/seeds.rb
+++ w03/d04/Christopher_Lee/intro_to_rails/first_rails_app/db/seeds.rb
@@ -5,11 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-100.times do
-Post.create({
-  title: Faker::Lorem.sentence(3),
-  body: Faker::Lorem.paragraph(1),
-  author_name: Faker::Name.name
-  })
-end
