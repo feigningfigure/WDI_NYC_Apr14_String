fe0557b106bc116a3781ef1b470e125fd7ea0646
fertingoff <art.incoming@gmail.com>
2014-05-08 21:53:40 -0400

    HW for THU

---
+++ w03/d04/Artem_Murga/hw/movier/db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+first_movie = Movie.create({
+
+	Title: "Test_movie",
+	Year: 1959,
+	Rated: 9,
+	Released: "05/05/1978",
+	Runtime: "IDK",
+	Genre: "Sci-Fi",
+	Director: "John Smith",
+	Writer: "John Smith",
+	Actors: "JL",
+	Plot: "...",
+	Poster: "..."
+
+	})
+
