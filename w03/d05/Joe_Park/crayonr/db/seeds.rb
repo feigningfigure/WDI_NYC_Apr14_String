f2e3c363fec5b7e85d7a552e2ee750bb37e35cca
jpark1120 <parkj1120@gmail.com>
2014-05-09 16:12:17 -0400

    commit before end of class

---
+++ w03/d05/Joe_Park/crayonr/db/seeds.rb
@@ -0,0 +1,41 @@
+Crayon.delete_all
+Owner.delete_all
+
+owners = []
+
+funny_titles = [
+  "Sgt. Pepper",
+  "Mr. Sir",
+  "Mrssss.",
+  "King",
+  "Sensei"
+]
+
+10.times do
+  owners << Owner.create({
+    name: Faker::Name.name,
+    formal_title: funny_titles.sample
+    })
+end
+
+
+100.times do
+  # this will make a new instance of Crayon
+  # then save it to the database in one swoop.
+  Crayon.create({
+    name: Faker::Commerce.color,
+    intensity: rand(100)
+    owner_id: owners.sample.id
+  })
+
+
+end
+
+# crayons = Crayon.all
+
+
+# crayons.each do |x|
+#   owners_list = Owner.all
+#   crayons[x]["owner_id"] = owners_list.sample.id
+
+# end
