b29e36faccf2d2a99d69f5c84f1655e651c77e2e
Mannymin <Mannymin@gmail.com>
2014-05-09 14:44:42 -0400

    please work

---
+++ w03/d05/Emmanuel_Tucker/superhero.rb
@@ -0,0 +1,21 @@
+require 'faker'
+
+
+
+attr_accessor number: gender: age:
+
+class human
+
+     def initialize (name, age ,gender)
+          @name =
+          @age =
+          @gender =
+          Faker::Name.name
+          Faker::Age.age
+          Faker::Gender.gender
+ end
+
+
+ def dog (name ,owner)
+ end
+end
