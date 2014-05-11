cd0a49a45f9207fe0828d284fc40da9a4cab0911
keyanbagheri <keyanbagheri@gmail.com>
2014-05-11 18:18:48 -0400

    planetary app

---
+++ w03/d05/Keyan_Bagheri/planetary/config/routes.rb
@@ -0,0 +1,32 @@
+Rails.application.routes.draw do
+
+  get "/" => "planets#index"
+
+  get "/planets" => "planets#index"
+
+  get "/planets/new" => "planets#new"
+
+  post "/planets" => "planets#create"
+
+  get "/planets/:id" => "planets#show"
+
+  get "/planets/update/:id" => "planets#update"
+
+  post "/planets/update/:id" => "planets#edit"
+
+  get "/planets/delete/:id" => "planets#delete"
+
+  get "/moons" => "moons#index"
+
+  get "/moons/new" => "moons#new"
+
+  post "/moons" => "moons#create"
+
+  get "/moons/:id" => "moons#show"
+
+  get "/moons/update/:id" => "moons#update"
+
+  post "/moons/update/:id" => "moons#edit"
+
+  get "/moons/delete/:id" => "moons#delete"
+end
