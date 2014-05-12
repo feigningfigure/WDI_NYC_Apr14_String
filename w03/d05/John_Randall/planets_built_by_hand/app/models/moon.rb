8ea81f80b2df506430a925184ddd62a7b008d9ab
johntrandall <john@johnrandall.com>
2014-05-12 09:02:41 -0400

    w03_d05 homwork

---
+++ w03/d05/John_Randall/planets_built_by_hand/app/models/moon.rb
@@ -0,0 +1,11 @@
+class Moon < ActiveRecord::Base
+
+
+# - Create a new table for `moons`
+# - All moons should belong to a planet
+# - Create the functionality needed to add moons to the database  
+# - Update your views so that when viewing a planet, a user can also see what moons belong to it. 
+
+
+
+end
---
+++ w03/d05/John_Randall/planets_via_scaffold/app/views/moons/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @moon, :id, :name, :image_url, :diameter, :mass, :life, :created_at, :updated_at
