c968aadacf66058ebcef92b752f20c1163f3f2e4
omardelarosa <thedelarosa@gmail.com>
2014-05-12 16:37:19 -0400

    git syncing added comments to routes file and added all the first few user stories for the cookbook

--- w04/d01/INSTRUCTORS/cookly/app/controllers/recipes_controller.rb
+++ w04/d01/INSTRUCTORS/cookly/app/controllers/recipes_controller.rb
@@ -8,6 +8,20 @@ class RecipesController < ApplicationController
     redirect_to "/cookbooks/#{recipe_attributes[:cookbook_id]}"
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+    render partial: "recipeform", locals: { recipe_local: @recipe, cookbook_id: @recipe.cookbook.id }
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+
+    recipe.update_attributes(recipe_attributes)
+
+    redirect_to "/cookbooks/#{recipe.cookbook.id}"
+    # render json: params
+  end
+
   private
 
   def recipe_attributes
