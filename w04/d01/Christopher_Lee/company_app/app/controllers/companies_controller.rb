e63b6c78fb3cd42d19038b2b84c8ada73952cdab
ChrisMLee <cristovao.m.lee@gmail.com>
2014-05-12 20:35:53 -0400

    Add feature working

--- w04/d01/Christopher_Lee/company_app/app/controllers/companies_controller.rb
+++ w04/d01/Christopher_Lee/company_app/app/controllers/companies_controller.rb
@@ -1,5 +1,34 @@
 class CompaniesController < ApplicationController
   def index
     @companies = Company.order(id: :desc)
+    @company = Company.new
   end
+
+   def show
+    # define @cookbook
+    @company = Company.find(params[:id])
+
+    # makes the form work
+    @products = Product.new
+  end
+
+  def create
+    Company.create(company_attributes)
+
+    # render json: {
+    #   strong_params: company_attributes
+    # }
+
+    redirect_to '/'
+  end
+
+
+  private
+
+  # strong params
+  def company_attributes
+    # whitelisting parts of the params hash, that are DB-safe
+    params.require(:company).permit(:name, :image_url, :assets_in_billions, :founded)
+  end
+
 end
