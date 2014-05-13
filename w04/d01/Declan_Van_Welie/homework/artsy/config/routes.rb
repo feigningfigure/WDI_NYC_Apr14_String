63bb87579f611b23a0f745d1fc513842221deabd
Crustacean87! <declanv@gmail.com>
2014-05-13 00:08:59 -0400

    Adding functionality for create and edit to the artsy app

--- w04/d01/Declan_Van_Welie/homework/artsy/config/routes.rb
+++ w04/d01/Declan_Van_Welie/homework/artsy/config/routes.rb
@@ -1,56 +1,23 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+root to: "artists#index"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+get "/artists" => "artists#index"
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+get "/artists/:id" => "artists#show", as: :artist
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+get "/artists/:id/edit" => "artists#edit", as: :edit_artist
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  patch "/artists/:id" => "artists#update"
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+post "/works" => "works#create"
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+
+get "/works/:id" => "works#index", as: :work
+
+  get "/works/:id/edit" => "works#edit", as: :edit_work
+
+  patch "/works/:id" => "works#update"
 end
