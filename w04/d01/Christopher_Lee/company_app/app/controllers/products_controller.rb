76eb46a4030fb4c90ca1f4a2deaca68e3cad3d46
ChrisMLee <cristovao.m.lee@gmail.com>
2014-05-14 13:21:07 -0400

    almost have dropdown working for products

--- w04/d01/Christopher_Lee/company_app/app/controllers/products_controller.rb
+++ w04/d01/Christopher_Lee/company_app/app/controllers/products_controller.rb
@@ -14,18 +14,22 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @company = Company.find(@product.company_id)
     @countries = Country.all
+    @distributor = Distributor.new
   end
 
 
   def update
-    product = Product.find(params[:id])
+    product = Product.find(params[:product_id])
     product.update_attributes(product_attributes)
     # render json: product_attributes
     redirect_to "/companies/#{product_attributes[:company_id]}"
   end
 
   def add_distributor
-
+    product = Product.find(params[:product_id])
+    selected_country = Country.find(params[:id])
+    product.distributors.create(country: selected_country)
+    redirect_to "products/#{product.id}/edit"
   end
 
   private
@@ -33,4 +37,5 @@ class ProductsController < ApplicationController
   def product_attributes
     params.require(:product).permit(:name, :image_url, :founded, :company_id)
   end
+
 end
