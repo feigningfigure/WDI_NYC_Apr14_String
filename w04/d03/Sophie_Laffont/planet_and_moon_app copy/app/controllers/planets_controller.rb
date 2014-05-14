5d015b13cff7f78719df0871ff731d4126e7fa1f
slaffont <slaffont@gmail.com>
2014-05-14 09:25:56 -0400

    week04 -day02- homwork seeds

---
+++ w04/d03/Sophie_Laffont/planet_and_moon_app copy/app/controllers/planets_controller.rb
@@ -0,0 +1,30 @@
+class PlanetsController  < ActionController::Base
+
+layout "application"
+
+	def index
+		@planets = Planet.all
+	end
+
+	def new
+		@planet =Planet.new
+	end
+
+	def create
+		new_planet = params[:planet]
+			Planet.create({
+				name: new_planet[:name],
+				image_url: new_planet[:image_url],
+				diameter: new_planet[:diameter],
+				mass: new_planet[:mass],
+				life: new_planet[:life],		
+				})
+		redirect_to planets_path
+	end
+
+	def show
+		planet_id = params[:id]
+		@planet = Planet.find(planet_id)
+	end
+
+end
