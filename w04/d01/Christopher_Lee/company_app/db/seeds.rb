eebb2b4135ee46c058e351633fab923a5efb2fb6
ChrisMLee <cristovao.m.lee@gmail.com>
2014-05-13 20:49:10 -0400

    Created working join table. Now I just need to load it up on the site

--- w04/d01/Christopher_Lee/company_app/db/seeds.rb
+++ w04/d01/Christopher_Lee/company_app/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Company.destroy_all
+Product.destroy_all
+Country.destroy_all
+
+new_company = Company.create :name => "Pepsico", :image_url => "pepsi.com", :assets_in_billions => 22, :founded => 1947
+
+Product.create :name => "San Pellegrino", :image_url => "sanpellegrino.com", :founded => 1947, :company_id => new_company.id
+Country.create :name => "France", :language => "French", :gdp => 2.6
+
+
+product = Product.first
+country = Country.first
+
+product.distributors.create(country: country)
