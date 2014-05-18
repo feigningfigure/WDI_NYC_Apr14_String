a2ec83947f6dd736a5ae346f32cd70dd320771f3
benticsay <benticsay@gmail.com>
2014-05-18 00:54:32 -0400

    spring cleaning

--- w03/d02/INSTRUCTORS/blog2_active_record/db/schema.sql
+++ w03/d02/INSTRUCTORS/blog2_active_record/db/schema.sql
@@ -8,3 +8,11 @@ CREATE TABLE posts
   body  text,
   author varchar(255)
 );
+
+CREATE TABLE posts
+(
+  id  serial NOT NULL primary key,
+  user_name varchar(255),
+  description varchar(255),
+  url  text
+);
