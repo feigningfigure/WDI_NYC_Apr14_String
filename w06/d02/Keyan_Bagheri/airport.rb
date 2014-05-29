2f497a0810f95cba714c5b0415d6e4bd841509d8
keyanbagheri <keyanbagheri@gmail.com>
2014-05-29 09:02:48 -0400

    hmwk w06d02

--- w06/d02/Keyan_Bagheri/airport.rb
+++ w06/d02/Keyan_Bagheri/airport.rb
@@ -3,33 +3,75 @@ api_response = {
         "LAX" => {},
         "LGA" => {},
         "BOS" => {}
-        },
-        "weather" => {
-            "Severe Wind & Rain" => ["LGA"],
-            "Flood Warning" => ["LGA", "BOS"]
-        }
+    },
+    "weather" => {
+        "Severe Wind & Rain" => ["LGA"],
+        "Flood Warning" => ["LGA", "BOS"]
     }
+}
 
-    def airports_with_weather(data)
-        data["airports"].each do |code|
-            weathers = []
-            data["weather"].each do |x,v|
-                if v == code
-                    weathers << x
-                end
-            end
-            yield(code,x)
-        end
+# I can define a block-taking method EXPLICITLY, like this...
+# def airports_with_weather(data, &block)
+
+# Or IMPLICITLY like this, and use yield() later.
+def airports_with_weather(data)
+    # EXPLICIT: if block.nil?
+    if !block_given?
+        puts "I got all these airports and all this weather, whaddya want me to do?"
+        return nil
     end
 
-    airports_with_weather(api_response) do |code, weathers|
-        puts "<section>"
-        puts "\t<h1>#{code}</h1>"
-        weathers.each do |weather|
-            puts "\t<div>#{weather}</div>"
+    data["airports"].each_key do |code|
+        weathers = data["weather"].keys.select do |weather|
+            data["weather"][weather].include?(code)
         end
-        puts "</section>"
+
+        # EXPLICIT: block.call(code, weathers)
+        yield(code, weathers)
     end
+end
+
+# Call using a block
+airports_with_weather(api_response) do |code, weathers|
+    puts "<section>"
+    puts "\t<h1>#{code}</h1>"
+    weathers.each do |weather|
+        puts "\t<div>#{weather}</div>"
+    end
+    puts "</section>"
+end
+
+puts
+
+# This variable contains an instance of the Proc class
+print_warning_if_weather_sucks = Proc.new do |code, weathers|
+    if weathers.length > 1
+        puts "DANGER! The weather at #{code} is awful."
+    end
+end
+
+puts "Call using a Proc TURNED INTO a block."
+airports_with_weather(api_response, &print_warning_if_weather_sucks)
+
+puts
+
+# What if I define this using a method?
+def print_warning_if_weather_sucks_method(code, weathers)
+    if weathers.length > 1
+        puts "DANGER! The weather at #{code} is awful."
+    end
+end
+
+# First, turn my method into a Proc
+print_warning_proc = method(:print_warning_if_weather_sucks_method)
+# Now, pass that Proc into my airports function as a block
+
+puts "Call using a method TURNED INTO a Proc TURNED INTO a block"
+airports_with_weather(api_response, &print_warning_proc)
+
+puts
+
+airports_with_weather(api_response)
 
 # <section>
 #     <h1>LAX</h1>
