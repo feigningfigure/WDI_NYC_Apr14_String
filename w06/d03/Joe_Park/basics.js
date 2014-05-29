4594092dfe8d390fcdaffbddeb34a65858a69f13
jpark1120 <parkj1120@gmail.com>
2014-05-29 13:46:53 -0400

    push after lunch

--- w06/d03/Joe_Park/basics.js
+++ w06/d03/Joe_Park/basics.js
@@ -18,3 +18,43 @@ if (myFavoriteNumber < 1000) {
 }
 
 console.log("Eh?");
+
+var myArray = [
+	"platypus",
+	"pterodactyl",
+	"aardvark",
+	"dragon"
+];
+
+for (var i = 0; i < myArray.length; i++) {
+	var item = myArray[i];
+	console.log(item);
+	// process.stdout.write(item);
+}
+
+console.log("\nNow with forEach():")
+myArray.forEach(function(item) {
+	console.log(item);
+});
+
+console.log("\nNow with a function in a variable");
+
+var viewFantasticCreature = function(item) {
+	console.log("Wow! Observe that amazing " + item);
+};
+
+myArray.forEach(viewFantasticCreature);
+
+function doSomethingCool() {
+	console.log("This isn't very cool, to be honest.");
+}
+
+console.log();
+console.log("Returns\n");
+
+function calculateAge(starting, years) {
+	return starting + years;
+}
+
+var myAge = calculateAge(50, 100);
+console.log(myAge);
