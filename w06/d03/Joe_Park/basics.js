58959d3807462a223b277e45f5630b8c36d4c607
jpark1120 <parkj1120@gmail.com>
2014-05-29 11:46:40 -0400

    starting exercises in basics.js

---
+++ w06/d03/Joe_Park/basics.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+console.log(1 + 1);
+
+console.log("1" + "1");
+
+var myFavoriteNumber = 500;
+
+if (myFavoriteNumber < 1000) {
+	console.log("Dude your fave number is weaksauce.");
+	var myArray = [
+		5,
+		7,
+		10
+	];
+} else {
+	console.log("Rad.");
+}
+
+console.log("Eh?");
