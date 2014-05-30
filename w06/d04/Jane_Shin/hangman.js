bf587808fbb80fee63f5f437ea109fde437242bc
janeplural <mlle.jihshi@gmail.com>
2014-05-30 17:19:55 -0400

    hw part 1 done

--- w06/d04/Jane_Shin/hangman.js
+++ w06/d04/Jane_Shin/hangman.js
@@ -1,14 +1,32 @@
 function Hangman(word) {
+var playerprogress = [];
 
 	this.progress = function(){
-		var letters = [];
-
-		for (var i = 0; i < word.length; i++) {
-			letters.push("_");
+		if(playerprogress.length === 0 ){
+			for (var i = 0; i < word.length; i++) {
+					playerprogress.push("_");
+			}
 		}
 
-		return letters.join(" ");
+		return playerprogress.join(" ");
 
+	};
+
+	this.guess = function(letter) {
+			if(word.indexOf(letter) !== -1){
+				for (var i = 0; i < word.length; i++) {
+					if(word.charAt(i) === letter){
+						playerprogress[i] = letter;
+					}
+				}
+				// function match_letter(letter, index, array){
+				// 	array[word.indexOf(letter)] = element;
+				// }
+				// playerprogress.filter(match_letter(letter));
+				return "true"	
+			}else{
+				return "false"
+		}
 	};
 
 }
@@ -16,3 +34,32 @@ function Hangman(word) {
 var game = new Hangman("detective");
 
 console.log(game.progress()); //=> "_ _ _ _ _ _ _ _ _"
+console.log(game.guess("e"));
+console.log(game.guess("t"));
+console.log(game.guess("x"));
+console.log(game.progress());
+
+
+
+
+word = "mom"
+letter = "m"
+array = ["-", "-", "-"]
+i = 3
+if(word.charAt(i) === letter){
+	array[i] = letter;
+}
+
+// for (var i = 0; i word.length; i++) {
+//     if (i == 3) continue;
+//     text += "The number is " + i + "<br>";
+// }
+
+
+// var numbersarray=[-3, 5, 34, 19]
+
+// function greaterThanFive(element, index, array) {
+//  return (element > 5)
+// }
+
+// var FiveplusArray=numbersarray.filter(greaterThanFive) //new array contains [34, 19]
