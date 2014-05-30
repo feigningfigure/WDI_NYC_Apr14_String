fb2c0767a8b160a6fa09168ad3d7213277e45fdb
Mannymin <Mannymin@gmail.com>
2014-05-30 09:31:47 -0400

    what

---
+++ w06/d03/Emmanuel_Tucker/Morningfriex.js
@@ -0,0 +1,24 @@
+
+
+
+
+function dollarsToPounds(5) {
+
+   if (5 === 2.99096727881797)return
+
+console.log()
+
+}
+
+
+
+function dollarsToPounds()
+
+
+console.log()
+
+
+
+
+currencyConverter()
+currencyConverter()
