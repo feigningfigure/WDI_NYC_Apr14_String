78bf07b31a86b3db3db22434d134ed8f1289e836
janeplural <mlle.jihshi@gmail.com>
2014-05-30 14:30:18 -0400

    started lab

---
+++ w06/d04/Jane_Shin/hangman.js
@@ -0,0 +1,20 @@
+function Hangman(word) {
+	// var LETTERS = [];
+	// this.game = []
+	// this.word = word;
+	this.progress = function(){
+		var letters = [];
+
+		for (var i = 0; i < word.length; i++) {
+			letters.push("_");
+		}
+
+		return letters.join(" ");
+
+	};
+
+}
+
+var game = new Hangman("detective");
+
+console.log(game.progress()); //=> "_ _ _ _ _ _ _ _ _"
