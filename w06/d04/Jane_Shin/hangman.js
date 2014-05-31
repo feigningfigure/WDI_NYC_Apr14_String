51a905c5e81ab0f61481e4643781e0d65cc2463e
janeplural <mlle.jihshi@gmail.com>
2014-05-31 13:58:37 -0400

    hangman hw phase 4 done

--- w06/d04/Jane_Shin/hangman.js
+++ w06/d04/Jane_Shin/hangman.js
@@ -1,10 +1,11 @@
 function Hangman(words) {
-var word = words[Math.floor(Math.random() * words.length)]
-var playerprogress = [];
-var count = 0;
+	var word = words[Math.floor(Math.random() * words.length)]
+	var playerprogress = [];
+	var count = 0;
+	var guessed_letters = [];
 
 	this.progress = function(){
-		if(playerprogress.length === 0 ){
+		if (playerprogress.length === 0 ){
 			for (var i = 0; i < word.length; i++) {
 				playerprogress.push("_");
 			}
@@ -13,16 +14,20 @@ var count = 0;
 	};
 
 	this.guess = function(letter) {
-		if(count < 5){
-			if(word.indexOf(letter) !== -1){
+		if (count < 5){
+			if (word.indexOf(letter) !== -1){
 				for (var i = 0; i < word.length; i++) {
-					if(word.charAt(i) === letter){
+					if (word.charAt(i) === letter){
 						playerprogress[i] = letter;
 					}
 				}
+				guessed_letters.push(letter);
 				return "true"	
 			}else{
-				console.log(count++);
+				if(guessed_letters.indexOf(letter) === -1){
+					count++;
+					guessed_letters.push(letter);
+				}
 				return "false"
 			}
 		}else{
@@ -31,30 +36,38 @@ var count = 0;
 		}
 	};
 
+	this.guesses = function() {
+		return guessed_letters
+	};
+
 	this.giveUp = function(){
 		count = 5;
 		return word
 	};
 
+	this.incorrect = function() {
+		return count
+	}
+
 }
 
-// var game = new Hangman("detective");
-var game = new Hangman(["camera", "picture", "gandhi"]);
+var game = new Hangman(["detective"]);
+// var game = new Hangman(["camera", "picture", "gandhi"]);
 
 console.log(game.progress()); //=> "_ _ _ _ _ _ _ _ _"
 console.log(game.guess("e")); 
 console.log(game.guess("c"));
 console.log(game.guess("x")); //1
 console.log(game.progress());
-console.log(game.giveUp());
+// console.log(game.giveUp());
 console.log(game.guess("x")); //2
-// console.log(game.guess("x")); //3
+console.log(game.guess("y")); //3
 // console.log(game.guess("x")); //4
 // console.log(game.guess("x")); //5
 // console.log(game.guess("x")); //6
-console.log(game.progress());
-
-
+// console.log(game.progress());
+console.log(game.guesses());
+console.log(game.incorrect());
 
 
 // node testing ========================
@@ -66,3 +79,9 @@ console.log(game.progress());
 // if(word.charAt(i) === letter){
 // 	array[i] = letter;
 // }
+
+
+
+
+
+
