7bbad98aec1c55097c82205754d5cea0b02131bf
janeplural <mlle.jihshi@gmail.com>
2014-05-31 14:47:52 -0400

    hangman hw bonus phase done

--- w06/d04/Jane_Shin/hangman.js
+++ w06/d04/Jane_Shin/hangman.js
@@ -1,8 +1,10 @@
 function Hangman(words) {
 	var word = words[Math.floor(Math.random() * words.length)]
 	var playerprogress = [];
-	var count = 0;
 	var guessed_letters = [];
+	var count = 0;
+	var hint_count = 0;
+
 
 	this.progress = function(){
 		if (playerprogress.length === 0 ){
@@ -36,6 +38,7 @@ function Hangman(words) {
 		}
 	};
 
+// why does this have to be a function?
 	this.guesses = function() {
 		return guessed_letters
 	};
@@ -45,9 +48,23 @@ function Hangman(words) {
 		return word
 	};
 
+// why does this have to be a function?
 	this.incorrect = function() {
 		return count
-	}
+	};
+
+	this.hint = function() {
+		for (var i = 0; i < word.length; i++) {
+			if (word.charAt(i) !== playerprogress[i]) {
+				playerprogress[i] = word.charAt(i);
+				hint_count++;
+					if (hint_count > 1) {
+						count++;
+					} 
+				return word.charAt(i)
+			}
+		}
+	};
 
 }
 
@@ -56,31 +73,24 @@ var game = new Hangman(["detective"]);
 
 console.log(game.progress()); //=> "_ _ _ _ _ _ _ _ _"
 console.log(game.guess("e")); 
-console.log(game.guess("c"));
-console.log(game.guess("x")); //1
+// console.log(game.guess("c"));
+// console.log(game.guess("x")); //#1
 console.log(game.progress());
 // console.log(game.giveUp());
-console.log(game.guess("x")); //2
-console.log(game.guess("y")); //3
-// console.log(game.guess("x")); //4
-// console.log(game.guess("x")); //5
-// console.log(game.guess("x")); //6
+console.log(game.guess("x")); //#2
+// console.log(game.guess("y")); //#3
+// console.log(game.guess("x")); //#4
+// console.log(game.guess("x")); //#5
+// console.log(game.guess("x")); //#6
 // console.log(game.progress());
 console.log(game.guesses());
-console.log(game.incorrect());
-
-
-// node testing ========================
-
-// word = "mom"
-// letter = "m"
-// array = ["-", "-", "-"]
-// i = 3
-// if(word.charAt(i) === letter){
-// 	array[i] = letter;
-// }
-
-
+console.log(game.incorrect()); //=>2
+console.log(game.hint());
+console.log(game.progress());
+console.log(game.incorrect()); //=>2
+console.log(game.hint());
+console.log(game.progress());
+console.log(game.incorrect()); //=>3
 
 
 
