4d5df7c2dc982461c51b12303faa21a2ac41b78b
janeplural <mlle.jihshi@gmail.com>
2014-06-01 15:48:55 -0400

    i think i'm really done now

--- w06/d04/Jane_Shin/hangman.js
+++ w06/d04/Jane_Shin/hangman.js
@@ -7,34 +7,42 @@ function Hangman(words) {
 
 
 	this.progress = function(){
-		if (playerprogress.length === 0 ){
-			for (var i = 0; i < word.length; i++) {
-				playerprogress.push("_");
+		if (count < 5){
+			if (playerprogress.length === 0 ){
+				for (var i = 0; i < word.length; i++) {
+					playerprogress.push("_");
+				}
 			}
+			return playerprogress.join(" ");
+		}else{
+			return word
 		}
-		return playerprogress.join(" ");
 	};
 
 	this.guess = function(letter) {
-		if (count < 5){
-			if (word.indexOf(letter) !== -1){
-				for (var i = 0; i < word.length; i++) {
-					if (word.charAt(i) === letter){
-						playerprogress[i] = letter;
+		if (playerprogress.join("") !== word) {
+			if (count < 5){
+				if (word.indexOf(letter) !== -1){
+					for (var i = 0; i < word.length; i++) {
+						if (word[i] === letter){
+							playerprogress[i] = letter;
+						}
 					}
-				}
-				guessed_letters.push(letter);
-				return "true"	
-			}else{
-				if(guessed_letters.indexOf(letter) === -1){
-					// count++;
 					guessed_letters.push(letter);
+					return "true"
+				}else{
+					if(guessed_letters.indexOf(letter) === -1){
+						// count++;
+						guessed_letters.push(letter);
+					}
+					return "false"
 				}
-				return "false"
+			}else{
+				playerprogress = word.split("");
+				return null
 			}
 		}else{
-			playerprogress = word.split("")
-			return null
+			return "you won"
 		}
 	};
 
@@ -78,24 +86,25 @@ var game = new Hangman(["detective"]);
 
 console.log(game.progress()); //=> "_ _ _ _ _ _ _ _ _"
 console.log(game.guess("e")); 
-// console.log(game.guess("c"));
-// console.log(game.guess("x")); //#1
-console.log(game.progress());
+console.log(game.guess("c"));
+console.log(game.guess("d")); //#1
+// console.log(game.progress());
 // console.log(game.giveUp());
 console.log(game.guess("x")); //#2
-// console.log(game.guess("y")); //#3
-// console.log(game.guess("x")); //#4
-// console.log(game.guess("x")); //#5
-// console.log(game.guess("x")); //#6
+console.log(game.guess("t")); //#3
+console.log(game.guess("i")); //#4
+console.log(game.guess("v")); //#5
+console.log(game.progress());
+console.log(game.guess("x")); //#6
+console.log(game.progress());
+// console.log(game.guesses());
+// console.log(game.incorrect()); //=>2
+// console.log(game.hint());
 // console.log(game.progress());
-console.log(game.guesses());
-console.log(game.incorrect()); //=>2
-console.log(game.hint());
-console.log(game.progress());
-console.log(game.incorrect()); //=>2
-console.log(game.hint());
-console.log(game.progress());
-console.log(game.incorrect()); //=>3
+// console.log(game.incorrect()); //=>2
+// console.log(game.hint());
+// console.log(game.progress());
+// console.log(game.incorrect()); //=>3
 
 
 
