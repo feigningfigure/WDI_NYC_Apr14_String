6124d8ed10cecbbcea03f6bc4e82a50e6bc61ca3
daramao <dara.mao@gmail.com>
2014-06-01 13:23:13 -0400

    Hangman Phase 1 v3 completed

--- w06/d04/Dara_Mao/hangman_v2.js
+++ w06/d04/Dara_Mao/hangman_v2.js
@@ -1,36 +1,60 @@
+
 function Hangman (word){
-  this.word = word;
+  this.word = word
+  this.playerProgress = [];
 
   this.progress = function(){
-    // array
-    var playerProgress = [];
+    playerProgress = [];
     for (var i = 0; i < word.length; i++){
-      // add an underscore to the array
+      // Add an underscore to the array
       playerProgress.push("_ ");
     }
-    // convert the array back to a string
-    return playerProgress.join("");// array => string
-    // return new_word = playerProgress.join("");
+    var index_num = -1;
+    // index_num = word.indexOf(new_word)
+    // playerProgress[index_num] = new_word;
+    // Convert the array back to a string
+    result = playerProgress.join("");
+    return result; // array => string
   };
 
   this.guess = function(letter){
-    var index_num = word.indexOf(letter)
+    var index_num = -1;
+    index_num = word.indexOf(letter)
     if (index_num === -1){
-        return false
+      console.log("false")
     } else {
-        return true
-    }
-     // return index_num
-  }
-}
+      console.log("true")
+        //update playerProgress
+        playerProgress[index_num] = letter;
+        result = playerProgress.join("");
+        console.log(result)
+
+       return letter;
+     }
+     return;
+   };
+
+
+ }
+
 
 //Calling the function
-var game = new Hangman ("detective");
+// var game = new Hangman ("detective");
 
-console.log(game.progress()); // produces "--------" by splitting the letters into an array and splitting it back into a word
+// console.log(game.progress()); // produces "--------" by splitting the letters into an array and splitting it back into a word
 
-var game = new Hangman ("detective");
+var game = new Hangman ("desk");
+
 
 console.log(game.progress());
 
-console.log(game.guess("g"));
+console.log(game.guess("k"));
+
+// console.log(game.progress());
+console.log(game.guess("e"));
+console.log(game.guess("l"));
+console.log(game.guess("d"));
+
+
+// console.log(game.progress());
+// console.log(game.progress());
