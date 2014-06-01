3b8b2d08f50a7d463a9012c2b127c1ec2eadd7b3
zackn9ne <zackn9ne@gmail.com>
2014-06-01 06:17:44 +0000

    finishing some testing stuff

--- w06/d02/INSTRUCTORS/airport.rb
+++ w06/d02/INSTRUCTORS/airport.rb
@@ -10,28 +10,12 @@ api_response = {
     }
 }
 
-# I can define a block-taking method EXPLICITLY, like this...
-# def airports_with_weather(data, &block)
-
-# Or IMPLICITLY like this, and use yield() later.
 def airports_with_weather(data)
-    # EXPLICIT: if block.nil?
-    if !block_given?
-        puts "I got all these airports and all this weather, whaddya want me to do?"
-        return nil
-    end
-
-    data["airports"].each_key do |code|
-        weathers = data["weather"].keys.select do |weather|
-            data["weather"][weather].include?(code)
-        end
-
-        # EXPLICIT: block.call(code, weathers)
-        yield(code, weathers)
-    end
+  
+w_airports = airport.select { |color| color.include?("s") }
+puts s_colors.inspect #=> ["chartreuse", "seafoam"]
 end
 
-# Call using a block
 airports_with_weather(api_response) do |code, weathers|
     puts "<section>"
     puts "\t<h1>#{code}</h1>"
@@ -40,38 +24,6 @@ airports_with_weather(api_response) do |code, weathers|
     end
     puts "</section>"
 end
-
-puts
-
-# This variable contains an instance of the Proc class
-print_warning_if_weather_sucks = Proc.new do |code, weathers|
-    if weathers.length > 1
-        puts "DANGER! The weather at #{code} is awful."
-    end
-end
-
-puts "Call using a Proc TURNED INTO a block."
-airports_with_weather(api_response, &print_warning_if_weather_sucks)
-
-puts
-
-# What if I define this using a method?
-def print_warning_if_weather_sucks_method(code, weathers)
-    if weathers.length > 1
-        puts "DANGER! The weather at #{code} is awful."
-    end
-end
-
-# First, turn my method into a Proc
-print_warning_proc = method(:print_warning_if_weather_sucks_method)
-# Now, pass that Proc into my airports function as a block
-
-puts "Call using a method TURNED INTO a Proc TURNED INTO a block"
-airports_with_weather(api_response, &print_warning_proc)
-
-puts
-
-airports_with_weather(api_response)
 
 # <section>
 #     <h1>LAX</h1>
---
+++ w06/d04/Zack_Levine/currencio.js
@@ -0,0 +1,30 @@
+// refactored pounds to dollars
+function poundsToDollars(pounds /*hold for future*/){
+	var result = pounds / 1.5; //do math
+	return result
+}
+poundsToDollars(400)
+
+
+
+
+///figuring out this
+
+function personMaker(name,social){  //two properties
+	this.social = social; //objects socail set to social 
+	this.name = name; //objects name whatever we pass for name
+	// this.yearsUntillRetire = thingsBoutThis; //third property no parenthesis because it already takes all teh paramaters from the object below
+}
+
+var
+
+
+
+
+function thingBoutThis{
+	var numYears = 65 - this.age; //variable name
+	return numYears; //so then you can print it out
+}
+var newGuy = new personMaker ("Steven Jobs", 34434); //add methods to this object, a method is a function that uses these properties
+	
+console.log(natalie.social.
