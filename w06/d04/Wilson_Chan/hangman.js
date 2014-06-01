6a07ee101c28ceb4af6384dbc8aff67828bddbfe
mrwilsondj <mrwilsondj@gmail.com>
2014-05-31 22:59:52 -0400

    Sketched out variable with a single word

--- w06/d04/Wilson_Chan/hangman.js
+++ w06/d04/Wilson_Chan/hangman.js
@@ -1,3 +1,6 @@
+// setting a top level scope variable word 'detective'
+// var targetWord = 'detective',
+
 function Hangman(word){
   this.progress = function(){
 
@@ -11,7 +14,17 @@ function Hangman(word){
 };
 }
 
-var game = new Hangman("hello");
+var game = new Hangman("detective");
 
-console.log(game.progress()); //=>"_ _ _ _ _ "
+// pick a letter at random function
+//targetLetter, a global variable set above, is picking a wordlist from the json file at random.
+//Math.random returns a number in a range from 0 < 1. It will never return 1 itself.
+//and then, we multiply by the numbers in wordlist // Math.floor rounds down to the nearest whole number
+function newLetter() {
+    targetletter = letter[Math.floor(Math.random() * letter.length)];
+}
 
+console.log(game.progress()); //=>"_ _ _ _ _ _ _ _ _"
+// game.guess("a");  //=> false
+// game.guess("t");  //=> true
+// game.progress();  //=> "_ _ t _ _ t _ _ _"
