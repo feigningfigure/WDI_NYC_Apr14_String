3a791210f0ba81da61ae2a2dd25e70a7cc9ae7e0
mrwilsondj <mrwilsondj@gmail.com>
2014-06-02 19:12:18 -0400

    renamed buttons in html and updated js file to match buttons

--- w07/d01/Wilson_Chan/atm-starter/atm.js
+++ w07/d01/Wilson_Chan/atm-starter/atm.js
@@ -4,26 +4,51 @@
 // and all the images and sub-frames have finished loading.
 
 // Set your initial balances for the savings and checking accounts
-
-
+// var balance1 = 0;
+// var balance2 = 0;
+var balance = 100
 
 $(document).ready(function(){
 
   // SEE:  https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
 
   // Create a click event that is raised when the user clicks on the "checkingDeposit" element.
+$('#checking-deposit-button').click(function(){
+    alert("hi there");
+  //     // Clears the input form
+  //   input.val('');
+  // });
 
+});
 
   // Create a click event that is raised when the user clicks on the "savingsDeposit" element.
+$('#checking-withdraw-button').click(function(){
+alert("hi there");
+      // Clears the input form
+  //   input.val('');
+  // });
 
+});
 
   // Create a click event that is raised when the user clicks on the "checkingWithdraw" element.
+$('#savings-deposit-button').click(function(){
+alert("hi there");
+      // Clears the input form
+  //   input.val('');
+  // });
 
+});
 
   // Create a click event that is raised when the user clicks on the "savingsWithdraw" element.
+$('#savings-withdraw-button').click(function(){
+alert("hi there");
+      // Clears the input form
+  //   input.val('');
+  // });
 
+});
 
-};
+})
 
 function withdrawFunds(amount, primary, secondary)
 {
