600a39d3c80078915b7dd4234c56d289661c15e2
feigningfigure <andrew.g.wallace@gmail.com>
2014-06-01 20:59:05 -0400

    removed unnecessary code from working phase 1

--- w06/d04/Andrew_Wallace/hangman.js
+++ w06/d04/Andrew_Wallace/hangman.js
@@ -1,6 +1,7 @@
 function Hangman(word) {
   var wrongGuess = false;
   var badGuesses = 0;
+  var gameOver = false;
   var playerProgress = []
   var wordLength = word.length;
   var letters = word.split('');
@@ -21,8 +22,15 @@ function Hangman(word) {
         wrongGuess = false;
         placeholders[i] = letter;
       } else {
-        badGuesses += 1;
+        badGuesses++;
       }
+    }
+  }
+
+  this.gameOver = function() {
+    if (badGuesses > 5) {
+      console.log("You lose!");
+    } else {
     }
   }
 }
@@ -30,86 +38,3 @@ function Hangman(word) {
 var game = new Hangman("detective");
 // game.progress();
 game.guess("e");
-
-
-
-
-
-
-
-/*
-var wrongGuess = false;
-var badGuesses = 0;
-
-function Hangman(word) {
-
-    var wordLength = word.length;
-    var letters = word.split('');
-    // var lettersArray = [];
-    // lettersArray.push(letters);
-
-    this.progress = function() {
-    //array to hold current state of guessed word
-    var playerProgress = [];
-    // for each letter in the string...
-    for (var i = 0; i < word.length; i++) {
-        playerProgress.push("_");
-    }
-        // use a space to join the underscores back together
-        return playerProgress.join(" "); //array => string
-    }
-
-// First, default the wrong guess to be true before checking letter.  Set the player's progress to placeholders and assume the guess is wrong.
-this.guess = function(letter) {
-  wrongGuess = true;
-
-// Split the placeholders into an array
-placeholders = playerProgress.split('');
-   // loop through the array
-   for (var i = 0; i < wordLength; i++) {
-      // if the guessed letter matches one in the word,
-      // replace the underscore and set wrong guess to false.
-      if (word.charAt(i) == letter.toLowerCase()) {
-       placeholders[i] = letter;
-       wrongGuess = false;
-   }
-
-
-   // if the guess was incorrect, increment the number of bad
-   if (wrongGuess) {
-      badGuesses ++;
-  }
-   // convert the array to a string and display it again
-   playerProgress = placeholders.join('');
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function alterAt ( n, c, originalString ) {
-//     return originalString.substr(0,n) + c + originalString.substr(n+1,originalString.length);
-// }
-
-}
-
-
-// var game = new Hangman("detective");
-
-// game.guess("a");
-// game.guess("t");
-// game.progress();
-
-
-
-*/
-
