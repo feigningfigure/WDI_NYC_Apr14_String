d8d909284798bd4a1ae32b65adb486fe5edf9e3d
jpark1120 <parkj1120@gmail.com>
2014-06-02 18:08:14 -0400

    got simple addition to work

--- w07/d01/Joe_Park/atm-starter/atm.js
+++ w07/d01/Joe_Park/atm-starter/atm.js
@@ -8,10 +8,27 @@
 
 
 $(document).ready(function(){
+	var currentCheckBalance = $('#balance1');
+	var x = parseInt($('#balance1').text().replace('$', ''), 10);
 
   // SEE:  https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
 
   // Create a click event that is raised when the user clicks on the "checkingDeposit" element.
+  
+  $('#chkdeposit').click(function(){
+
+  	var input = $('#transamount');
+
+  	var newBalance = x + parseInt(input.val(), 10);
+  	
+  	currentCheckBalance.html(('$' + newBalance.toString()));
+
+
+	});
+
+
+});
+
 
 
   // Create a click event that is raised when the user clicks on the "savingsDeposit" element.
@@ -22,15 +39,15 @@ $(document).ready(function(){
 
   // Create a click event that is raised when the user clicks on the "savingsWithdraw" element.
 
+	function withdrawFunds(amount, primary, secondary) {
 
-};
+	}
 
-function withdrawFunds(amount, primary, secondary)
-{
+	function updateDisplay() {
 
-}
+	}
 
-function updateDisplay()
-{
+	function depositFunds(amount, primary, secondary) {
+		// currentChkBalance = currentChkBalance
+	}
 
-}
