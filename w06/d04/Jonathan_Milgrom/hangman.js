369b6156750be68cc7ea1863d195fbec2ca46059
jbmilgrom <jbmilgrom@gmail.com>
2014-06-02 09:09:07 -0400

    hw submission w06/d04

--- w06/d04/Jonathan_Milgrom/hangman.js
+++ w06/d04/Jonathan_Milgrom/hangman.js
@@ -1,41 +1,108 @@
-function Hangman(word) {
-	var letter_array = [];
-	function length() {
-		return letter_array.length;
-	}
-	var array = word.split('');
-	
-	this.guess = function(guess_letter){
-		letter_array.push(guess_letter);
-		for (var i=0; i < array.length; i++) {
-			if (array[i] === letter_array[length() - 1]) {
-				return true;
-			} 
-		}
-		return false;
-	};
 
+function Hangman(arrayOfWords) {
+	var correct_guess_array = [];
+	var incorrect_guess_array = [];
+	var gameOn = true;
+	var word = arrayOfWords[randomIndex(arrayOfWords.length)];
+	var array = word.split('');
+	var hintCount = 0;
+	
+	function randomIndex(arrayLength) {
+		return Math.floor(Math.random() * arrayLength);
+	}
+
+	function correct_guess_array_length() {
+		return correct_guess_array.length;
+	}
+
+	function incorrect_guess_array_length() {
+		return incorrect_guess_array.length + hintCount;
+	}
+	
+	function hintCounter() {
+		return hintCount++;
+	}
+
+	this.guess = function(guess_letter){
+		if (incorrect_guess_array_length() < 5 && gameOn === true) {
+			for (var i=0; i < array.length; i++) {
+				if (array[i] === guess_letter) {
+					correct_guess_array.push(guess_letter);
+					return true;
+				} 
+			}
+			if (incorrect_guess_array_length() > 0) {
+				for (var i=0; i < incorrect_guess_array_length(); i++) {
+					if (incorrect_guess_array[i] === guess_letter) {
+						console.log("You guessed this letter already!");
+						return false;
+					}						
+				}
+				incorrect_guess_array.push(guess_letter);
+				return false;	
+			} else {
+				incorrect_guess_array.push(guess_letter);
+				return false;
+			}	
+		} else {
+			console.log("Game is OVER.");
+			console.log("Your Word: " + word)
+			return null;
+		}
+	};
 
 	this.progress = function(){
 		var progress_array = []
 		for (var i=0; i < array.length; i++) {
-			if (length() > 0) {
-				if(array[i] === letter_array[length() - 1]) {
-					progress_array.push(array[i]);
-				} else {
-				progress_array.push('_');
+			progress_array.push('_');
+		}
+		if (correct_guess_array_length() > 0) {
+			for (var i=0; i < array.length; i++) {
+				for (var j=0; j < correct_guess_array_length(); j++) {
+					if(array[i] === correct_guess_array[j]) {
+						progress_array[i] = array[i];
+					} 
 				}
-			}
-			else {
-				progress_array.push('_');
 			}
 		}
 		return progress_array.join(' ');
 	};
+
+	this.giveUp = function () {
+		gameOn = false;
+		return word;
+	}
+
+	this.incorrect = function () {
+		return incorrect_guess_array_length();
+	}
+
+	this.guesses = function() {
+		return correct_guess_array.concat(incorrect_guess_array);
+	}
+
+	//randomly selecting a correct letter
+	this.hint = function() {
+		var localCorrectGuessArray = correct_guess_array;
+		var localWordArray = array;
+		if (correct_guess_array_length() > 0) {
+			for (var i=0; i < array.length; i++) {
+				for (var j=0; j < correct_guess_array_length(); j++) {
+					if(localWordArray[i] === localCorrectGuessArray[j]) {
+						localWordArray.splice(i, 1);
+					} 
+				}
+			}
+		}		
+		var remainingCorrectGuesses = localWordArray;
+		this.guess(remainingCorrectGuesses[randomIndex(remainingCorrectGuesses.length)]);
+		hintCounter();
+		return this.progress;
+	}
 }
 
-var game = new Hangman("detective");
+var game = new Hangman(["detective"]);
+// Storing an instance of the Hangman "function" here - where game = "detective" - instantiates var correct_guess_array (the first line of the body of the function).  When calling other functions internal to Hangman, correct_guess_array does NOT get reset to [], but rather carries the value accumulated over the course of previous internal (i.e. Hangman) function calls.
 
-game.guess('t');
 
 
