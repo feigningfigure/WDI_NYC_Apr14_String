6fbea86e44ef4ad1d748596b274dd1e59d1a99ae
zackn9ne <zackn9ne@gmail.com>
2014-06-02 11:11:39 -0400

    home work

--- w06/d04/Zack_Levine/currencio.js
+++ w06/d04/Zack_Levine/currencio.js
@@ -1,20 +1,65 @@
-// refactored pounds to dollars
-function poundsToDollars(pounds /*hold for future*/){
-	var result = pounds / 1.5; //do math
-	return result
+// trying newest
+//
+function convertCurrency(inCurrency,outCurrency,amount) { 
+
+      //get all math figured out in case
+
+        var usaMath = inCurrency * 1.5;
+    //var ukMath = inCurrency / 1.5
+
+    //callback(the-amount);          
+
+         amount(inCurrency);
+    //     amount(outCurrency);
+
+
+      if (inCurrency == "dollars") {
+        //return it so it can be used
+          
+              return usaMath;
+    } else if {
+          return ukMath;
+    }
+      //end
+
 }
+convertCurrency("dollars","pounds",399)
+
+
+function notify(){
+        console.log("dude yoru moneys fixed")
+          }
+convertCurrency(dollars,pounds,399)
+
+// getting down to previously done stuff
+// refactored pounds to dollars
+
+function poundsToDollars(pounds) { //function declaration, pounds for later 
+    var dollars = pounds / 1.5; //set variable of dollars
+    return dollars
+}
+    
 poundsToDollars(400)
 
+function convertBank(input,output,howMuch) { //function declaration
+    if (input == "dollars" ) {
+            function crunchPounds(howMuch){
+                var dollars = input / 1.5;
+            return dollars
+            }
+            crunchPoundskkkk
+} else if {
+console.log("yer fucked")
+}}
+convertBank("dollars","pounds",333)
 
+    ///figuring out this
 
-
-///figuring out this
-
-function personMaker(name,social){  //two properties
-	this.social = social; //objects socail set to social 
-	this.name = name; //objects name whatever we pass for name
-	// this.yearsUntillRetire = thingsBoutThis; //third property no parenthesis because it already takes all teh paramaters from the object below
-}
+    function personMaker(name,social){  //two properties
+        this.social = social; //objects socail set to social 
+        this.name = name; //objects name whatever we pass for name
+        // this.yearsUntillRetire = thingsBoutThis; //third property no parenthesis because it already takes all teh paramaters from the object below
+    }
 
 var
 
@@ -22,9 +67,9 @@ var
 
 
 function thingBoutThis{
-	var numYears = 65 - this.age; //variable name
-	return numYears; //so then you can print it out
+    var numYears = 65 - this.age; //variable name
+    return numYears; //so then you can print it out
 }
 var newGuy = new personMaker ("Steven Jobs", 34434); //add methods to this object, a method is a function that uses these properties
-	
+
 console.log(natalie.social.
