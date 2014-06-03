34463b8f9cb40e70a49d0d1d6a1c9eda0bc606f6
mrwilsondj <mrwilsondj@gmail.com>
2014-06-03 19:15:14 -0400

    Cleared out db file but now having an endless loop in localhost whcih needs to be fixed

--- w07/d02/Wilson_Chan/sinatra_with_ajax/public/javascripts/scripts.js
+++ w07/d02/Wilson_Chan/sinatra_with_ajax/public/javascripts/scripts.js
@@ -1,50 +1,46 @@
-
 function setEventHandlers(){
 
-	$("#add_quiz_button").click(function(){
-		console.log("Submit button has been clicked");
+$("#add_quiz_button").click(function(){
+	console.log("Submit button has been clicked");
 
-		var $input = $("#quiz_input");
+var $input = $("#quiz_input");
 
-		$.ajax({
-			url: '/quizzes',
-			type: 'POST',
-			dataType: 'json' ,
-			data: {quiz_name: $input.val()},
-		}).done(function(data){
-			console.log(data);
-			alert("You have added " + data);
-			$input.val('');
-		});
-
+$.ajax({
+	url: '/quizzes',
+	type: 'POST',
+	dataType: 'json' ,
+	data: {quiz_name: $input.val()},
+	}).done(function(data){
+	console.log(data);
+	alert("You have added " + data);
+	$input.val('');
 	});
 
-	var $show_button = $("#show_button");
+});
+
+var $show_button = $("#show_button");
 
 	$show_button.click(function(){
-		console.log("Show button has been clicked");
-#setInterval(function(){
+	console.log("Show button has been clicked");
 
-		$.ajax({
-			url: '/quizzes',
-			type: 'GET',
-			dataType: 'json',
-		}).done(function(data){
-			console.log(data);
-			var $quiz_list = $("#quiz_list");
-			$quiz_list.empty();
-			data.quizzes.forEach(function(item){
-				$quiz_list.append("<li>"+ item["title"] +"</li>")
-			})
-		})
-	});
+	setInterval(function(){
+
+$.ajax({
+	url: '/quizzes',
+	type: 'GET',
+	dataType: 'json',
+	}).done(function(data){
+	console.log(data);
+var $quiz_list = $("#quiz_list");
+	$quiz_list.empty();
+	data.quizzes.forEach(function(item){
+	$quiz_list.append("<li>"+ item["title"] +"</li>")
+}, 5000);
+})
+})
+} );
 
 }
-
-}, 6000);
-
-
-
 
 $( document ).ready(function(){
 
