27f0a9049c95c36392607ae016edd6e65b4ea38c
Crustacean87! <declanv@gmail.com>
2014-06-02 23:26:58 -0400

    Adding deposit and withdrawal methods for checking account

--- w07/d01/Declan_Van_Welie/atm-starter/atm.js
+++ w07/d01/Declan_Van_Welie/atm-starter/atm.js
@@ -5,6 +5,8 @@
 
 // Set your initial balances for the savings and checking accounts
 
+var checking_balance = 0;
+var savings_balance = 0;
 
 
 $(document).ready(function(){
@@ -13,17 +15,56 @@ $(document).ready(function(){
 
   // Create a click event that is raised when the user clicks on the "checkingDeposit" element.
 
+$("#checking_deposit").click(function(){
+
+var input = $("#check_amount_input").val();
+
+amount_entered = parseInt(input);
+
+
+
+$("#balance1").text(amount_entered);
+
+
+
+});
 
   // Create a click event that is raised when the user clicks on the "savingsDeposit" element.
+$("#savings_deposit").click(function(){
 
+var input = $("#savings_amount_input").val();
+
+amount_entered = parseInt(input);
+
+
+
+$("#balance2").text(amount_entered);
+
+
+
+});
 
   // Create a click event that is raised when the user clicks on the "checkingWithdraw" element.
+
+$("#checking_withdrawal").click(function(){
+
+var input = $("#check_amount_input").val();
+
+amount_entered = parseInt(input);
+
+
+
+$("#balance1").text(amount_entered);
+
+
+
+});
+
 
 
   // Create a click event that is raised when the user clicks on the "savingsWithdraw" element.
 
 
-};
 
 function withdrawFunds(amount, primary, secondary)
 {
@@ -34,3 +75,5 @@ function updateDisplay()
 {
 
 }
+
+});
