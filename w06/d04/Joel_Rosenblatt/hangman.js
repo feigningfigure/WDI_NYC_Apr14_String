1571f6f677b72829135fe1595bcc8a59ae2f2c26
KLIMTHAUS <jojoel10@gmail.com>
2014-06-02 21:39:07 -0400

    corrected git issues

--- w06/d04/Joel_Rosenblatt/hangman.js
+++ w06/d04/Joel_Rosenblatt/hangman.js
@@ -2,45 +2,99 @@ var filmsArray = ["Killer's Kiss", "The Killing", "Paths of Glory", "Spartacus",
 
 function Hangman() {
 
-  //Rnadom choice of film
   var correct_guesses = new Array();
   var random_choice = filmsArray[Math.floor(Math.random() * filmsArray.length)];
   var word = random_choice.toLowerCase();
+  var playerProgress = [];
+  var attempts = 0;
+  var guesses = [];
+  var hint_counter = 0;
+  var correct_answer = word.split("").join(' ');
 
   //Split film
   var wordArray = word.split("");
 
-  //Guess function
-  this.guess = function(user_guess){
-    wordArray.forEach(function(letter) {
-      if (letter === user_guess) {
-        correct_guesses.push(user_guess);
-        return true;
+  this.progress = function(){
+    if (attempts < 5) {
+      if (playerProgress.length === 0) {
+        for (var i = 0; i < word.length; i++) {
+          playerProgress.push("_");
+        }
+      } else if (correct_answer === playerProgress.join(' ')) {
+        return word + " Woot woot!";
       }
-      else {
-        return false;
+      return playerProgress.join(" ");
+    } else if (attempts >= 5) {
+      return word + " Wah Wah";
+    }
+  };
+
+  this.guess = function(letter) {
+    if (attempts < 5){
+      if (word.indexOf(letter) !== -1) {
+        for (var i = 0; i < word.length; i++) {
+          if (word[i] === letter) {
+            playerProgress[i] = letter;
+          }
+        }
+        return true
+      } else if (word.indexOf(letter) === -1){
+        if (guesses.indexOf(letter) === -1){
+          attempts++;
+          guesses.push(letter);
+        }
+        return false
       }
-    });
+    } else {
+      playerProgress = word.split("", word.length);
+      return null
+    }
+  };
+
+
+  this.giveup = function() {
+    attempts = 5;
+    playerProgress = word.split("", word.length);
+    return word
   }
 
-  //Actual game
-  this.progress = function() {
-    wordArray.forEach(function(letter) {
-        if (letter === " ") {
-          console.log("/");
-        }
-        else if (correct_guesses.forEach(function(guess) {letter ===  guess;})){
-          console.log(user_guess);
-        }
-        else {
-          console.log("_");
-        }
-    });
+  this.incorrect = function() {
+    return attempts
+  }
+
+  this.guesses = function() {
+    return guesses
+  }
+
+  this.hint = function(){
+    var random = Math.floor(Math.random() * word.length);;
+    var letter = word[random];
+
+    if (playerProgress.indexOf(letter) === -1){
+      if (hint_counter === 0) {
+        this.guess(letter);
+        hint_counter++;
+      } else {
+        this.guess(letter);
+        attempts++;
+      }
+    } else {
+      this.hint();
+    }
+    return letter
   };
+
+  function randomWord(words) {
+    return words[Math.floor(Math.random() * words.length)];
+  }
+
 }
 
 var game = new Hangman();
-game.progress();
+console.log(game.progress());
+console.log(game.guess("a"));
+console.log(game.progress());
+
 
 // Array.prototype.some()
 // Returns true if at least one element in this array satisfies the provided testing function.
