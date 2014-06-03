429dc8cdd27a8e885b165499babfa4d1f4083d20
daramao <dara.mao@gmail.com>
2014-06-02 22:00:53 -0400

    hw atm deposit sum works

--- w07/d01/Dara_Mao/atm-starter/atm.js
+++ w07/d01/Dara_Mao/atm-starter/atm.js
@@ -6,18 +6,40 @@
 // Set your initial balances for the savings and checking accounts
 
 $(document).ready(function(){
-  //Deposit balance
+  //Checking Deposit balance
   var checking_balance = $("#checkingAccount > div");
-
-  $($("#checkingAccount > input:button:first")).click(function(){
-
-//Deposit field
   var checking_input = $("#checkingAccount > input:text");
-  checking_balance.append(checking_input.val());
+  var checking_sum = 0;
 
-  checking_input.val("");
+  //Deposit button
+  $($("#checkingAccount > input:button:first")).click(function(){
+  //Checking Deposit input number field
+    checking_sum = checking_sum + parseFloat(checking_input.val());
+    checking_balance.html(checking_sum);
+    checking_balance.val("");
+    checking_input.val("");
   });
 });
+
+function updateDisplay()
+{
+// checking_sum
+}
+
+// //Checking Withdrawal button
+// $("#checkingAccount > input:button:last")
+
+// //SAVINGS Withdrawal button
+// $("#savingsAccount > input:button:last")
+
+// //SAVINGS Deposit button
+// $("#savingsAccount > input:button:first")
+
+// //SAVINGS Deposit field
+// $("#savingsAccount > input:text")
+
+// //SAVINGS Deposit
+// $("#savingsAccount > div")
 
 
 // function withdrawFunds(amount, primary, secondary)
@@ -25,19 +47,27 @@ $(document).ready(function(){
 
 // }
 
-// function updateDisplay()
-// {
 
+
+// SEE:  https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
+
+// Create a click event that is raised when the user clicks on the "checkingDeposit" element.
+
+// Create a click event that is raised when the user clicks on the "savingsDeposit" element.
+
+
+// Create a click event that is raised when the user clicks on the "checkingWithdraw" element.
+
+
+// Create a click event that is raised when the user clicks on the "savingsWithdraw" element.
+
+// var sum = 0;
+// $("checking_input").each function(){
+  //   sum += Number(checking_input.val());
 // }
 
-  // SEE:  https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
-
-  // Create a click event that is raised when the user clicks on the "checkingDeposit" element.
-
-    // Create a click event that is raised when the user clicks on the "savingsDeposit" element.
 
 
-  // Create a click event that is raised when the user clicks on the "checkingWithdraw" element.
 
 
-  // Create a click event that is raised when the user clicks on the "savingsWithdraw" element.
+
