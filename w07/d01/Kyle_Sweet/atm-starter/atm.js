063e8bc66a4c68bcb4c4bbf29c94bf14c778ac28
kssweet1 <kssweet@gmail.com>
2014-06-03 09:01:37 -0400

    w07d01 HW

--- w07/d01/Kyle_Sweet/atm-starter/atm.js
+++ w07/d01/Kyle_Sweet/atm-starter/atm.js
@@ -4,31 +4,77 @@
 // and all the images and sub-frames have finished loading.
 
 // Set your initial balances for the savings and checking accounts
-
-
+var checking = 0;
+var savings = 0; 
 
 $(document).ready(function(){
 
   // SEE:  https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
 
   // Create a click event that is raised when the user clicks on the "checkingDeposit" element.
+  $("#checkingDeposit").click(function(){
+    
+    var input = $("#checkingValue");
+
+    $("#balance1").html( "$" + input.val() );
+
+    input.val('');
+
+    });
 
 
   // Create a click event that is raised when the user clicks on the "savingsDeposit" element.
 
+    $("#savingsDeposit").click(function(){
+    
+    var input = $("#savingsValue");
+
+    $("#balance2").html( "$" + input.val() );
+
+    input.val('');
+
+    });
+
+
+
 
   // Create a click event that is raised when the user clicks on the "checkingWithdraw" element.
+    $("#checkingWithdraw").click(function(){
+    
+    var input = $("#checkingValue");
+
+    $("#balance1").html( "$" + input.val() );
+
+    input.val('');
+
+    });
 
 
-  // Create a click event that is raised when the user clicks on the "savingsWithdraw" element.
+//   // Create a click event that is raised when the user clicks on the "savingsWithdraw" element.
+
+    $("#savingsWithdraw").click(function(){
+    
+    var input = $("#savingsValue");
+
+    $("#balance2").html( "$" + input.val() );
+
+    input.val('');
+
+    });
+});
 
 
+
+function withdrawFunds(amount, primary, secondary){
+
+if ( $("#checkingWithdraw").click ){
+  checking = checkingValue;
+} 
+
+else if ($("#savingsWithdraw").click ) {
+  console.log("false");
 };
 
-function withdrawFunds(amount, primary, secondary)
-{
-
-}
 
 function updateDisplay()
 {
