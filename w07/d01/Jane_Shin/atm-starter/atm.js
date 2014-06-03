b61e69e8564a5b28565b79e7fbc0e31351891290
janeplural <mlle.jihshi@gmail.com>
2014-06-03 09:03:27 -0400

    atm hw submission

--- w07/d01/Jane_Shin/atm-starter/atm.js
+++ w07/d01/Jane_Shin/atm-starter/atm.js
@@ -14,63 +14,106 @@ $(document).ready(function(){
   // Create a click event that is raised when the user clicks on the "checkingDeposit" element.
 
   var checkRaw = $("#balance1");
-  var balance = Number(checkRaw.text().substr(1));
+  var checkBalance = Number(checkRaw.text().substr(1));
   var checkAmount = $("#checking_amount");
   // var checkValue = Number(checkAmount.val());
+  var saveRaw = $("#balance2");
+  var saveBalance = Number(saveRaw.text().substr(1));
+  var saveAmount = $("#saving_amount");
+  // var checkValue = Number(checkAmount.val());
+
+  if (checkBalance === 0) {
+    $("#balance1").css('background-color','red');
+  } 
+  if (saveBalance === 0) {
+    $("#balance2").css('background-color','red');
+  } 
 
   $("#make_checking_deposit").on('click', function() {
-		balance += Number(checkAmount.val());
-  	updateDisplay(checkRaw, balance);
+		checkBalance += Number(checkAmount.val());
+  	updateDisplay(checkRaw, checkBalance);
   });
 
   // changed order of checking withdraw
 
- $("#make_checking_withdrawal").on('click', function() {
-
- 	// if (balance === 0)
-
-  
-	 	if (balance - checkAmount.val() >= 0) {
-	  	withdrawFunds(checkRaw, checkAmount);	
-			balance -= checkAmount.val();
-	  	updateDisplay(checkRaw, balance);
-	 	} else {
-	 		alert ("You don't have enough funds to withdraw.");
-	 	}
-
-
-
+  $("#make_checking_withdrawal").on('click', function() {
+    if (Number(checkAmount.val()) < checkBalance) { 	
+      if (checkBalance - checkAmount.val() >= 0) {
+  	  	withdrawFunds(checkRaw, checkAmount);	
+  			checkBalance -= checkAmount.val();
+  	  	updateDisplay(checkRaw, checkBalance);
+      }else if (checkBalance+saveBalance >=  checkAmount.val()) {
+        withdrawFunds(checkAmount, checkRaw, saveRaw); 
+      // checkBalance -= checkAmount.val();
+        updateDisplay(checkRaw, checkBalance);
+    	} else {
+    	 	alert ("You don't have enough funds to withdraw.");
+    	}
+    } else if (Number(checkAmount.val()) < (checkBalance + saveBalance)) {
+      withdrawFunds(checkAmount, checkRaw, saveRaw); 
+      saveBalance -= checkAmount.val();
+      updateDisplay(checkRaw, checkBalance);
+    } else {
+      alert ("come back when you get paid");
+    }
   });
 
   // Create a click event that is raised when the user clicks on the "savingsDeposit" element.
 
-  var saveRaw = $("#balance2");  
-
+  // var saveRaw = $("#balance2");  
   $("#make_saving_deposit").on('click', function() {
+  // 	var depositAmount = $("#saving_amount");
+  // 	var balance = Number(saveRaw.text().substr(1));	
+		// balance += Number(depositAmount.val());
+  // 	updateDisplay(saveRaw, balance);
 
-  	var depositAmount = $("#saving_amount");
-  	var balance = Number(saveRaw.text().substr(1));	
-		
-		balance += Number(depositAmount.val());
-
-  	updateDisplay(saveRaw, balance);
+    saveBalance += Number(saveAmount.val());
+    updateDisplay(saveRaw, saveBalance);
 
   });
 
   // Create a click event that is raised when the user clicks on the "checkingWithdraw" element.
 
   // Create a click event that is raised when the user clicks on the "savingsWithdraw" element.
+
+ $("#make_saving_withdrawal").on('click', function() {
+  if (saveBalance - saveAmount.val() >= 0) {
+      withdrawFunds(saveRaw, saveAmount); 
+      saveBalance -= saveAmount.val();
+      updateDisplay(saveRaw, saveBalance);
+   } else {
+      alert ("You don't have enough funds to withdraw.");
+    }
+  });
+ 
 });
 
-function withdrawFunds(amount, primary, secondary) {
-	var balance = Number(amount.text().substr(1));	
-	balance -= Number(primary.val());
+function withdrawFunds(amount, primary_account, secondary_account) {
+	var balance = Number(primary_account.text().substr(1));
+    balance -= Number(amount.val());
+
+  if (balance < amount) {
+    var secondary_balance = Number(secondary_account.text().substr(1));
+    balance = secondary_balance + balance
+  }
+
+  // if (balance >= amount) {
+	 // balance -= Number(amount.val());
+  // } else {
+  //   var secondary_balance = Number(secondary_account.text().substr(1));
+  //   balance -= Number(amount.val());
+  // }
 	// return balance;
-}
+};
 
 function updateDisplay(raw_dollars, balance) {
 	// var balance = Number(raw_dollars.text().substr(1));	
 	// balance += Number(depositAmount.val());
   raw_dollars.text('$' + balance);
+  if (balance === 0) {
+    raw_dollars.css('background-color','red');
+  } else {
+    raw_dollars.css('background-color','#E3E3E3');    
+  }
 };
 
