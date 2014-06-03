56ed5175aed80199990b2979356e2c445dbe3c77
lunsee <konancys@gmail.com>
2014-06-03 17:48:21 -0400

    Get hangman.js to a working state

--- w06/d04/Nancy_Ko/hangman.js
+++ w06/d04/Nancy_Ko/hangman.js
@@ -1,30 +1,25 @@
 function Hangman(word) {
-	// FILL IN
-//find out how many letters
-//return the num of letters  
-// print out _ same number as letters 
-// put space in between each _ 
-// word.length
-// puts "_" number of length times 
+  // FILL IN
+  //find out how many letters
+  //return the num of letters  
+  // print out _ same number as letters 
+  // put space in between each _ 
+  // word.length
+  // puts "_" number of length times 
 	this.progress = function() { 
-		// FILL IN
-		var playProgress = []
-  for (var i = 0; i < word.length; i++) {
-  		playProgress.push("_")	
-  }
-  return playProgress.join(" ");
-  } 
+		var playProgress = [];
+    for (var i = 0; i < word.length; i++) {
+    		playProgress.push("_");
+    }
+    return playProgress.join(" ");
+  };
 
-   this.guess = function(letter) {
-    var singleCharacter = []
-    var get
-  if(letter.search(singleCharacter) ===  
-  }			
-
-};
+  // this.guess = function(letter) {
+  //   var singleCharacter = []
+  //   var get
+  //   if(letter.search(singleCharacter) ===  
+  //   }			
 }
-
-
 	 
    // var result = "", // initialize list
    //     i;
@@ -34,14 +29,13 @@ function Hangman(word) {
    // }
    // return result;
 
-
 var game = new Hangman("detective");
 
 console.log(game.progress()); //=> " _ _ _ _ _ _ _ _ _"
 
-var game = new Hangman("gandhi");
+// var game = new Hangman("gandhi");
 
-console.log(game.progress()); //=> "_ _ _ _ _ _ "
+// console.log(game.progress()); //=> "_ _ _ _ _ _ "
 
-console.log(game.guess("a"); //=> false
+// console.log(game.guess("a"); //=> false
 
