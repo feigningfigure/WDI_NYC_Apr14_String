a6769c15d725cf97b2dbe5bfb3962a84c1d92e87
benticsay <benticsay@gmail.com>
2014-06-03 09:05:50 -0400

    w07 d01 hw

--- w07/d01/Ben_Ticsay/atm-starter/atm.js
+++ w07/d01/Ben_Ticsay/atm-starter/atm.js
@@ -1,3 +1,6 @@
+// STILL REBOUNDING FROM WEEKEND ILLNESS. I WILL SUBMIT A COMPLETED VERSION THIS WEEK. 
+
+
 // https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onload
 // The load event fires at the end of the document loading process.
 // At this point, all of the objects in the document are in the DOM,
@@ -5,7 +8,10 @@
 
 // Set your initial balances for the savings and checking accounts
 
-
+var balance2 = 0;
+var balance1 = 0;
+var checkingInput = $('checkingInput')
+var savingsInput = $('savingInput')
 
 $(document).ready(function(){
 
@@ -14,14 +20,34 @@ $(document).ready(function(){
   // Create a click event that is raised when the user clicks on the "checkingDeposit" element.
 
 
-  // Create a click event that is raised when the user clicks on the "savingsDeposit" element.
+$('#checkingDeposit').click(function(){
+      var input = $('#checkingInput');
+      balance1 += parseInt(input.val());
+      input.val('');
+      updateCheckingDisplay(balance1)
+    })
 
+  // Create a click event that is raised when the user clicks on the "savingsDeposit" element.
+$("#savingsDeposit").click(function(){
+   var input = $("#savingsInput");
+    balance2 += parseInt(input.val());
+    updateSavingsDisplay(balance2);
+    input.val('')
+   }
 
   // Create a click event that is raised when the user clicks on the "checkingWithdraw" element.
-
+$("#checkingWithdraw").click(function(){
+    var input = $("#checkingInput");
+    balance1 -= parseInt(input.val());
+    input.val('')
+   }
 
   // Create a click event that is raised when the user clicks on the "savingsWithdraw" element.
-
+$("#savingsWithdraw").click(function(){
+    var input = $("#savingsInput");
+    balance2 -= parseInt(input.val());
+    input.val('')
+   }
 
 };
 
@@ -30,7 +56,12 @@ function withdrawFunds(amount, primary, secondary)
 
 }
 
-function updateDisplay()
-{
-
+function updateCheckingDisplay() {
+  $("#balance1").html(balance1);
 }
+
+function updateSavingsDisplay() {
+  $("#balance2").html(balance2);
+}
+
+
