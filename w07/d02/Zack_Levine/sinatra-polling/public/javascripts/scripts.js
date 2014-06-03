6e71dc7db99e39f6a485769f63ce8856c4813579
zackn9ne <zackn9ne@gmail.com>
2014-06-03 18:36:05 -0400

    its working now I need to add the wrap

---
+++ w07/d02/Zack_Levine/sinatra-polling/public/javascripts/scripts.js
@@ -0,0 +1,64 @@
+function setEventHandlers(){
+
+	$("#add-quiz-button").click(function(){
+		console.log("Submit button has been clicked");
+
+		var $input = $("#quiz_input");
+
+		$.ajax({
+			url: '/quizzes',
+			type: 'POST',
+			dataType: 'json' ,
+			data: {quiz_name: $input.val()},
+		}).done(function(data){
+			console.log(data);
+			alert("You have added " + data);
+			$input.val('');
+		});
+
+	});
+
+	var $show_button = $("#show-button");
+
+	$show_button.click(function(){
+		console.log("Show button has been clicked");
+
+
+		$.ajax({
+			url: '/quizzes',
+			type: 'GET',
+			dataType: 'json',
+		}).done(function(data){
+			console.log(data);
+			var $quiz_list = $("#quiz_list");
+			$quiz_list.empty();
+			data.quizzes.forEach(function(item){
+				$quiz_list.append("<li>"+ item["title"] +"</li>")
+			})
+		})	
+	});
+
+}
+
+
+//testing setInterval
+var nIntervId;
+ 
+function changeColor() {
+  nIntervId = setInterval(flashText, 500);
+}
+ 
+function flashText() {
+  var oElem = document.getElementById("my_box");
+  oElem.style.color = oElem.style.color == "red" ? "blue" : "red";
+}
+ 
+function stopTextColor() {
+  clearInterval(nIntervId);
+}
+
+$( document ).ready(function(){
+
+	setEventHandlers();
+
+});
