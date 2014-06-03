deb031cd51966cb5156d4f76e77a98b6fb78da12
daramao <dara.mao@gmail.com>
2014-06-02 23:40:02 -0400

    hw atm deposit, withdrawal, and bonus completed

--- w07/d01/Dara_Mao/atm-starter/atm.js
+++ w07/d01/Dara_Mao/atm-starter/atm.js
@@ -1,45 +1,102 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onload
-// The load event fires at the end of the document loading process.
-// At this point, all of the objects in the document are in the DOM,
-// and all the images and sub-frames have finished loading.
-
-// Set your initial balances for the savings and checking accounts
-
 $(document).ready(function(){
-  //Checking Deposit balance
-  var checking_balance = $("#checkingAccount > div");
-  var checking_input = $("#checkingAccount > input:text");
-  var checking_sum = 0;
+    //Checking Deposit/Withdrawal Total Balance
+    var checking_balance = $("#checkingAccount > div");
+    var checking_input = $("#checkingAccount > input:text");
+    var checking_sum = 0;
 
-  //Deposit button
-  $($("#checkingAccount > input:button:first")).click(function(){
-  //Checking Deposit input number field
-    checking_sum = checking_sum + parseFloat(checking_input.val());
-    checking_balance.html(checking_sum);
-    checking_balance.val("");
-    checking_input.val("");
-  });
+    //Saving Deposit/Withdrawal Total Balance
+    var savings_balance = $("#savingsAccount > div");
+    var savings_input = $("#savingsAccount > input:text");
+    var savings_sum = 0;
+
+    //Checking Deposit Click Event
+    $($("#checkingAccount > input:button:first")).click(function(){
+        //Checking Deposit Update Balance
+        if (!isNaN(checking_input.val()) && checking_input.val() > 0 ){
+            checking_sum += parseFloat(checking_input.val());
+        } else {
+            alert("Please enter a valid deposit!");
+        }
+        checking_balance.html(checking_sum);
+        checking_input.val("");
+    //Balance color turns red when 0
+        if (checking_sum === 0){
+            checking_balance.css("color","red");
+        } else {
+            checking_balance.css("color","grey");
+        }
+    });
+
+    //Checking Withdrawal Click Event
+    $($("#checkingAccount > input:button:last")).click(function(){
+        //Checking Withdrawal Update Balance
+
+        if (checking_input.val() > checking_balance.html()){
+            alert("Not enough money in your account!")
+        } else if (!isNaN(checking_input.val()) && checking_input.val() > 0){
+            checking_sum -= parseFloat(checking_input.val());
+        } else {
+            alert("Please enter a valid withdrawal amount!")
+        }
+        checking_balance.html(checking_sum);
+        checking_input.val("");
+    //Balance color turns red when 0
+        if (checking_sum === 0){
+            checking_balance.css("color","red");
+        } else {
+            checking_balance.css("color","grey");
+        }
+    });
+
+
+
+    //Savings Deposit Click Event
+    $($("#savingsAccount > input:button:first")).click(function(){
+        //Savings Deposit Update Balance
+        if (!isNaN(savings_input.val()) && savings_input.val() > 0 ){
+            savings_sum += parseFloat(savings_input.val());
+        } else {
+            alert("Please enter a valid deposit!");
+        }
+        savings_balance.html(savings_sum);
+        savings_input.val("");
+    //Balance color turns red when 0
+        if (savings_sum === 0){
+            savings_balance.css("color","red");
+        } else {
+            savings_balance.css("color","grey");
+        }
+    });
+
+    //Savings Withdrawal Click Event
+    $($("#savingsAccount > input:button:last")).click(function(){
+        //Savings Withdrawal Update Balance
+        if (savings_input.val() > savings_balance.html()){
+            alert("Not enough money in your account!")
+        } else if (!isNaN(savings_input.val()) && savings_input.val() > 0){
+            savings_sum -= parseFloat(savings_input.val());
+        } else {
+            alert("Please enter a valid withdrawal amount!")
+        }
+
+        savings_balance.html(savings_sum);
+        savings_input.val("");
+         //Balance color turns red when 0
+        if (savings_sum === 0){
+            savings_balance.css("color","red");
+        } else {
+            savings_balance.css("color","grey");
+        }
+    });
 });
+
+
+
 
 function updateDisplay()
 {
-// checking_sum
+    // checking_sum
 }
-
-// //Checking Withdrawal button
-// $("#checkingAccount > input:button:last")
-
-// //SAVINGS Withdrawal button
-// $("#savingsAccount > input:button:last")
-
-// //SAVINGS Deposit button
-// $("#savingsAccount > input:button:first")
-
-// //SAVINGS Deposit field
-// $("#savingsAccount > input:text")
-
-// //SAVINGS Deposit
-// $("#savingsAccount > div")
 
 
 // function withdrawFunds(amount, primary, secondary)
@@ -50,21 +107,6 @@ function updateDisplay()
 
 
 // SEE:  https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onclick
-
-// Create a click event that is raised when the user clicks on the "checkingDeposit" element.
-
-// Create a click event that is raised when the user clicks on the "savingsDeposit" element.
-
-
-// Create a click event that is raised when the user clicks on the "checkingWithdraw" element.
-
-
-// Create a click event that is raised when the user clicks on the "savingsWithdraw" element.
-
-// var sum = 0;
-// $("checking_input").each function(){
-  //   sum += Number(checking_input.val());
-// }
 
 
 
