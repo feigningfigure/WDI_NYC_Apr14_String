1a19d3f33a40dd72ec8cb55d6e10ef4a77ef870e
Mannymin <Mannymin@gmail.com>
2014-06-03 18:21:54 -0400

    w07 d02

---
+++ w07/d02/Emmanuel_Tucker/mental.js
@@ -0,0 +1,12 @@
+
+
+     var ruby  =
+
+
+
+
+
+
+ruby { :title => "Pacific Rim", :release_year => 2013, :abstract => "Giant robots beating the snot out of giant monsters" }
+
+javascript { "title": "Pacific Rim", "release_year": 2013, "abstract": "Giant robots beating the snot out of giant monsters" }
