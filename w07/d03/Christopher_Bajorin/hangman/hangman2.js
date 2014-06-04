d9c760f454d72dbed96130d78c332ca6367eba19
cdbajorin <christopher.bajorin@gmail.com>
2014-06-04 14:46:50 -0400

    prepull

---
+++ w07/d03/Christopher_Bajorin/hangman/hangman2.js
@@ -0,0 +1,45 @@
+function Hangman(word) {
+  var wordArray = word.split('');
+  // console.log(wordArray);
+  var incorrect = 0;
+  this.progress = function() {
+      var guesses = [];
+
+      word.split("").forEach(function(letter) {
+          guesses.push("_");
+      });
+      return guesses.join(" ");
+  };
+
+  this.guess = function(letter) {
+    wordArray.forEach(function(item) {
+      if (item === letter) {
+        console.log(letter);
+      }
+      else {
+
+      }
+      guesses.push(letter);
+    });
+  }
+}
+
+var game = new Hangman("detective");
+
+console.log("_ _ _ _ _ _ _ _ _");
+console.log(game.progress());
+
+console.log(false);
+console.log(game.guess("a"));
+
+console.log(true);
+console.log(game.guess("d"));
+
+// console.log("d _ _ _ _ _ _ _ _");
+// console.log(game.progress());
+
+// console.log(true);
+// console.log(game.guess("t"));
+
+// console.log("d _ t _ _ t _ _ _");
+// console.log(game.progress());
