a5f58f7a557db7385cb52401a50c250fb90265de
jpark1120 <parkj1120@gmail.com>
2014-06-04 11:20:49 -0400

    working on hangman and iterating over string to look for letters

---
+++ w07/d03/Joe_Park/hangman.js
@@ -0,0 +1,44 @@
+function Hangman(word){
+	
+	var guesses = [];
+
+	this.progress = function(){
+		if (guesses.length === 0) {	
+			word.split("").forEach(function(letter){
+				guesses.push("_");
+			});
+		}
+		return guesses.join(" ");
+	};
+
+	this.guess = function(letter){
+		if ( word.indexOf(letter) !== -1) {
+			var i = 0
+			guesses[i] = letter;
+			return true;
+		} else {
+			return false;
+		}
+	};
+}
+
+var game = new Hangman("detective");
+
+
+console.log("_ _ _ _ _ _ _ _ _");
+console.log(game.progress());
+
+console.log(false);
+console.log(game.guess("a"));
+
+console.log(true);
+console.log(game.guess("d"));
+
+console.log("d _ _ _ _ _ _ _ _");
+console.log(game.progress());
+
+console.log(true);
+console.log(game.guess("t"));
+
+// console.log("d _ t _ _ t _ _ _");
+// console.log(game.progress());
