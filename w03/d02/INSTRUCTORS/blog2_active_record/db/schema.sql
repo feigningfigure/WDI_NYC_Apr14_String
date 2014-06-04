09508defdb2569900f169b718b7b82a163805a36
benticsay <benticsay@gmail.com>
2014-06-04 09:48:49 -0400

    UNDID ACCIDENTAL CHANGES TO INSTRUCTOR FILES in attempt to correct pull request issues

--- w03/d02/INSTRUCTORS/blog2_active_record/db/schema.sql
+++ w03/d02/INSTRUCTORS/blog2_active_record/db/schema.sql
@@ -8,11 +8,3 @@ CREATE TABLE posts
   body  text,
   author varchar(255)
 );
-
-CREATE TABLE posts
-(
-  id  serial NOT NULL primary key,
-  user_name varchar(255),
-  description varchar(255),
-  url  text
-);
