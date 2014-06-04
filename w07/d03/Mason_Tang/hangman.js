aa869edf8a5d2382fb5d15aca15635f21acab0c5
masewillis <mwillistang@gmail.com>
2014-06-04 14:49:59 -0400

    classwork

---
+++ w07/d03/Mason_Tang/hangman.js
@@ -0,0 +1,41 @@
+function Hangman(word) {
+  this.progress = function() {
+    var guesses = [];
+
+    word.split("").forEach(function(letter) {
+      guesses.push("_");
+
+
+    });
+
+    return guesses.join(" ");
+  }
+  word_array = word.split("")
+
+  this.guess = function(letter) {
+
+    for(var i=0; i < word_array.length; i++)
+
+    if(letter === word_array[i]) {
+      console.log("true")
+    } else {
+      console.log("false")
+    }
+
+  };
+}
+
+var game = new Hangman("detective");
+
+
+//this came first
+console.log("_ _ _ _ _ _ _ _ _");
+console.log(game.progress());
+
+
+// console.log(false);
+console.log(game.guess("e"));
+
+
+
+// big picture then code. work out--> in
