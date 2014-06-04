2fba0e5da9595945bad9ab8c02aca9e56268048b
LizCira <liz.cira.goldstein@gmail.com>
2014-06-04 11:13:02 -0400

    comitting before beginning guess function

--- w07/d03/Liz_Goldstein/hangman2.js
+++ w07/d03/Liz_Goldstein/hangman2.js
@@ -14,4 +14,17 @@ var game = new Hangman("detective");
 console.log("___________");
 console.log(game.progress());
 
+console.log(false)
+console.log(game.guess("a"));
 
+// console.log(true)
+// console.log(game.guess("d"));
+
+// console.log("d__________");
+// console.log(game.progress());
+
+// console.log(true)
+// console.log(game.guess("t"));
+
+// console.log("d_t_t__t___");
+// console.log(game.progress());
