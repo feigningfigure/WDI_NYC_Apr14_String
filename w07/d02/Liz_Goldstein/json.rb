41b35e76a30f1506f4289aff23bedad594d55174
LizCira <liz.cira.goldstein@gmail.com>
2014-06-04 09:03:27 -0400

    added polling to in class ajax example

---
+++ w07/d02/Liz_Goldstein/json.rb
@@ -0,0 +1,34 @@
+require 'json'
+
+data = {
+    :quizzes => [
+        {
+            :id => 1,
+            :name => "What Game of Thrones character are you?",
+            :questions => [
+                {
+                    :id => 1,
+                    :text => "What would you rather do?",
+                    :choices => [
+                        "Drink and make merry",
+                        "Have, like, a ton of honor",
+                        "Be a terrible person",
+                        "Swear a lot and bathe rarely"
+                    ]
+                },
+                {
+                    :id => 2,
+                    :text => "Are you all about family events?",
+                    :choices => ["Yes", "No"]
+                }
+            ]
+        }
+    ]
+}
+
+
+puts data.inspect()
+puts
+puts data.to_json()
+puts
+puts data.to_json().inspect()
