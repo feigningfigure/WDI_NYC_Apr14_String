0fc798601b93c0641718d9720975ed9ad596b665
lunsee <konancys@gmail.com>
2014-06-04 23:29:37 -0400

    no error

--- w07/d03/Nancy_Ko/facebook_ajax/main.rb
+++ w07/d03/Nancy_Ko/facebook_ajax/main.rb
@@ -6,6 +6,32 @@ require 'ap'
 require 'httparty'
 
 
+FILENAME = "faces_db"
+
+def get_data()
+
+	if File.exists?(FILENAME)
+		results = JSON.parse(File.read(FILENAME))
+		return results
+
+	else
+		{"friends" => []}
+	end
+
+end
+
+
+
+def save_data(data)
+
+	raw = JSON.dump(data)
+
+	File.open(FILENAME, 'w') do |file|
+
+		file.write(raw)
+	end
+
+end
 
 
 get '/' do
@@ -14,3 +40,28 @@ get '/' do
 end
 
 
+
+post '/faces' do
+
+	content_type :json
+
+	def get_friend(username)
+
+		username = params[:username]
+
+		url = "http://graph.facebook.com/#{username}"
+
+		find_this_guy = HTTParty.get(url)
+
+		friend = JSON(find_this_guy)
+
+		@name = friend["name"]
+
+		@gender = friend["gender"]
+
+		@locale = friend["locale"]
+
+		@username = friend["username"]
+	end
+
+end
--- w07/d03/Nancy_Ko/facebook_ajax/public/javascripts/scripts.js
+++ w07/d03/Nancy_Ko/facebook_ajax/public/javascripts/scripts.js
@@ -1,4 +1,15 @@
-$(document).ready(function() {
+function setEventHandlers() {
 
-	console.log("HI THIS IS LOADING");
-})
+	$("#find_on_facebook_button").click(function() {
+		
+		console.log("HI THIS IS LOADING");
+// semicolons after every line of executable code and none on statements.
+
+	}); 
+}
+
+$(document).ready(function() {
+	
+	setEventHandlers();
+
+});
