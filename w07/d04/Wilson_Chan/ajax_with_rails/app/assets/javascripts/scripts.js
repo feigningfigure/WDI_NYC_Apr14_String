0db1a4aa120deac5249ffd9973749697e7e67a8f
mrwilsondj <mrwilsondj@gmail.com>
2014-06-05 18:57:38 -0400

    Show all button added, database working

---
+++ w07/d04/Wilson_Chan/ajax_with_rails/app/assets/javascripts/scripts.js
@@ -0,0 +1,52 @@
+var jqXHR;
+
+function addEventListeners(){
+	var $add_task_button = $('#add_task_button'),
+			$task_text_input = $('#task_text_input'),
+			$due_date_input = $('#due_date_input');
+			$show_all_button = $('#show_all_button');
+
+			$add_task_button.click(function(){
+				var params = {task_text: $task_text_input.val(),
+											due_date: $due_date_input.val()
+										};
+				var task = new Task;
+				task.create(params);
+			});
+}
+
+var Task = function(){
+	this.create = function(params){
+			$.ajax({
+				url: '/tasks',
+				type: "POST",
+				dataType: "json",
+				data: params
+			}).done(function(data){
+				console.log(data);
+			});
+
+		// 	jqXHR.done(function(data) {
+		// 		console.log(data);
+		// 		console.log("DONE");
+		// 	});
+
+		// 	jqXHR.fail(function(data) {
+		// 		console.log("FAIL");
+		// 	});
+
+		// 	jqXHR.error(function(data) {
+		// 		console.log("ERROR");
+		// 	});
+
+		// 	jqXHR.always(function(data) {
+		// 		console.log("ALWAYS");
+		// 	});
+		};
+	};
+
+$(function(){
+
+	addEventListeners();
+
+});
