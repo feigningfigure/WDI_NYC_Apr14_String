0fc5d6bd99893149c256c2d23d7ee9b014ff339e
slaffont <slaffont@gmail.com>
2014-06-04 21:38:22 -0400

    facebook-sinatra-ajax homework

--- w07/d03/Sophie_Laffont/homework/facebook_friends/main.rb
+++ w07/d03/Sophie_Laffont/homework/facebook_friends/main.rb
@@ -2,15 +2,16 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 require 'json'
+require 'HTTParty'
 
-FILENAME = "quiz_db"
+FILENAME = "friends_rb"
 
 def get_data()
 	if File.exists?(FILENAME)
 		results = JSON.parse(File.read(FILENAME))
 		return results
 	else
-		{"quizzes" => []}
+		{"friends" => []}
 	end
 end
 
@@ -25,28 +26,27 @@ get '/' do
 	erb :index
 end
 
-post '/quizzes' do
+post '/friends' do
 	content_type :json
 
-	# binding.pry
 	data = get_data
-	# {"quizzes" => []} Is what our data variable should now look like.  But it may not be empty
-	puts request
-	puts request.params
-	# binding.pry
-	new_quiz = {"title" => request.params["quiz_name"]}
 
-	data["quizzes"].push(new_quiz)
+	user = request.params["friend_name"]
+	puts user
+	url = "http://graph.facebook.com/#{user}"
+	friend_json = HTTParty.get(url)
+	@friend = JSON(friend_json)
+	puts @friend
+	data["friends"].push(@friend)
 
 	save_data(data)
 
-	#the below is data in the .done function of the AJAX call 
-	message = request.params["quiz_name"]
+	message = @friend
 	message.to_json
 
 end
 
-get '/quizzes' do 
+get '/friends' do
 	content_type :json
 
 	db_contents = get_data
