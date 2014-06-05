8803584098a272787610e5d55fb38879c046f4c3
jpark1120 <parkj1120@gmail.com>
2014-06-05 08:50:50 -0400

    finished hw minus bonus

--- w07/d03/Joe_Park/ajax_graph_fb/main.rb
+++ w07/d03/Joe_Park/ajax_graph_fb/main.rb
@@ -3,6 +3,72 @@ require 'sinatra/reloader'
 require 'httparty'
 require 'pry'
 
+FILENAME = "user_db"
+
+def get_data()
+  if File.exists?(FILENAME)
+    results = JSON.parse(File.read(FILENAME))
+    return results
+  else
+    {"users" => []}
+  end
+end
+
+def save_data(data)
+  # binding.pry
+  raw = JSON.dump(data)
+  File.open(FILENAME, 'w') do |file|
+    file.write(raw)
+  end
+end
+
+def graph_call(name)
+  url = "http://graph.facebook.com/#{name.downcase.gsub(" ", "")}"
+  jsonresponse = JSON.parse(HTTParty.get(url).body)
+
+  return jsonresponse
+end
+
+
 get '/' do 
   erb :index
 end
+
+get '/users' do 
+  content_type :json
+
+  db_contents = get_data
+
+  db_contents.to_json
+
+end
+
+post '/users' do 
+  content_type :json 
+  # binding.pry
+  data = get_data
+  response = graph_call(params["name"])
+
+  new_user = {"facebook_id" => response["id"],
+              "first_name" => response["first_name"],
+              "gender" => response["male"],
+              "last_name" => response["last_name"],
+              "link" => response["link"],
+              "locale" => response["locale"],
+              "name" => response["name"],
+              "username" => response["username"]
+            }
+  data["users"].push(new_user)
+
+  save_data(data)
+
+  data.to_json
+
+end
+
+
+
+
+
+
+
--- w07/d03/Joe_Park/ajax_graph_fb/public/javascripts/scripts.js
+++ w07/d03/Joe_Park/ajax_graph_fb/public/javascripts/scripts.js
@@ -1,13 +1,69 @@
 function setEventHandlers(){
 
-	$
+	$('#search').click(function(){
+		var $input = $("#name_input");
+		var $previous = $("#previous");
 
+		$.ajax({
+			url: '/users',
+			type: 'POST',
+			dataType: 'json',
+			data: {name: $input.val()}
+		}).done(function(new_user){
+			console.log(new_user);
+			$input.val('');
+			});
+		
+	});
 
+	$('#show_button').click(function(){
+		var $user_list = $("#user_list");
+
+		$.ajax({
+			url: '/users',
+			type: 'GET',
+			dataType: 'json',
+		}).done(function(data){
+			$user_list.empty();
+			data.users.forEach(function(user){
+				$user_list.append("<li id='"+ user["facebook_id"] +"'>"+ user["name"] +"</li>");
+			});
+		});
+	});
+
+	$('#show_button').click(function(){
+		var $user_list = $("#user_list");
+
+		$.ajax({
+			url: '/users',
+			type: 'GET',
+			dataType: 'json',
+		}).done(function(data){
+			$user_list.empty();
+			data.users.forEach(function(user){
+				$user_list.append("<li>"+ user["name"] +"</li>");
+			});
+		});
+	});
 
 }
 
 
 $(function(){
+
 	setEventHandlers();
 
 });
+
+
+// "<li>"+ user["link"]+"</li>"
+
+
+// {"facebook_id": "id",
+// "first_name": "first_name"],
+// "gender": "male",
+// "last_name": "last_name",
+// "link": "link",
+// "locale": "locale",
+// "name": "name",
+// "username": "username"}
