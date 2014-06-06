5e3793228ba8f149821b10fc0c3b3d955726dba2
Crustacean87! <declanv@gmail.com>
2014-06-05 21:19:39 -0400

    Added in show function.

---
+++ w07/d04/Declan_Van_Welie/homework/ajax_with_rails/app/assets/javascripts/scripts.js
@@ -0,0 +1,85 @@
+var jqXHR;
+
+function addEventListeners(){
+	var $add_task_button = $('#add_task_button'),
+			$task_text_input = $('#task_text_input'),
+			$due_date_input = $('#due_date_input'),
+			$show_tasks_button = $('#show_button'),
+			$show_tasks_list = $('#show_tasks_list');
+
+			$add_task_button.click(function(){
+				var params = {task_text: $task_text_input.val(),
+											due_date: $due_date_input.val()
+										};
+				var task = new Task;
+				task.create(params);
+			});
+
+			$show_tasks_button.click(function(){
+				task.show();
+			})
+
+
+}
+
+
+
+
+var Task = function(){
+	this.create = function(params){
+			$.ajax({
+				url: '/tasks',
+				type: "POST",
+				dataType: "json",
+				data: params
+			}).done(function(data){
+				console.log(data);
+			});
+
+	this.show = function(){
+
+		 $.ajax({
+		    url: '/users',
+		    type: 'GET',
+		    dataType: 'json'
+		    }).done(function(data){
+		    console.log(data);
+
+
+	      $show_tasks_list.empty();
+	      data.tasks.forEach(function(task){
+	      	 $show_tasks_list.prepend(
+      "<li class='task_item'>" + item["task"].task_text + "</li>"
+
+      )
+
+	}
+
+
+
+
+
+		// 	jqXHR.done(function(data) {
+		// 		console.log(data);
+		// 		console.log("DONE");
+		// 	});
+
+		// 	jqXHR.fail(function(data) {
+		// 		console.log("FAIL");
+		// 	});
+
+		// 	jqXHR.error(function(data) {
+		// 		console.log("ERROR");
+		// 	});
+
+		// 	jqXHR.always(function(data) {
+		// 		console.log("ALWAYS");
+		// 	});
+		};
+	};
+
+$(function(){
+
+	addEventListeners();
+
+});
