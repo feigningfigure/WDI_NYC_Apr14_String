55014fb77619e420d9dcb8b0938c72cf384cae38
LizCira <liz.cira.goldstein@gmail.com>
2014-06-05 23:17:31 -0400

    show and add are working

--- w07/d04/Liz_Goldstein/ajax_with_rails/app/assets/javascripts/scripts.js
+++ w07/d04/Liz_Goldstein/ajax_with_rails/app/assets/javascripts/scripts.js
@@ -1,54 +1,51 @@
-// var jqXHR;
-
 function addEventListeners(){
-	var $add_task_button = $('#add_task_button'),
-			$task_text_input = $('#task_text_input'),
-			$due_date_input = $('#due_date_input');
+var $add_task_button = $('#add_task_button'),
+$task_text_input = $('#task_text_input'),
+$due_date_input = $('#due_date_input');
 
-			$add_task_button.click(function(){
-				var params = {task_text: $task_text_input.val(),
-											due_date: $due_date_input.val()
-										};
-				var task = new Task;
-				task.create(params);
-			});
+$add_task_button.click(function(){
+var params = {task_text: $task_text_input.val(),
+due_date: $due_date_input.val()
+};
+var task = new Task;
+task.create(params);
+});
 }
 
 var Task = function(){
-	this.create = function(params){
-			$.ajax({
-				url: '/tasks',
-				type: "POST",
-				dataType: "json",
-				data: params
-			}).done(function(data){
-				console.log(data);
-				$("#task_text_input").val('')
-				$('#due_date_input').val('')
-				$("#task_list").append("<li>" + data["task_text"] + "</li>");
-			});
+this.create = function(params){
+$.ajax({
+url: '/tasks',
+type: "POST",
+dataType: "json",
+data: params
+}).done(function(data){
+console.log(data);
+$('#task_text_input').val('');
+$('#due_date_input').val('');
+$("#task_list").prepend("<li>" + data["task_text"] + " " + data["due_date"]+ "</li>");
+});
 
-		// 	jqXHR.done(function(data) {
-		// 		console.log(data);
-		// 		console.log("DONE");
-		// 	});
 
-		// 	jqXHR.fail(function(data) {
-		// 		console.log("FAIL");
-		// 	});
 
-		// 	jqXHR.error(function(data) {
-		// 		console.log("ERROR");
-		// 	});
+};
 
-		// 	jqXHR.always(function(data) {
-		// 		console.log("ALWAYS");
-		// 	});
-		};
-	};
+};
+
 
 $(function(){
+$.ajax({
+url: '/tasks',
+type: 'GET',
+dataType: 'json'
+}).done(function(data){
+var $task_list = $("#task_list");
+for (i=0; i<data.length; i++){
+var $display = $("#task_list").prepend("<li>" + data[i]["task_text"] + " " + data[i]["due_date"]+ "</li>");
+};
+console.log($display);
+});
 
-	addEventListeners();
+addEventListeners();
 
 });
