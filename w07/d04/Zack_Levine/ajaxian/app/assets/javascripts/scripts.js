f990aa82df0194d4eefbc6205bc7321ff6569b33
zackn9ne <zackn9ne@gmail.com>
2014-06-05 20:00:33 -0400

    its working in json but not in RAILS

--- w07/d04/Zack_Levine/ajaxian/app/assets/javascripts/scripts.js
+++ w07/d04/Zack_Levine/ajaxian/app/assets/javascripts/scripts.js
@@ -3,14 +3,23 @@ var jqXHR;
 function addEventListeners(){
 	var $add_task_button = $('#add_task_button'),
 		$task_text_input = $('#task_text_input'),
-		$due_date_input = $('#due_date_input');			
+		$due_date_input = $('#due_date_input'),			
+		$show = $('#show');			
 
+//actual usage of these things
+	//adding a task to it
 	$add_task_button.click(function(){
 		var params = {task_text: $task_text_input.val(),
 			due_date: $due_date_input.val()
 		};
 		var task = new Task;
 		task.create(params);							
+	});
+	//showing the list
+	$show.click(function(){
+	console.log("show all button has been clicked");
+	var list = new List;
+	list.create(); //this actually go ahead and creates the list to be UsEd below
 	});
 }
 
@@ -25,25 +34,26 @@ var Task = function(){
 			console.log(data);
 		});
 
-		// 	jqXHR.done(function(data) {
-		// 		console.log(data);
-		// 		console.log("DONE");
-		// 	});
-
-		// 	jqXHR.fail(function(data) {
-		// 		console.log("FAIL");
-		// 	});
-
-		// 	jqXHR.error(function(data) {
-		// 		console.log("ERROR");
-		// 	});
-
-		// 	jqXHR.always(function(data) {
-		// 		console.log("ALWAYS");
-		// 	});
 	};
 };
 
+var List = function(){
+	console.log('the words');
+	this.create = function(params){
+		$.ajax({
+			url: '/tasks',
+			type: "GET",
+			dataType: "json",
+			data: params
+		}).done(function(data){
+			console.log(data);
+			data.tasks.forEach(function(task) {
+			$list.append("<li>"+ list.task + "</li>")
+		});
+
+	});
+};
+};
 $(function(){
 
 	addEventListeners();
