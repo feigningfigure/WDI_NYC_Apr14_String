bd165c8a9e3c49392d2d1f7a2a2beba5bf95673a
Crustacean87! <declanv@gmail.com>
2014-06-07 15:57:34 -0400

    Added functionality to show all beasts names, unformatted. Plan to work on replicating original partial displays, potentially with _.template

--- w07/d05/Declan_Van_Welie/smoketop-rails/app/assets/javascripts/scripts.js
+++ w07/d05/Declan_Van_Welie/smoketop-rails/app/assets/javascripts/scripts.js
@@ -1,20 +1,54 @@
 function setEventHandlers() {
 
-// Add in EventHandlaers here (the code to listen for the clicks and interactions on the client side.)
+  // Add in EventHandlaers here (the code to listen for the clicks and interactions on the client side.)
+
+  var $beast_list = $(".beast_list"),
+      $home_button = $("#home_button"),
+      $beast_list_button = $("#beasts_list_button"),
+      $about_button = $("#about_button");
+
+  $beast_list_button.click(function(){
+    var beast = new Beast;
+    beast.list();
+    // beast_list_button.click(function() closing braces
+  });
+
+
+
+
+  // We'll need to setup the Ajax call for the get requests
+
+  var Beast = function(){
+
+    this.list = function(){
+      $.ajax({
+        url: '/beasts',
+        type: 'GET',
+        dataType: 'json'
+      }).done(function(data){
+        $beast_list.empty();
+        data.forEach(function(beast){
+          $beast_list.append("<li id='" + beast["id"] + "'>" + beast["name"] + "</li>");
+          // data.forEach closing brackets/braces
+        });
+        // this.list .done function closing brace
+      });
+      // this.list function closing brace
+    }
+
+
+
+
+
+
+    // Closing brace for Beast function
+  }
+
+// Closing brace for setEventHandlers
+}
+
+$(function() {
+
+  setEventHandlers();
 
 });
-
-
-// We'll need to setup the Ajax call for the get requests
-
-var Beast = function(){
-
-  this.create = function(params)
-
-
-
-
-
-
-  // Closing brace for Beast function
-}
--- w07/d05/Declan_Van_Welie/smoketop-rails/app/controllers/beasts_controller.rb
+++ w07/d05/Declan_Van_Welie/smoketop-rails/app/controllers/beasts_controller.rb
@@ -5,26 +5,47 @@ class BeastsController < ApplicationController
   end
 
   def index
-    if params[:diet]
-      @diet = params[:diet]
-    else
-      @diet = "any"
-    end
 
-    if @diet != "any"
-      @beasts = Beast.where("diet = ?", params[:diet])
-    else
-      @beasts = Beast.all
+    # show all beasts on the page
+
+    respond_to do |format|
+      format.html { @beasts = Beast.all }
+      format.json { render json: Beast.all }
     end
+    # if params[:diet]
+    #   @diet = params[:diet]
+    # else
+    #   @diet = "any"
+    # end
+
+    # if @diet != "any"
+    #   @beasts = Beast.where("diet = ?", params[:diet])
+    # else
+    #   @beasts = Beast.all
+    # end
   end
 
   def create
+    # Add in forms for a create page.
+     new_beast = Beast.create(
+      {name: params["name"],
+       diet: params["diet"],
+       light_ethology: params["light_ethology"],
+       habitat: params["habitat"],
+       size: params["size"],
+       description: params["description"],
+       image_filename: params["image_filename"]
+       })
 
+    respond_to do |format|
+      format.json { render json: { message: "New beast created" } }
+
+    end
   end
 
   def destroy
 
-  end   
+  end
 
   def update
 
