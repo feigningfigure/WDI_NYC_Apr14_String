a4f0a3e41e0c0552b262e5c3e7f38d72a320440d
Crustacean87! <declanv@gmail.com>
2014-06-07 13:00:36 -0400

    Added in resources: beasts to the routes file, and removed the link_to tags in the application view so the homepage would render correctly. About to start setting up the js to render the partial content when the links are clicked.

---
+++ w07/d05/Declan_Van_Welie/smoketop-rails/app/assets/javascripts/scripts.js
@@ -0,0 +1,20 @@
+function setEventHandlers() {
+
+// Add in EventHandlaers here (the code to listen for the clicks and interactions on the client side.)
+
+});
+
+
+// We'll need to setup the Ajax call for the get requests
+
+var Beast = function(){
+
+  this.create = function(params)
+
+
+
+
+
+
+  // Closing brace for Beast function
+}
