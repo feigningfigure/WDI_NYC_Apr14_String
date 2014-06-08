c377f06bb50030a2cd569d897d5200581c5bf901
Crustacean87! <declanv@gmail.com>
2014-06-07 22:01:24 -0400

    Working on replicating layout from html/partials in jquery

--- w07/d05/Declan_Van_Welie/smoketop-rails/app/assets/javascripts/scripts.js
+++ w07/d05/Declan_Van_Welie/smoketop-rails/app/assets/javascripts/scripts.js
@@ -2,13 +2,15 @@ function setEventHandlers() {
 
   // Add in EventHandlaers here (the code to listen for the clicks and interactions on the client side.)
 
-  var $beast_list = $(".beast_list"),
+  var $show_section = $(".show_all"),
+      $beast_list = $(".beast_list"),
       $home_button = $("#home_button"),
       $beast_list_button = $("#beasts_list_button"),
       $about_button = $("#about_button");
 
   $beast_list_button.click(function(){
     var beast = new Beast;
+    $show_section.html("<h1> Beasties below... </h1>");
     beast.list();
     // beast_list_button.click(function() closing braces
   });
@@ -26,9 +28,18 @@ function setEventHandlers() {
         type: 'GET',
         dataType: 'json'
       }).done(function(data){
-        $beast_list.empty();
+        // $beast_list.empty();
+
+
+
         data.forEach(function(beast){
-          $beast_list.append("<li id='" + beast["id"] + "'>" + beast["name"] + "</li>");
+          $show_section.append(
+            "<article class='beast' id='beast-" + beast["id"] + "'> \
+            <header> \
+            <h1>" + beast["name"] + "</h1> \
+            </header> \
+            </article>"
+            );
           // data.forEach closing brackets/braces
         });
         // this.list .done function closing brace
@@ -37,7 +48,22 @@ function setEventHandlers() {
     }
 
 
+  // <article class="beast" id="beast-<%= beast.id %>">
+ //    <header>
+ //      <h1><%= beast.name %></h1>
+ //      <%= image_tag(beast.image_filename || 'unavailable.png') %>
+ //    </header>
+ //    <section class="entry-body">
+ //      <%= attribute_table beast, %w(diet habitat light_ethology size) %>
+ //      <% beast.description_paragraphs.each do |p| %>
+ //        <p><%= p %></p>
+ //      <% end %>
+ //    </section>
+ //  </article>
 
+
+
+ // "<li id='" + beast["id"] + "'>" + beast["name"] + "</li>");
 
 
 
