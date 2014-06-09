60a735c73ae126480e89ddc8ccd32e7478110367
cdbajorin <christopher.bajorin@gmail.com>
2014-06-09 16:52:07 -0400

    prepull

--- w08/d01/Christopher_Bajorin/smoketop/app/assets/javascripts/beasts.js
+++ w08/d01/Christopher_Bajorin/smoketop/app/assets/javascripts/beasts.js
@@ -1,11 +1,26 @@
 function repopulateBeastList(diet) {
 
-    var data = $.param({
+    // There's no way to pass a data object directly to the
+    // $.load() function without it assuming that we want a POST
+    // request, so I'm manually creating the query string parameters
+    // to use convenient data object syntax.
+    var options = {
       diet: diet,
       partial: true
-    });
+    };
 
-    $("#beast-list").load("/beasts?" + data);
+    var queryString = $.param(options);
+
+    // $("#beast-list").load("/beasts?" + options);
+
+    $.ajax({
+        url: "/beasts",
+        type: "GET",
+        dataType: "html",
+        data: options
+    }).done(function(response) {
+        $("#beast-list").html(response);
+    });
 
 }
 
@@ -15,10 +30,35 @@ function setEventListeners() {
     repopulateBeastList($(this).val());
   });
 
+
 }
 
 $(document).ready(function() {
 
   setEventListeners();
 
+  var beastTemplate = _.template($("#beast-template").text());
+
+  console.log(
+    beastTemplate({
+      beast: {
+        id: 5,
+        name: "Crocotillion"
+      }
+    })
+  );
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
