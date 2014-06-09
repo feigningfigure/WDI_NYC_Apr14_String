b827ad3eb4902287830b1c7e96260280bab36e7c
Mannymin <Mannymin@gmail.com>
2014-06-09 16:55:27 -0400

    nooo

---
+++ w07/d04/Emmanuel_Tucker/ajax_with_rails/app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+class TasksController < ApplicationController
+
+	protect_from_forgery except: :create
+
+	def index
+		# binding.pry
+
+		@tasks = Task.all
+		respond_to do |format|
+			# format.html { render json: Task.all }
+			format.html { @tasks = "Task.all" }
+			format.json { render json: "I am a response" }
+		end
+	end
+
+	def new
+
+	end
+
+	def create
+		# binding.pry
+
+		new_task = Task.create({task_text: params["task_text"], due_date: params["due_date"]})
+		respond_to do |format|
+			format.json { render json: new_task}
+		end
+	end
+
+	def show
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
+	private
+
+	def task_params
+		params.require(:task).permit(:task_text, :due_date, :completed)
+	end
+
+end
