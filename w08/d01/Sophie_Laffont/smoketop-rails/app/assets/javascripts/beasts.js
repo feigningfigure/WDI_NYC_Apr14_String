95109ddf78d4b5d8f50d30f5fd891138a6aee0d0
slaffont <slaffont@gmail.com>
2014-06-09 14:22:09 -0400

    monday early afternoon

---
+++ w08/d01/Sophie_Laffont/smoketop-rails/app/assets/javascripts/beasts.js
@@ -0,0 +1,64 @@
+function repopulateBeastList(diet) {
+
+    // There's no way to pass a data object directly to the
+    // $.load() function without it assuming that we want a POST
+    // request, so I'm manually creating the query string parameters
+    // to use convenient data object syntax.
+    var options = {
+      diet: diet,
+      partial: true
+    };
+
+    var queryString = $.param(options);
+
+    // $("#beast-list").load("/beasts?" + options);
+
+    $.ajax({
+        url: "/beasts",
+        type: "GET",
+        dataType: "html",
+        data: options
+    }).done(function(response) {
+        $("#beast-list").html(response);
+    });
+
+}
+
+function setEventListeners() {
+
+  $("#diet-filter input:radio[name=diet]").click(function() {
+    repopulateBeastList($(this).val());
+  });
+
+
+}
+
+$(document).ready(function() {
+
+  setEventListeners();
+
+  var beastTemplate = _.template($("#beast-template").text());
+
+  console.log(
+    beastTemplate({
+      beast: {
+        id: 5,
+        name: "Crocotillion"
+      }
+    })
+  );
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
