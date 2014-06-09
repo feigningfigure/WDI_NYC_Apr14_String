71a280f48f0eb3579219667127d5f099014fbf96
feigningfigure <andrew.g.wallace@gmail.com>
2014-06-09 09:17:03 -0400

    syncing git

--- w07/d05/Andrew_Wallace/smoketop-rails/app/assets/javascripts/scripts.js
+++ w07/d05/Andrew_Wallace/smoketop-rails/app/assets/javascripts/scripts.js
@@ -1,4 +1,44 @@
-$(document).ready(function() {
+function setEventHandlers() {
 
+  var $filter = $("#filter");
 
+  $filter.click(function(){
+    var diet_choice = $(".diet_choice:checked").val(),
+        beast = new Beast,
+        $beast_list = $("#beast-list");
+
+    beast.filter(diet_choice);
+
+    if (diet_choice === "any") {
+      $beast_list.load("beasts?diet=any .beast");
+    } else if (diet_choice === "herbivore") {
+      $beast_list.load("beasts?diet=herbivore .beast");
+    } else if (diet_choice === "carnivore"){
+      $beast_list.load("beasts?diet=carnivore .beast");
+    } else {
+      $beast_list.load("beasts?diet=omnivore .beast");
+    }
+  });
+
+  var Beast = function(){
+    this.filter = function(filter_choice){
+      $.ajax({
+        url: '/beasts',
+        type: 'GET',
+        dataType: 'json',
+        data: { "diet": filter_choice }
+      }).done(function() {
+
+        console.log("hello");
+      });
+
+    }
+  }
 }
+
+
+$(function() {
+
+  setEventHandlers();
+
+})
