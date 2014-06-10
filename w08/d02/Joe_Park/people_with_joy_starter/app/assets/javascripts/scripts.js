d6dd6c2487351e75dd58ba21cf644097f67a3f19
jpark1120 <parkj1120@gmail.com>
2014-06-10 14:43:26 -0400

    doing class exercise

--- w08/d02/Joe_Park/people_with_joy_starter/app/assets/javascripts/scripts.js
+++ w08/d02/Joe_Park/people_with_joy_starter/app/assets/javascripts/scripts.js
@@ -1,27 +1,35 @@
 
 // ************ Model *************
-
-
-
-
+function Person(personJSON){
+  this.name = personJSON.name;
+  this.id = personJSON.id;
+}
 
 // ************ View *************
-
-
-
+function PersonView(model){
+  this.model = model;
+  this.el = undefined;
+}
 
 
 
 // ************ Collection *************
 
-
-
-
+function PeopleCollection(){
+  this.models = {};
+}
 
 
 
 $(function(){
-
+  // e is convention for event
+  $('.name-form').on('submit', function(e){
+    console.log(e);
+    e.preventDefault();
+    // var newName = $('.name-form input[name="name]').val();
+    // INSERT AJAX CALL BELOW
+    
+  });
 
 })
 
