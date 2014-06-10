bca7bea54f3df70c560f2b0d6c16f9ca8646f80b
Crustacean87! <declanv@gmail.com>
2014-06-10 07:46:58 -0400

    Fixed to randomly assign moods rather than taking mood as a parameter

--- w08/d01/Declan_Van_Welie/homework/moody_dudes.js
+++ w08/d01/Declan_Van_Welie/homework/moody_dudes.js
@@ -1,21 +1,22 @@
-    function Person(mood){
+    function Person(){
 
     function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
     };
 
     var people = ["Bob","Curly","Barney","Bruce","Edgar"];
+    var moods = ["mopey","pissed", "happy"];
     this.name = people[Math.floor(Math.random() * people.length)];
     this.age = getRandomInt(25, 50);
-    this.mood = mood;
+    this.mood = moods[Math.floor(Math.random() * moods.length)];
 
     this.introduce = function(){
 
-      if (mood === "happy"){
+      if (this.mood === "happy"){
       console.log("Hi! I'm " + this.name + ". I'm " + this.age + " years old. Pleased to meet you!");
-      } else if (mood === "mopey") {
+      } else if (this.mood === "mopey") {
       console.log("Hi! I'm " + this.name + ". I'm " + this.age + " years old. Life is meaningless!");
-      } else if (mood == "pissed") {
+      } else if (this.mood == "pissed") {
       console.log("Hi...I'm " + this.name + ". I'm " + this.age + " years old. I hate you.")
       }
 
@@ -23,7 +24,7 @@
 
   }
 
-person = new Person("mopey");
+person = new Person();
 person.introduce();
 
 
