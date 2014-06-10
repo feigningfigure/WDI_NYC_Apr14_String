edb9c4d91fd1b20ff40de6551b2cf74f256302f3
LizCira <liz.cira.goldstein@gmail.com>
2014-06-09 22:07:33 -0400

    comitting max's in class beast code before ruining it

--- w08/d01/Liz_Goldstein/smoketop-rails/db/schema.rb
+++ w08/d01/Liz_Goldstein/smoketop-rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609144845) do
+ActiveRecord::Schema.define(version: 20140609150858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,14 +26,6 @@ ActiveRecord::Schema.define(version: 20140609144845) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_filename"
-  end
-
-  create_table "sightings", force: true do |t|
-    t.integer  "beast_id"
-    t.string   "mood"
-    t.string   "location"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
