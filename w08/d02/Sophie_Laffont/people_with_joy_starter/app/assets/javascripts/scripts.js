e72dc0b0194736030fa02585d1e38ae89035fc05
slaffont <slaffont@gmail.com>
2014-06-10 15:35:24 -0400

    tuesday afternoon

--- w08/d02/Sophie_Laffont/people_with_joy_starter/app/assets/javascripts/scripts.js
+++ w08/d02/Sophie_Laffont/people_with_joy_starter/app/assets/javascripts/scripts.js
@@ -1,27 +1,52 @@
 
 // ************ Model *************
 
-
+// this is just so we know we are receiving a json hash
+function Person(personJSON){
+   this.name = personJSON.name;
+   this.id = personJSON.id;
+}
 
 
 
 // ************ View *************
 
-
-
+function PersonView(model){
+   this.model = model;
+   this.el= undefined;
+}
 
 
 
 // ************ Collection *************
 
+function PeopleCollection(){
+this.models = {};
+}
 
+PeopleCollection.prototype.add = function(personJSON){
+var newPerson = new Person (personJSON);
 
+}
 
-
-
+PeopleCollection.prototype.create = function(){
+   $.ajax({
+   url: '/people',
+   method: 'post',
+   dataType: 'json',
+   data: {person: paramObject}, //this is because we have strong params
+   }).done(function(data){
+.add
+   })
+}
+var peopleCollection = new PeopleCollection();
 
 $(function(){
-
-
+$('.name-form').on('submit',function(e){
+   console.log(e);
+   e.preventDefault();
+   var newName = $('.name-form input[name="name"]').val();
+  peopleCollection.create({name: newName});
 })
 
+})
--- w08/d02/Sophie_Laffont/people_with_joy_starter/config/routes.rb
+++ w08/d02/Sophie_Laffont/people_with_joy_starter/config/routes.rb
@@ -2,3 +2,14 @@ Rails.application.routes.draw do
   root "welcome#index"
   resources :people
 end
+
+#   Prefix Verb   URI Pattern                Controller#Action
+#        root GET    /                          welcome#index
+#      people GET    /people(.:format)          people#index
+#             POST   /people(.:format)          people#create
+#  new_person GET    /people/new(.:format)      people#new
+# edit_person GET    /people/:id/edit(.:format) people#edit
+#      person GET    /people/:id(.:format)      people#show
+#             PATCH  /people/:id(.:format)      people#update
+#             PUT    /people/:id(.:format)      people#update
+#             DELETE /people/:id(.:format)      people#destroy
