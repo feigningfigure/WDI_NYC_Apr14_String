61c44b3d0d3a99e6887234688308c96397579956
jpark1120 <parkj1120@gmail.com>
2014-06-10 16:22:13 -0400

    js model for Person in progress

--- w08/d02/Joe_Park/people_with_joy_starter/app/assets/javascripts/scripts.js
+++ w08/d02/Joe_Park/people_with_joy_starter/app/assets/javascripts/scripts.js
@@ -11,6 +11,12 @@ function PersonView(model){
   this.el = undefined;
 }
 
+PersonView.prototype.render = function(){
+  var newElement = $('<li>').html(this.model.name);
+  this.el = newElement
+  return this
+}
+
 
 
 // ************ Collection *************
@@ -19,17 +25,54 @@ function PeopleCollection(){
   this.models = {};
 }
 
+PeopleCollection.prototype.add = function(personJSON){
+  var newPerson = new Person(personJSON);
+  this.models[personJSON.id] = newPerson;
+}
 
+PeopleCollection.prototype.create = function(paramObject){
+  $.ajax({
+    url: "/people",
+    method: "POST",
+    dataType: "json",
+    data: {person: paramObject}
+  }).done(function(data){
+    // INSERT CALLBACK CODE HERE
+    peopleCollection.add(data);
+  });
+}
+
+
+
+
+
+// global
+function displayEntireCollection(){
+
+  $(".people").html('');
+
+  for(id in peopleCollection.models){
+    var person = peopleCollection.models[id];
+    var personView = new PersonView(person);
+    $('.people').append(personView.render().el);
+  }
+
+}
+
+var peopleCollection = new PeopleCollection();
 
 $(function(){
   // e is convention for event
   $('.name-form').on('submit', function(e){
     console.log(e);
     e.preventDefault();
-    // var newName = $('.name-form input[name="name]').val();
+    var newNameInput = $('.name-form input[name="name"]');
     // INSERT AJAX CALL BELOW
-    
+    peopleCollection.create({name: newNameInput.val()});
+    newNameInput.val('');
   });
+
+  displayEntireCollection();
 
 })
 
