ad06d431c2d6596b0b33af29b51f279663cb2ec0
daramao <dara.mao@gmail.com>
2014-06-09 21:56:49 -0400

    hw completed

--- w08/d01/Dara_Mao/moody_person/moody_person.js
+++ w08/d01/Dara_Mao/moody_person/moody_person.js
@@ -1,20 +1,14 @@
-// 5 person array
 var people_array = ["Dara", "Jane", "Joe", "Liz", "John"]
 var mood_array = ["happy", "unhappy"]
-var age_array = [30,22,23,32,26]
 
 function messageMoody (people){
-  var index = Math.floor(Math.random()*5);
-  var age = age_array[index];
+  this.age = Math.floor(Math.random()*50);
+  this.mood = mood_array[Math.floor(Math.random()*2)];
 
-  // Randomly pick a mood for the person
-  var mood_array_index = Math.floor(Math.random()*2);
-  var mood = mood_array[mood_array_index];
-
-  if (mood == "happy"){
-    console.log("Hi! I am "+ people +". I am "+ age +" years old. Please to meet you! I'm in a "+ mood +" mood.");
+  if (mood === "happy"){
+    console.log("Hi! I am "+ people +". I am "+ this.age +" years old. Please to meet you! I'm in a "+ this.mood +" mood.");
   } else {
-    console.log("Hi... "+ people +" here. Don't talk to me. I'm "+ mood +".")
+    console.log("Hi... "+ people +" here. Don't talk to me. I'm "+ this.mood +" :(")
   }
   return;
 };
