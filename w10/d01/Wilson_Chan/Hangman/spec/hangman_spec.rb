09dc5918b51265d9d852c2c749d93290af9ffa0b
mrwilsondj <mrwilsondj@gmail.com>
2014-06-23 19:45:51 -0400

    2nd attempt

--- w10/d01/Wilson_Chan/Hangman/spec/hangman_spec.rb
+++ w10/d01/Wilson_Chan/Hangman/spec/hangman_spec.rb
@@ -42,12 +42,42 @@ RSpec.describe Hangman do
     expect(detective.progress).to eq("_ _ t _ _ t _ _ _")
   end
 
+  it "guessed letters should see itself" do
+      Hangman.@guessed_letters
+  end
+
   it "should reveal letters after correct guesses" do
+    game = Hangman.new("detective")
     gandhi.guess?("b")
     gandhi.guess?("z")
     gandhi.guess?("y")
     gandhi.guess?("e")
-    expect(gandhi.progress).to eq("_ _ _ _ _ _")
+    expect(detective.progress).to eq("_ _ _ _ _ _ _ _")
+  end
+
+  describe 'game_lost' do
+    it 'if the player has more than 6 wrong guesses, game over' do
+      game = Game.new
+      detective.guess?("t")
+      detective.guess?('f')
+      detective.guess?('j')
+      detective.guess?('j')
+      detective.guess?('j')
+      detective.guess?('j')
+      detective.guess?('j')
+      game.game_lost.should == true
+    end
+  end
+  describe 'game_won' do
+    it 'if the player guesses all of the letters corectly than the player has won' do
+      game = Game.new
+      gandhi.guess?('ghandhi')
+      game.letter_guess('g')
+      game.letter_guess('h')
+      game.letter_guess('a')
+      game.letter_guess('n')
+      game.game_won.should == true
+    end
   end
 
   # it "should show a function of incorrect_guesses" do
