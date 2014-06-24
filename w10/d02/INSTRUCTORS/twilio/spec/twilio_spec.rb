736bf4b08c1915e302e84fea615ec9bc835c5f84
Max Cantor <max@maxcantor.net>
2014-06-24 13:55:56 -0400

    more twilio fixes

--- w10/d02/INSTRUCTORS/twilio/spec/twilio_spec.rb
+++ w10/d02/INSTRUCTORS/twilio/spec/twilio_spec.rb
@@ -5,16 +5,16 @@ require 'twilio'
 describe Twilio do
 
     it "should send a text message" do
-        my_phone_number = '+15164950887'
+        my_phone_number = ENV["MY_PHONE_NUMBER"]
         result = Twilio.send_sms(my_phone_number, "Hello, world!")
-        ap result, indent: -6
+        ap result if ENV["TWILIO_DEBUG"] == "true"
         expect(result["status"]).to eq("queued")
     end
 
-    it "should ask about a text message" do
-      result = Twilio.get("https://api.twilio.com/2010-04-01/Accounts/AC0f8741036e06b515d23e48808c6c4391.json")
-      ap result, indent: -6
-      expect(result["type"]).to eq("Trial")
+    it "should ask about an account" do
+        result = Twilio.view_account("AC0f8741036e06b515d23e48808c6c4391")
+        ap result if ENV["TWILIO_DEBUG"] == "true"
+        expect(result["type"]).to eq("Trial")
     end
 
 end
