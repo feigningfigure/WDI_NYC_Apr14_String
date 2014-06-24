2647d350b7d4c4693b1e35b1910fcf9f43d89c14
masewillis <mwillistang@gmail.com>
2014-06-24 10:45:14 -0400

    clean3

--- w10/d01/Mason_Tang/hangman/lib/hangman.rb
+++ w10/d01/Mason_Tang/hangman/lib/hangman.rb
@@ -12,12 +12,15 @@ class Hangman
   def guess?(guessed_letter)
     if @word.include?(guessed_letter)
       @word.each_with_index do |letter, index|
+        incorrect_letters = []
+
         if letter == guessed_letter
           @progress[index] = letter
         end
       end
       return true
     else
+      incorrect_letter << letter
       return false
     end
   end
