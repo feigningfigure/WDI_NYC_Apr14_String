3850921bd02b14bd344ed7643fbea78423847c1c
mrwilsondj <mrwilsondj@gmail.com>
2014-06-24 18:58:17 -0400

    added some functions

--- w10/d02/Wilson_Chan/mta/lib/mta.rb
+++ w10/d02/Wilson_Chan/mta/lib/mta.rb
@@ -1,3 +1,8 @@
-n = ['ts', '34th', '28th-n', '23rd-n', 'us']
-l = ['8th', '6th', 'us', '3rd', '1st']
-s = ['gc', '33rd', '28th-s', '23rd-s', 'us']
+require 'mta'
+
+RSpec.describe display_trains do
+
+
+
+
+end
