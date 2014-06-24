25723d7549cb6bffd7128e8de128cf122179596e
feigningfigure <andrew.g.wallace@gmail.com>
2014-06-24 13:43:47 -0400

    homework

--- w10/d01/Andrew_Wallace/hangman/lib/hangman.rb
+++ w10/d01/Andrew_Wallace/hangman/lib/hangman.rb
@@ -21,4 +21,19 @@ class Hangman
       return false
     end
   end
+
+  def lose_status
+    @bad_guess_count = 6
+    if @bad_guess_count > 5
+      return "You lost!"
+    else
+      return "Keep guessing!"
+    end
+  end
+
+  def win_status
+    if @bad_guess_count <= 5 || progress
+      return
+    end
+  end
 end
--- w10/d01/Andrew_Wallace/hangman/spec/hangman_spec.rb
+++ w10/d01/Andrew_Wallace/hangman/spec/hangman_spec.rb
@@ -42,5 +42,19 @@ RSpec.describe Hangman do
     expect(detective.progress).to eq("_ _ t _ _ t _ _ _")
   end
 
+  it "should tell the user they have lost if they guess incorrectly more than 5 times" do
+    detective.guess("a")
+    detective.guess("b")
+    detective.guess("f")
+    detective.guess("g")
+    detective.guess("h")
+    detective.guess("i")
+    expect(detective.lose_status).to eq("You lost!")
+  end
+
+  it "should tell the player they have won if the number of guesses is less than or equal to 5 and the word is complete"
+  it "should return the word when the game is over"
+  it "should return the word if the player gives up"
+
 
 end
