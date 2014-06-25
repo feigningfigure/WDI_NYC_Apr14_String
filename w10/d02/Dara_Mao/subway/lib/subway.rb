69c19febd32eb29794353cb364f5f533c5e59e92
daramao <dara.mao@gmail.com>
2014-06-24 22:47:05 -0400

    hw

--- w10/d02/Dara_Mao/subway/lib/subway.rb
+++ w10/d02/Dara_Mao/subway/lib/subway.rb
@@ -6,37 +6,23 @@ class Subway
     "six_line" => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
   }
 
-  def subway_on (line_on, station_on)
+  def station_diff (line_on, station_on, line_off, station_off)
+      index_on = SUBWAY_LINE[line_on]
+      index_off = SUBWAY_LINE[line_off]
 
-    if SUBWAY_LINE[line_on].include?(station_on) == true
-      index_on = SUBWAY_LINE[line_on].index(station_on)
-      return index_on
+    if index_on.include?(station_on) == true && index_off.include?(station_off) == true
+
+      if index_on != index_off
+         diff =  (index_on.index(station_on) - index_on.index("Union Square")).abs+
+         (index_off.index(station_off) - index_off.index("Union Square")).abs
+      else
+         diff = (index_on.index(station_on) - index_off.index(station_off)).abs
+      end
+      return diff
     else
       return "Please enter a valid station"
     end
 
-  end
-
-  def subway_off (line_off, station_off)
-
-    if SUBWAY_LINE[line_off].include?(station_off) == true
-      index_off = SUBWAY_LINE[line_off].index(station_off)
-      return index_off
-    else
-      return "Please enter a valid station"
-    end
-
-  end
-
-# if SUBWAY_LINE[line_on] != SUBWAY_LINE[line_off]
-#    diff =  (index_on - SUBWAY_LINE[line_on].index("Union Square").abs+
-#    (index_off - SUBWAY_LINE[line_off].index("Union Square").abs
-# end
-
-
-  def station_diff(subway_on, subway_off)
-    diff = (subway_on - subway_off).abs
-    return diff
   end
 
 end
--- w10/d02/Dara_Mao/subway/spec/subway_spec.rb
+++ w10/d02/Dara_Mao/subway/spec/subway_spec.rb
@@ -7,32 +7,16 @@ RSpec.describe Subway do
     expect(subway).to be_a(Subway)
   end
 
-  it "should return true if the line contains a station on" do
+  it "should return error message if the station doesn't exist on this line" do
     subway = Subway.new
-    index_on = subway.subway_on("n_line", "Grand Central")
-    expect(index_on).to eq("Please enter a valid station")
-  end
-
-  it "should return true if the line contains a station off" do
-    subway = Subway.new
-    index_off = subway.subway_on("n_line", "34th")
-    expect(index_off).to eq(1)
+    no_station = subway.station_diff("l_line", "Time Square", "n_line", "34th")
+    expect(no_station).to eq("Please enter a valid station")
   end
 
   it "should return the number of stops between station on and off" do
     subway = Subway.new
-    subway_on = subway.subway_on("n_line", "8th")
-    subway_off = subway.subway_off("n_line", "34th")
-    diff = subway.station_diff(subway_on,subway_off)
+    diff = subway.station_diff("l_line", "6th", "n_line", "34th")
     expect(diff).to eq(4)
   end
-
- # it "should return the number of stops between station on and off" do
- #    subway = Subway.new
- #    subway_on = subway.subway_on("l_line", "1st")
- #    subway_off = subway.subway_off("n_line", "34th")
- #    diff = subway.station_diff(subway_on,subway_off)
- #    expect(diff).to eq(4)
- #  end
 
 end
