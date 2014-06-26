dcf26a2ec5162886eca11c75cfd7e8663a224724
keyanbagheri <keyanbagheri@gmail.com>
2014-06-26 09:44:11 -0400

    public trans homework

--- w10/d03/Keyan_Bagheri/PublicTransportation/db/seeds.rb
+++ w10/d03/Keyan_Bagheri/PublicTransportation/db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+SubwayLine.delete_all
+Station.delete_all
+Stop.delete_all
+
+SubwayLine.create(name:"1", color:"red")
+SubwayLine.create(name:"2", color:"red")
+SubwayLine.create(name:"3", color:"red")
+SubwayLine.create(name:"4", color:"green")
+SubwayLine.create(name:"5", color:"green")
+SubwayLine.create(name:"6", color:"green")
+SubwayLine.create(name:"6 Express", color:"green")
+SubwayLine.create(name:"7", color:"purple")
+SubwayLine.create(name:"7 Express", color:"purple")
+SubwayLine.create(name:"A", color:"blue")
+SubwayLine.create(name:"C", color:"blue")
+SubwayLine.create(name:"E", color:"blue")
+SubwayLine.create(name:"B", color:"orange")
+SubwayLine.create(name:"D", color:"orange")
+SubwayLine.create(name:"F", color:"orange")
+SubwayLine.create(name:"M", color:"orange")
+SubwayLine.create(name:"G", color:"lightgreen")
+SubwayLine.create(name:"J", color:"brown")
+SubwayLine.create(name:"Z", color:"brown")
+SubwayLine.create(name:"L", color:"lightgrey")
+SubwayLine.create(name:"S", color:"gray")
+SubwayLine.create(name:"N", color:"yellow")
+SubwayLine.create(name:"Q", color:"yellow")
+SubwayLine.create(name:"R", color:"yellow")
+
+result = HTTParty.get("https://data.cityofnewyork.us/api/views/he7q-3hwy/rows.json")
+
+result["data"].each do |station|
+  new_station = Station.create({
+    address: station[9]
+    })
+  station[10].split("-").each do |line|
+    line_name = SubwayLine.find_by_name(line)
+    if line_name
+      line_id = line_name.id
+      Stop.create({
+        subway_line_id: line_id,
+        station_id: new_station.id
+        })
+    end
+  end
+end
+
+
+
+
+
+
