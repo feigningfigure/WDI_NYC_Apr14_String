53d944b8d036a04ee9e8fe4ca4a48fc6a8681a6b
montricher <montricher@icloud.com>
2014-06-26 11:59:23 -0400

    Trains app

--- w10/d03/David_Montricher/subway_manager/app/models/subway_line.rb
+++ w10/d03/David_Montricher/subway_manager/app/models/subway_line.rb
@@ -1,4 +1,5 @@
 class SubwayLine < ActiveRecord::Base
-  has_many_and_belongs_to_many :stations
+  has_many :stations
+  has_many_and_belongs_to_many :stations, through :subway_lines_stations
 
 end
