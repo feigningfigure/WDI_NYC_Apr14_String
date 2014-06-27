b8ffb18a0c70533e6e111e62e57372a1e48b8d77
montricher <montricher@icloud.com>
2014-06-27 15:54:02 -0400

    Authors and collections updated

--- w10/d05/David_Montricher/bookr/javascripts/backbone/book.js
+++ w10/d05/David_Montricher/bookr/javascripts/backbone/book.js
@@ -1,20 +1,57 @@
-// *** MODEL ***
+// ***Model***
 
-// Create the Author class, notice the capitalization
 var Book = Backbone.Model.extend({
   initialize: function(){
-    console.log("an Author has been born")
+  	console.log("A book has been written");
   },
 
   defaults: {
-    name: "",
+    title: ""
   }
 });
 
-// *** VIEW ***
-// *** COLLECTION ***
+//***Collection***
 
-car AuthorCollection = Backbone.Collection.extend({
-  model: Author
-
+var BookCollection = Backbone.Collection.extend({
+  model: Book
 });
+
+// ***Views***
+
+var BookView = Backbone.View.extend({
+	initialize: function(){
+		this.listenTo(this.model, 'all', this.render)
+	},
+	tagName: 'li',
+	template: _.template( $('#book-template').html() ),
+	render: function(){
+		this.$el.empty();
+		this.$el.html( this.template( this.model.attributes ) );
+
+		return this
+	}
+});
+
+var BookListView = Backbone.View.extend({
+	initialize: function(){
+		this.listenTo(this.collection, 'all', this.render )
+	},
+	render: function(){
+		var self = this;
+		this.$el.empty();
+		_.each(this.collection.models, function(book){
+			var bookView = new BookView({ model: book });
+			self.$el.append( bookView.render().el )
+		})
+		return this
+	}
+});
+
+
+
+
+
+
+
+
+
