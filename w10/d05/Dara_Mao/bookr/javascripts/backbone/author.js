c84ee3ac686826fabf1375dbb7c093af40337712
daramao <dara.mao@gmail.com>
2014-06-30 00:12:17 -0400

    .

--- w10/d05/Dara_Mao/bookr/javascripts/backbone/author.js
+++ w10/d05/Dara_Mao/bookr/javascripts/backbone/author.js
@@ -20,23 +20,77 @@ var AuthorView = Backbone.View.extend({
     },
 
     tagName: 'li',
-    template: _.template($("#author-template")),
+    template: _.template($("#author-template").html()),
     render: function() {
 
+        //this.$el is the template above with the author-template passed into it
         this.$el.empty();
         this.$el.html(this.template(this.model.attributes));
         var listView = new BookListView({
             collection: this.model.get("books"),
             el: this.$el.find(".books")
-            //find books for this author (within the scope of this el, which is this author-template html mode)
+            //find books for this author (within the scope of this el, which is this author-template html mode) .find lets you scope it inward, jquery  checks for every html element in that name
         });
-
+        listView.render();
+        return this;
     }
 });
 
+// author listView ////
+var AuthorListView = Backbone.View.extend({
+    initialize: function() {
+        this.listenTo(this.collection, 'all', this.render);
+    },
+    render: function() {
+        var self = this;
+        this.$el.empty();
+        _.each(this.collection.models, function(author) {
+            var authorView = new AuthorView({
+                model: author
+            });
+            //use self, "this" refers to each of these authorview and not the authorListView
+            self.$el.append(authorView.render().el)
+        })
+        return this;
+    }
+});
 
 
 // author collection///
 var AuthorCollection = Backbone.Collection.extend({
     model: Author
 });
+
+
+
+$(function() {
+
+
+    // new Instances ///
+    var author = new Author({
+        name: "stephen king"
+    });
+    var authorCollection = new AuthorCollection;
+
+    var authorListView = new AuthorListView({
+        collection: authorCollection,
+        el: $("#author-list")
+    });
+
+    authorCollection.add(author);
+
+    //listener for adding a new author and display the author
+    $(".new-author").on("submit", function(e) {
+        e.preventDefault();
+        //prevent html default reload
+        var nameField = $(".author-name");
+        var authorName = nameField.val();
+        nameField.val("");
+        authorCollection.add({
+            name: authorName
+        });
+        //backbone will create a model author for us
+
+    })
+
+})
