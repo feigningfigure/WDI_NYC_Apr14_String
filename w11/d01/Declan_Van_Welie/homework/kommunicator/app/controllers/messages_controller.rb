bdd88f54bc78576b6c3af03a29be87cec12586ee
Crustacean87! <declanv@gmail.com>
2014-06-30 23:25:05 -0400

    Add and delete of messages working. About to work on edit

---
+++ w11/d01/Declan_Van_Welie/homework/kommunicator/app/controllers/messages_controller.rb
@@ -0,0 +1,40 @@
+class MessagesController < ApplicationController
+
+  respond_to :json
+
+  def create
+
+    message = Message.create(message_params)
+    respond_with message
+
+  end
+
+  def index
+
+    messages = Message.all
+    respond_with messages
+
+  end
+
+  def show
+
+    message = Message.find_by_title(message_params)
+    respond_with message
+
+  end
+
+  def destroy
+
+    message = Message.find(params[:id])
+    message.destroy
+
+    respond_with message
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :id)
+  end
+
+end
