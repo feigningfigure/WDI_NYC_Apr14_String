1682f4248030f98a46acebace25ffdd8ddf1a67e
cdbajorin <christopher.bajorin@gmail.com>
2014-07-01 10:32:10 -0400

    hw

---
+++ w11/d01/Christopher_Bajorin/penguin_pals/app/controllers/penguins_controller.rb
@@ -0,0 +1,21 @@
+class PenguinsController < ApplicationController
+
+respond_to :json
+
+  def index
+    penguins = Penguin.all
+    respond_with penguins
+  end
+
+  def create
+    penguin = Penguin.create(penguin_params)
+    respond_with penguin
+  end
+
+  private
+
+  def penguin_params
+    params.require(:penguin).permit(:name)
+  end
+
+end
