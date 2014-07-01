79b210b7089749a60a9cf63d2f58c747848feec7
LizCira <liz.cira.goldstein@gmail.com>
2014-06-30 23:44:27 -0400

    create works

--- w11/d01/Liz_Goldstein/postr/app/controllers/posts_controller.rb
+++ w11/d01/Liz_Goldstein/postr/app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+
+   respond_to :json
+
+   def index
+      posts = Post.all
+      respond_with posts
+   end
+
+   def create
+    post = Post.create(post_params)
+    respond_with post
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit(:title)
+  end
+
 end
---
+++ w11/d01/Liz_Goldstein/postr/public/javascripts/backbone/app.js
@@ -0,0 +1,30 @@
+var Postapp = Postapp || {
+    Models: {},
+    Collections: {},
+    Views: {}
+};
+
+Postapp.initialize = function() {
+    var collection = new Postapp.Collections.PostCollection();
+    var listView = new Postapp.Views.PostListView({
+        collection: collection,
+        el: $('.post-list')
+    });
+
+    collection.fetch();
+
+    $('.posts').find('form').on("submit", function(e) {
+        e.preventDefault();
+        var postTitle = $('input').val();
+        $('input').val('')
+        collection.create({
+            title: postTitle
+        })
+
+    })
+}
+
+
+$(function() {
+    Postapp.initialize();
+});
