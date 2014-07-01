d5bc26ca230b45d7d0def1e88dfc7207d45376fb
mrwilsondj <mrwilsondj@gmail.com>
2014-06-30 23:12:18 -0400

    MVC details

--- w11/d01/Wilson_Chan/records/db/schema.rb
+++ w11/d01/Wilson_Chan/records/db/schema.rb
@@ -16,4 +16,9 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+    create_table "records", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+
 end
