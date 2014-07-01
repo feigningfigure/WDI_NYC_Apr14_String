c766650683b5d4493c0a55179ea595acac1f9a14
daramao <dara.mao@gmail.com>
2014-07-01 09:30:01 -0400

    delete

--- w11/d01/Dara_Mao/cupcakes/public/javascripts/backbone/views/CupcakeView.js
+++ w11/d01/Dara_Mao/cupcakes/public/javascripts/backbone/views/CupcakeView.js
@@ -5,11 +5,23 @@ var Cupcakemaker = Cupcakemaker || {
 };
 
 Cupcakemaker.Views.CupcakeView = Backbone.View.extend({
+    intialize: function() {
+        this.listenTo(this.model, 'all', this.render)
+        this.listenTo(this.model, 'destroy', this.remove)
+    },
+
     tagName: 'li',
     template: _.template($('#cupcake-template').html()),
     render: function() {
         this.$el.empty();
         this.$el.html(this.template(this.model.attributes));
+        return this
+    },
+    events: {
+        'click button[name="delete_cupcake"]': 'removeCupcake'
+    },
+    removeCupcake: function() {
+        this.model.destroy();
         return this
     }
 });
