ba9c2eae68f2fb96b8ff5a971137976b75aa0334
mrwilsondj <mrwilsondj@gmail.com>
2014-07-01 20:13:56 -0400

    js code added

---
+++ w11/d02/Wilson_Chan/jasmine_test/app/assets/javascripts/jasmine_test.js
@@ -0,0 +1,13 @@
+window.JasmineTest = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  JasmineTest.initialize();
+});
