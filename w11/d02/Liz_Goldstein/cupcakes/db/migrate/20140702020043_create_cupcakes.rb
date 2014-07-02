cce174b6ef91672e0d042deed9391286057d774a
LizCira <liz.cira.goldstein@gmail.com>
2014-07-01 22:23:13 -0400

    seems to run a test on the commandline. now lets write some tests

--- w11/d02/Liz_Goldstein/cupcakes/db/migrate/20140702020043_create_cupcakes.rb
+++ w11/d02/Liz_Goldstein/cupcakes/db/migrate/20140702020043_create_cupcakes.rb
@@ -2,6 +2,8 @@ class CreateCupcakes < ActiveRecord::Migration
   def change
     create_table :cupcakes do |t|
 
+        t.string :name
+
       t.timestamps
     end
   end
--- w11/d02/Liz_Goldstein/cupcakes/spec/javascripts/foo_spec.js
+++
@@ -1,8 +0,0 @@
-// include spec/javascripts/helpers/some_helper_file.js and app/assets/javascripts/foo.js
-//= require helpers/some_helper_file
-//= require foo
-describe('Foo', function() {
-  it("does something", function() {
-    expect(1 + 1).toBe(2);
-  });
-})
